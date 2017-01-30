e219b1053fa4b604d6864e561453c6e39cbf3a7a
Valery Shchepashchenko <valery.sntx@gmail.com>
2017-01-30 18:26:33 +0200

    AbstractExchangeRateProvider + Dtos  - refactoring for adapter pattern implementation

---
+++ ExchangeRateProvider/ExchangeRateProvider/AbstractExchangeRateProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRateProvider.Infrastructure.ApiProxy;
+using ExchangeRateProvider.Model;
+
+namespace ExchangeRateProvider
+{
+    /// <summary>
+    /// Abstract ExchangeRateProvider
+    /// </summary>
+    public abstract class AbstractExchangeRateProvider: ApiProxy, IExchangeRateProvider
+    {
+        public AbstractExchangeRateProvider(string controllerName) : base(controllerName)
+        {
+        }
+
+        public abstract IEnumerable<ExchangeRateDto> GetExchangeRates(IEnumerable<CurrencyDto> currencies);
+    }
+}
