694a1e836cb806c7360180e0211ae67044f5328a
flypixel <flypixel@outlook.com>
2017-02-25 21:40:40 +0200

    Czech National Bank

--- ExchangeRateUpdater/ExchangeRateProvider.cs
+++ ExchangeRateUpdater/ExchangeRateProvider.cs
@@ -3,66 +3,67 @@ using System.Linq;
 using System.Net;
 using System.Xml.Linq;
 using System;
+using System.IO;
+using System.Globalization;
 
 namespace ExchangeRateUpdater
 {
     public class ExchangeRateProvider
     {
-		const string _query = "http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.xchange where pair in ({0})&env=store://datatables.org/alltableswithkeys";
+		const string _query = "https://www.cnb.cz/en/financial_markets/foreign_exchange_market/exchange_rate_fixing/daily.txt";
 
-		XElement LoadPage(Uri uri)
+		string LoadFile(Uri uri)
 		{
 			var req = WebRequest.Create(uri);
 			using (var resp = req.GetResponse())
 			{
 				using (var stream = resp.GetResponseStream())
 				{
-					return XDocument.Load(stream).Root;
+					using (var reader = new StreamReader(stream))
+					{
+						return reader.ReadToEnd();
+					}
 				}
 			}
 		}
 
-		IEnumerable<string> JoinCurrencies(IEnumerable<Currency> currencies)
+		ExchangeRate ParseExchangeRate(string text)
 		{
-			foreach (var c1 in currencies)
-			{
-				foreach (var c2 in currencies)
-				{
-					if (c1.Code != c2.Code) yield return $"\"{c1.Code}{c2.Code}\"";
-				}
-			}
+			var line = text.Split('|');
+			if (line.Length != 5) 
+				throw new ApplicationException("Unexpected length of datasource line");
+			var rate = decimal.Parse(line[4], CultureInfo.InvariantCulture);
+			var amount = int.Parse(line[2]);
+			var code = line[3];
+			return new ExchangeRate(new Currency(code), new Currency("CZK"), rate / amount);
 		}
 
-		string CreateQuery(IEnumerable<Currency> currencies)
+		IEnumerable<ExchangeRate> ParseExchangeRates(string page)
 		{
-			var joined = JoinCurrencies(currencies);
-			return String.Format(_query, String.Join(",", joined));
-		}
-
-		IEnumerable<ExchangeRate> ParseExchangeRates(XElement page)
-		{
-			return page.Descendants()
-						.Where(el => el.Name.LocalName == "rate")
-						.Select(el =>
-								new
-								{
-									Currencies = el.Element("Name")
-												   .Value
-												   .Split('/')
-												   .Select(x => new Currency(x))
-												   .ToArray(),
-									Rate = el.Element("Rate").Value
-								})
-				       .Where(x => x.Rate != "N/A")
-					   .Select(x => new ExchangeRate(x.Currencies[0], x.Currencies[1], Convert.ToDecimal(x.Rate)));
-
+			return page.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries)
+				.Skip(2)
+			    .Select(ParseExchangeRate);
 		}
 
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-			var query = CreateQuery(currencies);
-			var page = LoadPage(new Uri(query));
-			return ParseExchangeRates(page);
+			var page = LoadFile(new Uri(_query));
+			return ParseExchangeRates(page)
+				.Where(exr => currencies
+				       .Contains(exr.SourceCurrency, new CurrencyComparrer()));
         }
+
+		public class CurrencyComparrer : IEqualityComparer<Currency>
+		{
+			public bool Equals(Currency x, Currency y)
+			{
+				return x.Code == y.Code;
+			}
+
+			public int GetHashCode(Currency obj)
+			{
+				return obj.GetHashCode();
+			}
+		}
 	}
 }
