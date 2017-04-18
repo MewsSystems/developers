6541f112fe47a9466fd8de5d2b4262ab61c8f660
dim0nickb <basstorniloo@gmail.com>
2017-04-19 00:40:38 +0300

    test

---
+++ ExchangeRateUpdater/BankConnection/IExchangeDataProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.BankConnection
+{
+	interface IExchangeDataProvider
+	{
+		/// <summary>
+		/// Get exchange rate for currency pair
+		/// </summary>
+		/// <param name="code1">First currency code</param>
+		/// <param name="code2">Second currency code</param>
+		/// <returns>Exchange rate</returns>
+		string GetPairValue(string code1, string code2);
+	}
+}
