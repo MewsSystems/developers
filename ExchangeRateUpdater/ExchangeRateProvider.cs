9447f7f0dd170643bee7c3702f34ba5b96e2b119
Dmitry Shkarendin <dmitry.shkarendin@hotmail.com>
2017-08-19 16:58:09 +0600

    Test results

--- ExchangeRateUpdater/ExchangeRateProvider.cs
+++ ExchangeRateUpdater/ExchangeRateProvider.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace ExchangeRateUpdater
 {
@@ -11,9 +17,62 @@ namespace ExchangeRateUpdater
         /// do not return exchange rate "USD/EUR" with value calculated as 1 / "EUR/USD". If the source does not provide
         /// some of the currencies, ignore them.
         /// </summary>
-        public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
+        public async Task<IEnumerable<ExchangeRate>> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            List<ExchangeRate> results = new List<ExchangeRate>();
+
+            foreach (var currency in currencies)
+            {
+
+                ReturnedCurrencies result = await GetAsyncSimple<ReturnedCurrencies>(String.Format(ConfigurationManager.AppSettings.Get("SourceAPI"), currency.Code), currency.Code);
+
+                if (result != null)
+                {
+                    foreach (var item in result.rates)
+                    {
+                        results.Add(new ExchangeRate(currency, new Currency(item.Key), item.Value));
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        private HttpClient CreateHttpClient()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
+
+        /// <summary>
+        /// Here is take all resuls of our source code currency.
+        /// The url of source you can find in app.config.
+        /// </summary>
+        private async Task<T> GetAsyncSimple<T>(string url, string currencyCode)
+            where T : class
+        {
+            HttpClient httpClient = CreateHttpClient();
+            try
+            {
+                string response = await httpClient.GetStringAsync(url);
+                var result = await Task.Run(() => JsonConvert.DeserializeObject<T>(response));
+
+                return result;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Could not be found: {currencyCode}");
+                return default(T);
+            }
+        }
+
+        private class ReturnedCurrencies
+        {
+            public string @base { get; set; }
+            public string date { get; set; }
+            public Dictionary<string, decimal> rates { get; set; }
+
         }
     }
 }
