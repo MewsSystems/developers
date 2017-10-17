1bb20dff011e8a486f1b939fc8f56bd3e356f613
PMilan <polacek.e.milan@gmail.com>
2017-10-17 23:13:45 +0200

    Added test files Added classes (method) and structures from basic view to resolving this problem Time: 20 min (coding), 20 min (googling)

---
+++ ExchangeRateUpdater/App_helpers/FileParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater
+{
+    class FileParser
+    {
+        public List<ExchangeRate> GetDataFromServer(DataSourceEnum dataSource)
+        {
+            List<ExchangeRate> eRates = new List<ExchangeRate>();
+            switch (dataSource)
+            {
+                case DataSourceEnum.CzechNationalBank:
+                    return GetRatesFromCzechNationalBank();
+                case DataSourceEnum.EuropeCentralBank:
+                    return GetRatesFromEuropeCentralBank();
+                case DataSourceEnum.CustomsAdministrationXML:
+                    return GetRatesFromCustomsAdministration(true);
+                case DataSourceEnum.CustomsAdministrationTXT:
+                    return GetRatesFromCustomsAdministration();
+                default:
+                    throw new ArgumentException("Not implemented datasource parser");
+            }
+        }
+
+        /// <summary>
+        /// Returning data from xml or txt depend on bool
+        /// </summary>
+        /// <param name="fileType">If true return data from xml. Default return data from txt.</param>
+        private List<ExchangeRate> GetRatesFromCustomsAdministration(bool fileType=false)
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<ExchangeRate> GetRatesFromEuropeCentralBank()
+        {
+            throw new NotImplementedException();
+        }
+
+        private List<ExchangeRate> GetRatesFromCzechNationalBank()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
