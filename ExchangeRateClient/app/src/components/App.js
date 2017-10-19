50374976c005fde7ab2274470c7e9f9544ff8d55
Dmitry Samsonov <user1736@yandex.ru>
2017-10-19 22:20:49 +0300

    Added filter styling

--- ExchangeRateClient/app/src/components/App.js
+++ ExchangeRateClient/app/src/components/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import sn from 'classnames';
+import s from '../styles/App.less';
 import Filter from './Filter';
 import {getFilteredRates} from '../selectors';
 
 
 const App = (props) => (
-  <div>
+  <div className={sn(s.app)}>
     <Filter />
     <pre>{JSON.stringify(props.rates, null, ' ')}</pre>
   </div>
