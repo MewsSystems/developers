5ef52e595802fee845b995325e6956bd74059973
wertar <wertar.g@gmail.com>
2017-10-30 23:41:49 +0100

    CNB

--- ExchangeRateUpdater/Currency.cs
+++ ExchangeRateUpdater/Currency.cs
@@ -1,6 +1,8 @@
-﻿namespace ExchangeRateUpdater
+﻿using System;
+
+namespace ExchangeRateUpdater
 {
-    public class Currency
+    public class Currency: IEquatable<Currency>
     {
         public Currency(string code)
         {
@@ -11,5 +13,17 @@
         /// Three-letter ISO 4217 code of the currency.
         /// </summary>
         public string Code { get; private set; }
+               
+        public bool Equals(Currency other)
+        {
+            if (other == null) return false; 
+           return this.Code == other.Code;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Currency);
+        }
+
     }
 }
