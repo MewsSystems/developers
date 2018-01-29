cba69a424d6e1fe0dda20bf2cc33db340688d21f
unknown <rershov@factor-ts.ru>
2018-01-29 20:46:53 +0300

    new coomit

--- ExchangeRateUpdaterTests/Properties/AssemblyInfo.cs
+++ ExchangeRateUpdaterTests/Properties/AssemblyInfo.cs
@@ -1,19 +1,18 @@
 ﻿using System.Reflection;
-using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExchangeRateUpdater")]
+[assembly: AssemblyTitle("ExchangeRateUpdaterTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExchangeRateUpdater")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ExchangeRateUpdaterTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b2695d6-d24c-4460-a58e-a10f08550ce0")]
+[assembly: Guid("d5a7132c-a1d9-454f-aace-ee52ca65cf2e")]
 
 // Version information for an assembly consists of the following four values:
 //
