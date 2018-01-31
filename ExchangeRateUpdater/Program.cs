2cec4f141293e5c954e52f67a5bb7c072aef9c57
Roman <1991.e.r.a@gmail.com>
2018-01-31 14:52:54 +0300

    Other changes

--- ExchangeRateUpdater/Program.cs
+++ ExchangeRateUpdater/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExchangeRateUpdater.Providers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -23,7 +24,7 @@ namespace ExchangeRateUpdater
         {
             try
             {
-                var provider = new ExchangeRateProvider();
+                var provider = new ExchangeRateProvider(new CzechNationalBankProvider());
                 var rates = provider.GetExchangeRates(currencies);
 
                 Console.WriteLine("Successfully retrieved " + rates.Count() + " exchange rates:");
