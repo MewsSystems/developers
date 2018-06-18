0d4c43b83be174242b4b6c65bafccaccfcbd0daf
Daria Doronina <daria.doronina@easysoftware.com>
2018-06-18 04:18:27 +0200

    displays trends

--- ExchangeRateClient/app/modules/rates/components/CurrencyPairs.jsx
+++ ExchangeRateClient/app/modules/rates/components/CurrencyPairs.jsx
@@ -9,62 +9,85 @@ class CurrencyPairs extends Component {
         super(props);
 
         this.state = {
-            timer: null
-        }
+            timer: null,
+            anotherTimer: null,
+            currencyPairs: {},
+            propsArray: []
+        };
+
     }
 
     componentDidMount() {
-        let arrayOfIds = [];
-        this.props.currencyPairs.forEach(x => arrayOfIds.push(x['id']));
-
-        let timer = setInterval(() => this.props.fetchCurrencyRates(arrayOfIds), 15000);
-        this.setState({timer});
+        this.props.fetchCurrencyRates(Object.keys(this.props.currencyPairs));
+        let timer = setInterval(() => this.props.fetchCurrencyRates(Object.keys(this.props.currencyPairs)), 15000);
+        let anotherTimer = setInterval(() => this.setState({currencyPairs: this.props.currencyPairs}), 15000);
+        this.setState({
+            timer,
+            anotherTimer
+        });
     }
 
     componentWillUnmount() {
         this.clearInterval(this.state.timer);
+        this.clearInterval(this.state.anotherTimer);
+    }
+
+    propsToArray() {
+        let newArr = [];
+        const {currencyPairs} = this.props;
+        for (let key in currencyPairs) {
+            if (currencyPairs.hasOwnProperty(key)) {
+                let newObj = {
+                    id: key,
+                    currency1: currencyPairs[key][0],
+                    currency2: currencyPairs[key][1],
+                    rate: currencyPairs[key][2],
+                    prevRate: (this.state.currencyPairs[key] ? this.state.currencyPairs[key][2] : undefined)
+                };
+                newArr.push(newObj);
+            }
+        }
+        this.setState({
+            propsArray: newArr
+        });
     }
 
     render() {
         const {currencyPairs} = this.props;
 
-        const currencyLis = currencyPairs.map(currency => (
-            <li key={currency.id}>{currency.currency1['code'] + ' / ' + currency.currency2['code']}</li>
-        ));
+        console.log(this.state);
 
         let newArr = [];
-
-        for (let key in this.props.rates) {
-            if (this.props.rates.hasOwnProperty(key)) {
+        for (let key in currencyPairs) {
+            if (currencyPairs.hasOwnProperty(key)) {
                 let newObj = {
                     id: key,
-                    rate: this.props.rates[key]
+                    currency1: currencyPairs[key][0],
+                    currency2: currencyPairs[key][1],
+                    rate: currencyPairs[key][2],
+                    prevRate: (this.state.currencyPairs[key] ? this.state.currencyPairs[key][2] : undefined)
                 };
                 newArr.push(newObj);
             }
         }
+
+        const currencyLis = newArr.map(currency => (
+            <li key={currency.id}>{currency.currency1['code'] + ' / ' + currency.currency2['code'] + ' = ' + (currency.rate ? currency.rate['rate'] : '') + ' ' + (currency.prevRate ? (currency.rate['rate'] > currency.prevRate['rate']) ? 'up' : 'down' : 'same')}</li>
+        ));
 
         return (
             <div>
                 <ul>
                     {currencyLis}
                 </ul>
-                <p>yolo</p>
-                <ul>
-                    {newArr.map(currency => (
-                        <li key={currency.id}>{currency.rate}</li>
-                    ))}
-                </ul>
             </div>
         );
     }
-};
-
-// export default CurrencyPairs;
+}
 
 function mapStateToTheProps(state) {
     return {
-        rates: state.rates,
+        currencyPairs: state.currencyPairs,
     };
 }
 
--- ExchangeRateClient/app/modules/rates/reducer.js
+++ ExchangeRateClient/app/modules/rates/reducer.js
@@ -9,30 +9,40 @@ export default (state = INITIAL_STATE, action) => {
         case FETCH_CURRENCY_PAIRS_SUCCEEDED: {
             const {currencyPairs} = action.payload;
 
-            let newArr = [];
-            for (let key in currencyPairs) {
-                if (currencyPairs.hasOwnProperty(key)) {
-                    let newObj = {
-                        id: key,
-                        currency1: currencyPairs[key][0],
-                        currency2: currencyPairs[key][1]
-                    };
-                    newArr.push(newObj);
-                }
-            }
+            // let newArr = [];
+            // for (let key in currencyPairs) {
+            //     if (currencyPairs.hasOwnProperty(key)) {
+            //         let newObj = {
+            //             id: key,
+            //             currency1: currencyPairs[key][0],
+            //             currency2: currencyPairs[key][1]
+            //         };
+            //         newArr.push(newObj);
+            //     }
+            // }
 
             return {
                 ...state,
-                currencyPairs: newArr
+                // currencyPairs: newArr
+                currencyPairs: currencyPairs
             };
         }
         case FETCH_CURRENCY_RATES_SUCCEEDED: {
             const {rates} = action.payload;
 
-            return {
-                ...state,
-                rates
-            };
+            let newState = JSON.parse(JSON.stringify(state)); //dirty hack
+
+            for (let key in rates) {
+                if (rates.hasOwnProperty(key) && newState.currencyPairs.hasOwnProperty(key)) {
+                    if (newState.currencyPairs[key].length <= 2) {
+                        newState.currencyPairs[key].push({rate: rates[key]});
+                    } else {
+                        newState.currencyPairs[key][2] = {rate: rates[key]};
+                    }
+                }
+            }
+
+            return newState;
         }
         default:
             return state;
