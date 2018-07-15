4c564bf655f1d223d6012d6d4334e20d12ece53b
Sebastian <sebastian.vorac@netvor.co>
2018-07-15 20:20:53 +0200

    implemented increasing/decreasing quantity of product and connected it with the state, calculated totals according to quantity and cost of the products, callback to dialog submit - displaying the result into the console and to the user in alert

--- ExchangeRateClient/app/components/App.jsx
+++ ExchangeRateClient/app/components/App.jsx
@@ -5,10 +5,13 @@ import { hot } from 'react-hot-loader'
 import { Button } from 'mews-ui';
 import OrderDialog from './billings/OrderDialog';
 
+import './styles.scss';
+import 'font-awesome/css/font-awesome.min.css';
+
 const App = () => (
-    <div>
-        <OrderDialog />
-    </div>
+	<div>
+		<OrderDialog />
+	</div>
 );
 
 export default hot(module)(App);
--- ExchangeRateClient/app/components/billings/Product.jsx
+++ ExchangeRateClient/app/components/billings/Product.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { ListItem, FontIcon } from "mews-ui";
+import styled from 'styled-components';
 
 export default (props) => {
-    const fontIconProps = {
-        onClick: () => props.onClick(props.id),
-        value: '▶',
-    };
 
-    return (
-        <ListItem 
-            primaryText={props.name}
-            secondaryText={props.cost} 
-            rightIcon={<FontIcon {...fontIconProps} />} 
-        />
-    );
+	const onClickHandler = () => {
+		props.onClick(props.id);
+	};
+
+	return (
+		<ListItem
+			className='listStyleItem'
+			primaryText={props.name}
+			secondaryText={`$${props.cost}`}
+			rightIcon={<i className='fa fa-chevron-right' aria-hidden={true} onClick={onClickHandler} />}
+			style={listStyle}
+			onClick={onClickHandler}
+		/>
+	);
+};
+
+const listStyle = {
+	boxShadow: '0 2px 5px 0 rgba(0,0,0,0.15)',
+	width: '48%',
+	display: 'inline-block',
+	backgroundColor: 'white',
+	borderRadius: '4px',
+	marginBottom: '13px',
 };
 
