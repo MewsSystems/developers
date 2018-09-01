319ba962946a197fc6895a354010a8b913f3d9a3
Ilnur Khalilov <xomyaq@gmail.com>
2018-09-01 21:50:03 +0300

    Scaffolding

---
+++ ExchangeRateClient/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { modules: false }],
+    '@babel/preset-react',
+    '@babel/preset-flow',
+  ],
+};
---
+++ ExchangeRateClient/src/Root.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Root = () => <h1>Hello world!</h1>;
+
+export default Root;
