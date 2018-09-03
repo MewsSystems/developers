ce2565158d46d2dd4a6014534080c5c5e025e37b
Ilnur Khalilov <xomyaq@gmail.com>
2018-09-03 10:21:58 +0300

    Reviewed data storing

--- ExchangeRateClient/src/reducer.js
+++ ExchangeRateClient/src/reducer.js
@@ -4,17 +4,29 @@ import { Map, Record, type RecordOf, type RecordFactory } from 'immutable';
 import getIn from 'lodash.get';
 import { actionTypes } from './actions';
 
-type RequestStatus = 'init' | 'loading' | 'success' | 'failure';
-type Action = { type: string, error?: boolean, payload?: Object };
-type Currency = { code: string, name: string };
-type CurrencyPair = [Currency, Currency];
+export type RequestStatus = 'init' | 'loading' | 'success' | 'failure';
+export type Action = { type: string, error?: boolean, payload?: Object };
+export type Currency = { code: string, name: string };
+
+type CurrencyPairProps = {
+  id: ?string,
+  left: Currency,
+  right: Currency,
+};
+export type CurrencyPair = RecordOf<CurrencyPairProps>;
+
 type AppStateProps = {
   configRequestStatus: RequestStatus,
   currencyPairs: Map<string, CurrencyPair>,
-  rates: Map<string, number>,
+  rates: Map<string, Array<number>>,
 };
 type AppState = RecordOf<AppStateProps>;
 
+const CurrencyPairRecord: RecordFactory<CurrencyPairProps> = Record({
+  id: undefined,
+  left: { code: '', name: '' },
+  right: { code: '', name: '' },
+});
 const AppStateRecord: RecordFactory<AppStateProps> = Record({
   configRequestStatus: 'init',
   currencyPairs: Map(),
@@ -28,18 +40,40 @@ function app(state: AppState = appInitialState, action: Action) {
       return state.set('configRequestStatus', 'loading');
 
     case actionTypes.FETCH_CONFIG_FULFILLED: {
-      const currencyPairs = getIn(action, ['payload', 'currencyPairs'], {});
+      const loadedCurrencyPairs = getIn(
+        action,
+        ['payload', 'currencyPairs'],
+        {}
+      );
+      const currencyPairs = Map(
+        Object.keys(loadedCurrencyPairs).map(id => {
+          const [left, right] = loadedCurrencyPairs[id];
+
+          return [id, CurrencyPairRecord({ id, left, right })];
+        })
+      );
 
       return state.merge({
+        currencyPairs,
         configRequestStatus: 'success',
-        currencyPairs: Map(currencyPairs),
       });
     }
 
     case actionTypes.FETCH_RATES_FULFILLED: {
-      const rates = getIn(action, ['payload', 'rates'], {});
+      const loadedRates = getIn(action, ['payload', 'rates'], {});
+      const prevRates = state.get('rates');
+      const nextRates = Object.keys(loadedRates).map(id => {
+        const rate = loadedRates[id];
+        const prevRate = prevRates.get(id);
 
-      return state.mergeIn(['rates'], Map(rates));
+        if (prevRate !== undefined) {
+          return [id, [prevRate[1], rate]];
+        }
+
+        return [id, [rate, rate]];
+      });
+
+      return state.set('rates', Map(nextRates));
     }
 
     default:
@@ -50,11 +84,10 @@ function app(state: AppState = appInitialState, action: Action) {
 type StateProps = {
   app: AppState,
 };
+export type State = RecordOf<StateProps>;
 
 export const StateRecord: RecordFactory<StateProps> = Record({
   app: appInitialState,
 });
-
-export type State = RecordOf<StateProps>;
 
 export default combineReducers({ app }, StateRecord);
