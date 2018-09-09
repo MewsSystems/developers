01d6d46c90f5eb8fdd3b65b5269b3aa9e8390abe
Filip Messa <filip.messa@kiwi.com>
2018-09-09 14:38:46 +0200

    create new client app

---
+++ ExchangeRateClient/client-app/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react',
+    'plugin:jest/recommended',
+    'plugin:flowtype/recommended',
+  ],
+  plugins: ['prettier', 'jest', 'flowtype'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
