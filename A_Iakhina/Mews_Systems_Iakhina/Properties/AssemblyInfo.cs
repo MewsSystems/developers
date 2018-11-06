1b12f5c0f3da284652bda66276070832e486082e
Anastasiia.Iakhina <legedzaa@gmail.com>
2018-11-06 20:26:07 +0100

    Samples of API calls tests

---
+++ A_Iakhina/Mews_Systems_Iakhina/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mews_Systems_Iakhina")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Encoder")]
+[assembly: AssemblyProduct("Mews_Systems_Iakhina")]
+[assembly: AssemblyCopyright("Copyright © Encoder 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b15b695f-3ec1-4b39-98dc-5ceb57f9a36e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
