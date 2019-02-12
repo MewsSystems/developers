945ae021542663782e51941ab160a0b1adfea081
unknown <sanchosa@mail.ru>
2019-02-12 21:32:43 +0600

    Added rates request with interval.

--- Frontend/Task/src/containers/Rates/state/saga.js
+++ Frontend/Task/src/containers/Rates/state/saga.js
@@ -1,9 +1,10 @@
-import {takeLatest, put, call, delay} from 'redux-saga/effects'
+import {takeLatest, put, call, delay, select} from 'redux-saga/effects'
 import request from 'utils/request'
 import {ratesAPIURI} from 'common/constants'
 import {SET_CONFIG} from 'containers/App/state/constants'
 import {setPairs, setRates} from './actions'
 import {SELECT_PAIRS} from './constants'
+import {makeSelectSelectedPairs} from './selectors'
 
 const prepareName = (source, full) => source && `${source.code}${full ? `(${source.name})` : ``}`
 const getName = (data, full = true) => {
@@ -30,14 +31,33 @@ function *fetchRates({payload: currencyPairIds}) {
 	if (currencyPairIds.length === 0) return
 
 	yield delay(500)
+	yield call(runFetchRates, currencyPairIds)
+}
+
+function *runFetchRates(currencyPairIds) {
 	const rates = yield call(request.get, `${ratesAPIURI}/rates`, {currencyPairIds})
 
 	if (rates) {
-		yield put(setRates(rates.rates))	
+		yield put(setRates(rates.rates))
+	}
+}
+
+function *intervalFetch() {
+	while (true) {
+		const selectedPairs = yield select(makeSelectSelectedPairs())
+
+		if (selectedPairs.size > 0) {
+			const pairs = selectedPairs.toJS()
+
+			yield call(runFetchRates, pairs)
+		}
+
+		yield delay(10000)
 	}
 }
 
 export default function *storageSaga() {
 	yield takeLatest(SET_CONFIG, buildPairs)
 	yield takeLatest(SELECT_PAIRS, fetchRates)
+	yield call(intervalFetch)
 }
