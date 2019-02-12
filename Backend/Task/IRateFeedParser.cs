c92e440e583cd66c5a7548784a649825529c4186
Michal Náprstek <c_Michal.Naprstek@viasat.com>
2019-02-12 09:26:52 +0100

    interfaces for loading and parsing feed introduced

---
+++ Backend/Task/IRateFeedParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRateUpdater
+{
+    /// <summary>
+    /// Parses exchange rates out of the feed data.
+    /// </summary>
+    public interface IRateFeedParser
+    {
+        /// <summary>
+        /// Parses exchange rates out of the feed data.
+        /// </summary>
+        /// <param name="feed">Feed data in string form.</param>
+        /// <returns>Collection of <see cref="ExchangeRate"/> parsed from the feed.</returns>
+        IEnumerable<ExchangeRate> Parse(string feed);
+    }
+}
---
+++ Backend/Task/IRateFeedSource.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater
+{
+    /// <summary>
+    /// Loads exchange rates from external source in form of string feed.
+    /// </summary>
+    public interface IRateFeedSource
+    {
+        /// <summary>
+        /// Asynchronously loads exchange rates from external source in form of string feed.
+        /// </summary>
+        /// <returns></returns>
+        Task<string> LoadRatesFeedAsync();
+    }
+}
