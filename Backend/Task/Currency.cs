ef4ea42328c82304dedeaae3c207758041cb0344
Michal Náprstek <c_Michal.Naprstek@viasat.com>
2019-02-12 11:26:02 +0100

    generated comments removed

--- Backend/Task/Currency.cs
+++ Backend/Task/Currency.cs
@@ -18,17 +18,9 @@ namespace ExchangeRateUpdater
         {
             return Code;
         }
-
-        // override object.Equals
+        
         public override bool Equals(object obj)
         {
-            //       
-            // See the full list of guidelines at
-            //   http://go.microsoft.com/fwlink/?LinkID=85237  
-            // and also the guidance for operator== at
-            //   http://go.microsoft.com/fwlink/?LinkId=85238
-            //
-
             if (obj == null || GetType() != obj.GetType())
             {
                 return false;
@@ -36,8 +28,7 @@ namespace ExchangeRateUpdater
 
             return Code.Equals(((Currency)obj).Code);
         }
-
-        // override object.GetHashCode
+        
         public override int GetHashCode()
         {
             return Code.GetHashCode();
