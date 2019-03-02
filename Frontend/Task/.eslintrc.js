826153c351c47c01901aa8625f562418cec92ec8
Vladimír Zdražil <mail@vzdrazil.com>
2019-03-02 13:57:06 +0100

    Transfer to create react app

---
+++ Frontend/Task/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['react-app'],
+  plugins: ['react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+  },
+};
--- Frontend/Task/server/index.js
+++ Frontend/Task/server/index.js
@@ -50,6 +50,6 @@ server.get('/rates', (req, res) => {
     }
 });
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000.');
+server.listen(3001, () => {
+    console.log('Server is running on port 3001.');
 });
