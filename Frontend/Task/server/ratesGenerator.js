14950d0ab153b441c459a8d405fec78d26ed6a00
Vladimír Zdražil <mail@vzdrazil.com>
2019-03-09 16:47:03 +0100

    Fix mockup server to allow decreasing value trend

--- Frontend/Task/server/ratesGenerator.js
+++ Frontend/Task/server/ratesGenerator.js
@@ -18,7 +18,7 @@ module.exports = function rateGenerator({ generator, pairCount, updateInterval }
             return Object.assign({}, currentRates);
         },
     };
-    
+
     function generateCurrencyPairs() {
         let pairs = {};
 
@@ -44,7 +44,7 @@ module.exports = function rateGenerator({ generator, pairCount, updateInterval }
 
         for (let pairId of Object.keys(currentRates)) {
             const value = currentRates[pairId];
-            updatedRates[pairId] = generator.floating({ min: value + MAX_RATE_UPDATE_STEP, max: value + MAX_RATE_UPDATE_STEP })
+            updatedRates[pairId] = generator.floating({ min: value - MAX_RATE_UPDATE_STEP, max: value + MAX_RATE_UPDATE_STEP })
         }
 
         return updatedRates;
