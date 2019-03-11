13d36ff2a88e3436992c1131a39836fb084f4901
Vladimír Zdražil <mail@vzdrazil.com>
2019-03-11 18:47:50 +0100

    Remove serviceWorker

--- Frontend/Task/src/index.js
+++ Frontend/Task/src/index.js
@@ -9,7 +9,6 @@ import { configureStore, getPersistor } from './configureStore';
 import config from './config';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const preloadedState = {};
 const env = {
@@ -27,8 +26,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
