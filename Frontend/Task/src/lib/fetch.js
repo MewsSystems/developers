fdf4ddd6fe92a15cc437e24ad8ffc330f8a40c7b
Vladimír Zdražil <mail@vzdrazil.com>
2019-03-12 17:36:09 +0100

    Retry once on 500 status

--- Frontend/Task/src/lib/fetch.js
+++ Frontend/Task/src/lib/fetch.js
@@ -1,6 +1,23 @@
 import { composableFetch, pipeP, tryCatchP } from 'composable-fetch';
 
-export const handleFetchError = async error => {
+const newError = (id, message, res) => {
+  const error = new Error(message);
+  error.id = id;
+  if (res) error.res = res;
+  return error;
+};
+
+const checkStatus = res => {
+  if (res.status === 500)
+    throw newError(
+      'InvalidStatusCodeError',
+      'Invalid status code: ' + res.status,
+      res,
+    );
+  return res;
+};
+
+const handleFetchError = async error => {
   const { res } = error;
   const genericError = new Error('Fetch error');
   genericError.res = res;
@@ -22,7 +39,13 @@ export const fetchJSON = url =>
       composableFetch.withBaseUrl(url),
       composableFetch.withHeader('Accept', 'application/json'),
       composableFetch.withEncodedBody(JSON.stringify),
-      composableFetch.fetch1(fetch),
+      composableFetch.retryable(
+        pipeP(
+          composableFetch.fetch1(fetch),
+          checkStatus,
+        ),
+      ),
+      composableFetch.withRetry(2),
       composableFetch.withSafe204(),
       composableFetch.decodeJSONResponse,
       composableFetch.checkStatus,
