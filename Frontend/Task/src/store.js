896b358e10a59fe19f1e06d12abde43c17903620
overzunov <overzunov@lohika.com>
2019-04-07 14:08:54 +0300

    add persistansy level to keep store data between reloads

--- Frontend/Task/src/store.js
+++ Frontend/Task/src/store.js
@@ -2,13 +2,17 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import immediateRatesFetcher from './middlewares/immediate-rates-fetcher';
+import { loadState, saveState } from './helpers/local-storage';
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 /* eslint-enable */
 
+const persistedState = loadState();
+
 const store = createStore(
   rootReducer,
+  persistedState,
   composeEnhancers(
     applyMiddleware(
       thunk,
@@ -16,6 +20,10 @@ const store = createStore(
     ),
   ),
 );
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
 
 if (module.hot) {
   // Enable Webpack hot module replacement for reducers
