35d96a60a0b96cf9006e2eb845cc4d38dea8a913
Trong Chung Chau Nguyen <nguyet39@fit.cvut.cz>
2019-04-25 00:20:13 +0200

    Backend task complete - in-person meeting with Jan Meissner on Friday, May 3rd, at 10:40

--- Backend/Task/ExchangeRateProvider.cs
+++ Backend/Task/ExchangeRateProvider.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ExchangeRateUpdater
 {
     public class ExchangeRateProvider
     {
+        private readonly ExchangeRateRestClient _restClient = new ExchangeRateRestClient();
         /// <summary>
         /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
         /// by the source, do not return calculated exchange rates. E.g. if the source contains "EUR/USD" but not "USD/EUR",
@@ -13,7 +15,24 @@ namespace ExchangeRateUpdater
         /// </summary>
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            var exchangeRates = new List<ExchangeRate>();
+            var response = _restClient.GetExchangeRate();
+            var codes = currencies.Select(a => a.Code).ToList();
+            var dictionary = currencies.ToDictionary(a => a.Code);
+            var exchangeRows = response.Table.Rows.Where(a => codes.Contains(a.Code)).ToList();
+
+            foreach (var row1 in exchangeRows)
+            {
+                foreach(var row2 in exchangeRows)
+                {
+                    var source = dictionary[row1.Code];
+                    var target = dictionary[row2.Code];
+                    var exrate = (Decimal.Parse(row1.Rate) * row2.Amount) / (Decimal.Parse(row2.Rate) * row1.Amount);
+                    exchangeRates.Add(new ExchangeRate(source, target, exrate));
+                }
+            }
+
+            return exchangeRates;
         }
     }
 }
---
+++ Backend/Task/ExchangeRateRestClient.cs
@@ -0,0 +1,72 @@
+﻿using RestSharp;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ExchangeRateUpdater
+{
+    public class ExchangeRateRestClient
+    {
+        private RestClient _client = new RestClient("https://www.cnb.cz/");
+        private RestRequest _request = new RestRequest("cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml");
+        private XmlSerializer _serializer = new XmlSerializer(typeof(ResponseExchangeRate));
+
+        public ResponseExchangeRate GetExchangeRate()
+        {
+            var response = _client.Execute(_request);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                var bytes = Encoding.UTF8.GetBytes(response.Content);
+                var stream = new MemoryStream(bytes);
+                var reader = new StreamReader(stream, Encoding.UTF8, true);
+                return (ResponseExchangeRate)_serializer.Deserialize(reader);
+            }
+            return null;
+        }
+    }
+
+    [XmlRoot("kurzy")]
+    public partial class ResponseExchangeRate
+    {
+        [XmlElement("tabulka")]
+        public RateTable Table { get; set; }
+
+        [XmlAttribute("banka")]
+        public string Bank { get; set; }
+
+        [XmlAttribute("datum")]
+        public string Date { get; set; }
+
+        [XmlAttribute("poradi")]
+        public byte Rank { get; set; }
+    }
+
+    public partial class RateTable
+    {
+        [XmlElement("radek")]
+        public RateRow[] Rows { get; set; }
+
+        [XmlAttribute("typ")]
+        public string Type { get; set; }
+    }
+
+    public partial class RateRow
+    {
+        [XmlAttribute("kod")]
+        public string Code { get; set; }
+
+        [XmlAttribute("mena")]
+        public string Currency { get; set; }
+
+        [XmlAttribute("mnozstvi")]
+        public ushort Amount { get; set; }
+
+        [XmlAttribute("kurz")]
+        public string Rate { get; set; }
+
+        [XmlAttribute("zeme")]
+        public string Country { get; set; }
+    }
+
+}
