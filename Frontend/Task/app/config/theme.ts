47b5ef02c8c6b781a869dbf39948c76894d3b313
Daniel Zeman <ledaniel.zeman@gmail.com>
2019-06-05 17:10:54 +0200

    FE-task | theme styles setup

---
+++ Frontend/Task/app/config/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  baseSpacing: 15,
+  containerWidth: 500,
+  minWidth: 320,
+  borderColor: "#f0f0f0",
+  emphColor: "crimson",
+  transitionDuration: 300
+};
+
+export type ThemeType = typeof theme;
