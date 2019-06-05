b7521125304a1e2389cdd89d9b975702bf1943e0
Daniel Zeman <ledaniel.zeman@gmail.com>
2019-06-05 17:30:51 +0200

    FE-task | forgot to remove ts-ignore

--- Frontend/Task/app/api/api.ts
+++ Frontend/Task/app/api/api.ts
@@ -1,7 +1,6 @@
 import * as config from "../config.json";
 const url = require("url");
 
-// @ts-ignore
 export async function fetchApi<T>(endpoint: string): Promise<T> {
   try {
     const response = await fetch(`${config.url}/${endpoint}`);
