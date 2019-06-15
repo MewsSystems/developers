a20d3627d04e645f7c966e9ddec8061363fccd37
Soren ZVand <sunrise.freedom@gmail.com>
2019-06-15 13:09:11 +0430

    created ExchangeRateProviderByCnb for specific implementation.

---
+++ Backend/Task/ExchangeRateProviderByCnb.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRateUpdater
+{
+    public class ExchangeRateProviderByCnb : IExchangeRateProvider
+    {
+        public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
