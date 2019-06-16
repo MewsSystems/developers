e7a8b93fc103ff219951b7fec441f6537826136a
Soren ZVand <sunrise.freedom@gmail.com>
2019-06-16 09:20:39 +0430

    add unit test project and test CurrencyEqualityComparer

---
+++ Backend/Task/ExchangeRateUpdaterTests/CurrencyEqualityComparerFixture.cs
@@ -0,0 +1,39 @@
+﻿using ExchangeRateUpdater;
+using Xunit;
+
+namespace ExchangeRateUpdaterTests
+{
+    public class CurrencyEqualityComparerFixture
+    {
+
+        [Fact]
+        public void Same_Objects_Are_Equal()
+        {
+            // arrange 
+            var x = new Currency("USD");
+            var y = new Currency("USD");
+            var comparer = new CurrencyEqualityComparer();
+
+            // act
+            var actual = comparer.Equals(x, y);
+
+            // assert 
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Different_Objects_Not_Equal()
+        {
+            // arrange 
+            var x = new Currency("USD");
+            var y = new Currency("EUR");
+            var comparer = new CurrencyEqualityComparer();
+
+            // act
+            var actual = comparer.Equals(x, y);
+
+            // assert 
+            Assert.False(actual);
+        }
+    }
+}
