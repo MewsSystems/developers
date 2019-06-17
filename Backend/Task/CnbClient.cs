1546032352bbfe496d464e2521bf095c2f6f8dff
Fronéma Virglerová <fronema.virglerova@ictcapital.cz>
2019-06-17 13:41:25 +0200

    Solution

---
+++ Backend/Task/CnbClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater
+{
+    public static class CnbClient
+    {
+
+        private const string API_URL_FORMAT = @"https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt";
+
+        public static string GetCnbRates()
+        {
+            // Download content from CNB
+            using (var wc = new WebClient())
+            {
+                return  wc.DownloadString(API_URL_FORMAT);
+            }
+        }
+
+    }
+}
--- Backend/Task/ExchangeRateProvider.cs
+++ Backend/Task/ExchangeRateProvider.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace ExchangeRateUpdater
@@ -11,9 +13,56 @@ namespace ExchangeRateUpdater
         /// do not return exchange rate "USD/EUR" with value calculated as 1 / "EUR/USD". If the source does not provide
         /// some of the currencies, ignore them.
         /// </summary>
+        /// 
+
+        private const string CZECH_CURRENCY = "CZK";
+
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            var listCurrencies = currencies.ToList();
+
+            if (listCurrencies.Exists(c => c.Code.Equals(CZECH_CURRENCY)) == false)
+                throw new ArgumentException($"Specified currencies does not contain Czech currency({CZECH_CURRENCY})");
+
+            var cnbData = CnbClient.GetCnbRates();
+            return GetRatesFromString(cnbData, listCurrencies);
+        }
+
+
+        internal static List<ExchangeRate> GetRatesFromString(string cnbData, List<Currency> currencies)
+        {
+            if (cnbData == null) throw new ArgumentNullException(nameof(cnbData));
+            if (string.IsNullOrWhiteSpace(cnbData))
+                throw new ArgumentException("Value cannot be empty or whitespace only string.", nameof(cnbData));
+
+            // Split data files to lines
+            var lines = cnbData.Split('\n');
+            if (lines.Length < 3)
+                throw new FormatException($"Invalid data file format - too few lines. Expected at least 3, got {lines.Length}.");
+
+            // Process lines
+            var rateList = new List<ExchangeRate>();
+            for (int i = 2; i < lines.Length; i++)  // skip first two lines. Real data starts on third
+            {
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(lines[i])) continue;
+                    var data = lines[i].Split('|');
+                    if (data.Length != 5) throw new FormatException($"Expected 5 segments, got {data.Length}.");
+
+                    var rate = decimal.Parse(data[4], CultureInfo.GetCultureInfo("cs-CZ"));
+                    var amount = int.Parse(data[2]);
+
+                    if (currencies.Exists(c => c.Code.Equals(data[3])))
+                        rateList.Add(new ExchangeRate(new Currency(data[3]), new Currency(CZECH_CURRENCY), rate / amount));
+                }
+                catch (Exception e)
+                {
+                    throw new FormatException($"Invalid file format - Error on line {i + 1}: \"{lines[i]}\" cannot be parsed as exchange rate.", e);
+                }
+
+            }
+            return rateList;
         }
     }
 }
