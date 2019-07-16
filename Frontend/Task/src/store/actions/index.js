748f3d0b67c7fbbf6f80275cb7559c5d9081b8a5
andrewtomanek <47529234+andrewtomanek@users.noreply.github.com>
2019-07-16 10:35:53 +0200

    Revert "upload of project files"

--- Frontend/Task/src/store/actions/index.js
+++
@@ -1,5 +0,0 @@
-export {
-  saveTrendPairs,
-  saveConfigurationData,
-  saveFilterConfiguration
-} from "./storageActions";
--- Frontend/Task/webpack.config.js
+++ Frontend/Task/webpack.config.js
@@ -1,40 +1,34 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
 
-const config = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              modules: true
-            }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  devServer: {
-    contentBase: "./dist"
-  }
+const webpackConfig = {
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+    ],
+    entry: {
+        app: './app/app.js',
+    },
+    output: {
+        filename: '[name].js',
+        library: 'app',
+        libraryTarget: 'window',
+    },
+    resolve: {
+        extensions: ['', '.js', '.json'],
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /(node_modules|Generated)/,
+            loader: 'babel',
+        }, {
+            test: /\.json$/,
+            loader: 'json',
+        }],
+    },
+    devtool: 'eval',
+    devServer: {
+        contentBase: './app',
+    },
 };
 
-module.exports = config;
+module.exports = webpackConfig;
