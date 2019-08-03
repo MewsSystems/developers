bd29ba1416293f18c283d5c61762a7cc305127d3
prztrz <przemyslaw.trzepinski@kiwi.com>
2019-08-03 17:41:43 +0200

    [CONFIG] Added react

--- Frontend/Task/app/app.js
+++ Frontend/Task/app/app.js
@@ -1,3 +1,5 @@
-import { endpoint, intervale } from './config';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('App is running.');
+const App = () => <span>Hello world!</span>;
+ReactDOM.render(<App />, document.getElementById('exchange-rate-client'));
