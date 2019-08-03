dfa9ab0b5906981668e19e62851388d19e6a53f3
prztrz <przemyslaw.trzepinski@kiwi.com>
2019-08-03 18:19:29 +0200

    [CONFIG] added and config flow

--- Frontend/Task/app/app.js
+++ Frontend/Task/app/app.js
@@ -1,5 +1,14 @@
+// @flow strict
+
 import * as React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <span>Hello world!</span>;
-ReactDOM.render(<App />, document.getElementById('exchange-rate-client'));
+document.addEventListener('DOMContentLoaded', () => {
+  const App = () => <span>Hello world!</span>;
+
+  const appContainer = document.getElementById('exchange-rate-client');
+
+  if (appContainer) {
+    ReactDOM.render(<App />, appContainer);
+  }
+});
