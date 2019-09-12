7992e2e80dba8f2e238999d0579472aeb7f85624
Nick Skotarenko <nick.skotarenko@gmail.com>
2019-09-12 10:32:47 +0200

    Test pull request

--- Backend/Task/ExchangeRateProvider.cs
+++ Backend/Task/ExchangeRateProvider.cs
@@ -6,6 +6,7 @@ namespace ExchangeRateUpdater
     public class ExchangeRateProvider
     {
         /// <summary>
+        /// Test pull request
         /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
         /// by the source, do not return calculated exchange rates. E.g. if the source contains "EUR/USD" but not "USD/EUR",
         /// do not return exchange rate "USD/EUR" with value calculated as 1 / "EUR/USD". If the source does not provide
