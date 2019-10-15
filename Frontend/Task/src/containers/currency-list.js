eea56057712cdaf81874fb8c4cbeb7035aa9537f
Marco F <marco.friso@gmail.com>
2019-10-16 00:09:03 +0200

    feat: match configuration - rates

--- Frontend/Task/src/containers/currency-list.js
+++ Frontend/Task/src/containers/currency-list.js
@@ -28,49 +28,78 @@ export default class CurrencyList extends Component {
   getRates(configurationRates) {
     console.log('run2', configurationRates[0][0]);
 
-    const rates = configurationRates.map(key => {
+    configurationRates.forEach(key => {
       const request = {
         params: {
           currencyPairIds: [key[0]],
         },
       };
 
-      const rateColl = [];
+      // const call = async () => {
+      //   const res = await Axios.get('http://localhost:3000/rates', request, {
+      //     timeout: 10000,
+      //   });
+      //   const rate = res.data.rates;
+      //   console.log('rate', rate);
+      //   const R1 = Object.entries(res.data.rates);
+      //   const R2 = Object.keys(res.data.rates);
+      //   this.setState({ rates: [...this.state.rates, [R1, R2]] });
+      // };
+
+      // call();
 
       Axios.get('http://localhost:3000/rates', request, {
         timeout: 10000,
       })
         .then(response => {
-          rateColl.push(Object.entries(response.data.rates));
+          const rate = Object.entries(response.data.rates);
+          // const R2 = Object.keys(response.data.rates);
+          this.setState(state => {
+            const rates = [...state.rates, rate];
+            return {
+              rates,
+            };
+          });
         })
         .catch(error => console.log('error', error));
-
-      return rateColl;
-    });
-
-    console.log('RATESARRAY', rates);
-
-    this.setState({
-      rates,
     });
   }
 
   render() {
     const { configuration, rates } = this.state;
 
-    console.log('run3: render', rates);
-    const currencyCouples = configuration.map((key, i) => {
-      // const rate = rates[i] ? rates[i][0][0][1] : '';
-      console.log('RATE', rates[i]);
+    console.log('run3 - C', configuration);
+    // console.log('run3 - R', rates);
+    // console.log('run3 - C', configuration instanceof Array);
+    const currencyCouples = configuration.map(key => {
+      // const strollo = rates[i] ? rates[i][0][0][1] : 'n/c';
+      let rate = [];
+
+      if (rates) {
+        console.log('YES');
+        rates.filter(j => {
+          if (j[0][0] === key[0]) {
+            console.log('RATE-x', j);
+            rate = j[0][1];
+          }
+        });
+      }
+
+      // rates.filter(j => j[0][0] === key[0]);
+
+      // if (rates[any][0][0] === key[0]) {return rates[0][0][1]};
+      // forEach
+      console.log('KEY-C', key[0]);
+      console.log('KEY-R', rates);
       return (
         <li className="list-group-item" key={key[0]}>
-          <p>element: {i}</p>
+          <p>element: {key[0]}</p>
           <p>code1: {key[1][0].code}</p>
           <p>name1: {key[1][0].name}</p>
           <p>code2: {key[1][1].code}</p>
           <p>name2: {key[1][1].name}</p>
           <br />
-          <p>rates: {rates[i]}</p>
+          <p>rates: {rate.length !== 0 ? rate : 'n/c'}</p>
         </li>
       );
     });
