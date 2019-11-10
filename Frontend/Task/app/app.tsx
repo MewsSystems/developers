df3eb7ff51de38b7d5f3ba7c751158809790f147
Vaclav Sir <vaclav.sir@gmail.com>
2019-11-10 15:40:43 +0100

    feat: set the default font

--- Frontend/Task/app/app.tsx
+++ Frontend/Task/app/app.tsx
@@ -2,14 +2,28 @@ import { endpoints, interval } from './config.json';
 import { render } from 'react-dom';
 import { Main } from './Main';
 import * as React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+  
+  body {
+    font-family: 'Montserrat', sans-serif;
+    background: #fff;
+    color: #000;
+  }
+`;
 
 export function run(element: HTMLElement) {
   render(
-    <Main
-      configUrl={endpoints.config}
-      ratesUrl={endpoints.rates}
-      ratesRefreshMilliseconds={interval}
-    />,
+    <>
+      <GlobalStyle />
+      <Main
+        configUrl={endpoints.config}
+        ratesUrl={endpoints.rates}
+        ratesRefreshMilliseconds={interval}
+      />
+    </>,
     element,
   );
 }
