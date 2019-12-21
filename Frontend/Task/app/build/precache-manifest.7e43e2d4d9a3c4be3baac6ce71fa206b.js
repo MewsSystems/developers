c8223fdc96fc1de45dab22ccc952d14c81f1b981
Oleg <56952440+reactivedeveloper@users.noreply.github.com>
2019-12-21 23:18:16 +0300

    update task

---
+++ Frontend/Task/app/build/precache-manifest.7e43e2d4d9a3c4be3baac6ce71fa206b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7014c1602d8075ba82c649c45f5a0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b744fc9f10783ca74f",
+    "url": "/static/css/2.2c0ec7c8.chunk.css"
+  },
+  {
+    "revision": "3164fa26896efa63c08f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "32b744fc9f10783ca74f",
+    "url": "/static/js/2.f0a0b63d.chunk.js"
+  },
+  {
+    "revision": "3164fa26896efa63c08f",
+    "url": "/static/js/main.50c82c3b.chunk.js"
+  },
+  {
+    "revision": "1882455c72a948adb86b",
+    "url": "/static/js/runtime-main.4e7eaef6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
