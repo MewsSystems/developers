e968cf47faab3687f76582e65084e0d70961bec5
gortibaldik <47665799+gortibaldik@users.noreply.github.com>
2020-01-27 16:26:26 +0100

    Backend Task.

--- Backend/Task/ExchangeRateProvider.cs
+++ Backend/Task/ExchangeRateProvider.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace ExchangeRateUpdater
 {
     public class ExchangeRateProvider
     {
+        string mainCurrencies = @"https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt?";
+        string otherCurrencies = @"https://www.cnb.cz/en/financial-markets/foreign-exchange-market/fx-rates-of-other-currencies/fx-rates-of-other-currencies/fx_rates.txt?";
+
+
         /// <summary>
         /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
         /// by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
@@ -13,7 +18,55 @@ namespace ExchangeRateUpdater
         /// </summary>
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            AppendCurrentTimeToURI();
+            ExchangeRateReader rateReader = new ExchangeRateReader(mainCurrencies, currencies, "CZK");
+            List<ExchangeRate> exchangeRates = new List<ExchangeRate>();
+
+            // reading exchange rates from mainCurrencies webPage
+            AppendExchangeRates(rateReader, exchangeRates);
+
+            // reading exchange rates from otherCurrencies webPage
+            rateReader.ChangeWebPage(otherCurrencies);
+            AppendExchangeRates(rateReader, exchangeRates);
+
+            return exchangeRates;
+        }
+
+        public void AppendExchangeRates(ExchangeRateReader rateReader, List<ExchangeRate> exchangeRates)
+        {
+            // appending read exchange rates
+            ExchangeRate eR;
+            while (rateReader.Peek() != -1)
+                if ((eR = rateReader.Read()) != null)
+                    exchangeRates.Add(eR);
+        }
+
+        public void AppendCurrentTimeToURI()
+        {
+            // Exchange rates of main currencies 
+            // are updated on working days on 14:30.
+            // We set up dates so that we would get
+            // the latest exchange rates.
+            DateTime date = DateTime.Now;
+            int month = date.Month;
+            if (date.DayOfWeek == DayOfWeek.Saturday)
+                date = date.AddDays(-1);
+
+            else if (date.DayOfWeek == DayOfWeek.Sunday)
+                date = date.AddDays(-2);
+
+            else if (date.Hour < 14 || (date.Hour == 14 && date.Minute <= 30))
+                date = date.AddDays(-1);
+
+            mainCurrencies += $"date={date.Day}.{date.Month}.{date.Year}";
+
+            // Exchange rates of other currencies
+            // are updated the last day of the month.
+            if (month == date.Month)
+                date = date.AddMonths(-1);
+
+            otherCurrencies += $"year={date.Year}&month={date.Month}";
+
         }
     }
 }
---
+++ Backend/Task/ExchangeRateReader.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.IO;
+
+namespace ExchangeRateUpdater
+{
+    public class ExchangeRateReader : IDisposable
+    {
+        StreamReader streamReader;
+        IEnumerable<Currency> currencies;
+        Currency baseCode;
+        public int Peek() => streamReader.Peek();
+
+        public ExchangeRateReader(string webPage, IEnumerable<Currency> currencyCodes, string baseCode)
+        {
+            this.currencies = currencyCodes;
+            this.baseCode = IsSearchedCode(baseCode) ?? throw new ArgumentException();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Stream stream = client.GetStreamAsync(webPage).GetAwaiter().GetResult();
+                streamReader = new StreamReader(stream);
+            }
+
+            ReadHeader();
+        }
+
+        public void ReadHeader()
+        {
+            streamReader.ReadLine();
+            streamReader.ReadLine();
+        }
+
+        public ExchangeRate Read()
+        {
+            // format of the line :
+            // Country|Currency|Amount|Code|Rate
+            string[] tokens = streamReader.ReadLine().Split('|');
+            string code = tokens[3];
+
+            Currency currency = IsSearchedCode(code);
+
+            if (currency != null && decimal.TryParse(tokens[4], out decimal exchangeRate))
+            {
+                int amount = int.Parse(tokens[2]);
+
+                if (amount == 100)
+                    exchangeRate = exchangeRate / 100;
+
+                return new ExchangeRate(currency, baseCode, exchangeRate);
+            }
+
+            return null;
+        }
+
+        private Currency IsSearchedCode(string code)
+        {
+            foreach (Currency c in currencies)
+                if (c.Code == code)
+                    return c;
+
+            return null;
+        }
+
+        public void Dispose()
+        {
+            streamReader?.Dispose();
+        }
+
+        public void ChangeWebPage(string webPage)
+        {
+            streamReader.Dispose();
+
+            using (HttpClient client = new HttpClient())
+            {
+                Stream stream = client.GetStreamAsync(webPage).GetAwaiter().GetResult();
+                streamReader = new StreamReader(stream);
+
+                ReadHeader();
+            }
+        }
+    }
+}
