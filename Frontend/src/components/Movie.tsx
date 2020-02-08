f81fc00f119f2ceca777658511d063e81c463351
rafalmalenta <rmalenta@wp.pl>
2020-02-08 20:37:50 +0100

    added noimg if api endpoint have no poster_path

--- Frontend/src/components/Movie.tsx
+++ Frontend/src/components/Movie.tsx
@@ -1,11 +1,14 @@
 import link from 'react-router-dom';
 import * as React from "react";
 import styled from 'styled-components';
-
+//import * as url from '../assets/noimg.png' ;
+const noimg =require('../assets/noimg.png');
 const Div = styled.div`
 min-height: 12em;
   & >div{
-  width: 90%;  
+  padding: 8px 8px 8px 8px;
+  width: 80%;  
+  min-width: 400px;
   display: inline-block;
   vertical-align: top;
   margin-top: 0px;  
@@ -17,16 +20,25 @@ min-height: 12em;
   padding-bottom: 8px;
   display: inline-block;  
   }
-  & >image{
+  & img{
+  width: 10%;
   min-width: 92px; 
+  height: auto;
   }
 `
 
 export default function Movie(props) {
     let movie = props.movie;
+    let img=<img alt="xd"></img>;
+    if(movie.poster_path){
+        img = <img alt="Smiley face" src={`http://image.tmdb.org/t/p/w92${movie.poster_path}`} />
+    }
+    else {
+        img = <img alt="Smiley face" src={noimg} />
+    }
     return(
         <Div>
-            <img alt="Smiley face" src={`http://image.tmdb.org/t/p/w92${movie.poster_path}`} />
+            { img }
             <div>
                 <div className="title">{movie.title} </div>
                 <div className="overview">{movie.overview}</div>
