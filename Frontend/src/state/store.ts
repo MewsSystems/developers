01e0435313972dfdab71b4f6a2d8aef6b8df81b0
vojtechvportes <vojtech.v.portes@gmail.com>
2020-02-23 19:20:56 +0100

    Movies object in store persisted

--- Frontend/src/state/store.ts
+++ Frontend/src/state/store.ts
@@ -11,13 +11,18 @@ import {
 } from 'redux-persist/es/types'
 import createDebounce from 'redux-debounced'
 import { configurationInitialState } from './actions/configuration'
+import { moviesInitialState } from './actions/movies'
 
 export const STORE_VERSION = 1
 
 const migrations: MigrationManifest = {
   1: (state: PersistedState) => {
     if (state) {
-      return { ...state, configuration: configurationInitialState }
+      return {
+        ...state,
+        configuration: configurationInitialState,
+        movies: moviesInitialState,
+      }
     }
   },
 }
@@ -26,7 +31,7 @@ const persistConfig: PersistConfig<State> = {
   key: 'root',
   version: STORE_VERSION,
   storage,
-  whitelist: ['configuration'],
+  whitelist: ['configuration', 'movies'],
   migrate: createMigrate(migrations, {
     debug: process.env.NODE_ENV === 'development',
   }),
