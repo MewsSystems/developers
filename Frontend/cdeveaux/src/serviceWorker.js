a5614b828757199ca850cfc2d0468f8c2a61b136
Christophe Deveaux <christophe.deveaux@showmax.com>
2020-03-02 18:35:03 +0100

    Add eslint airbnb

--- Frontend/cdeveaux/src/serviceWorker.js
+++ Frontend/cdeveaux/src/serviceWorker.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
@@ -139,3 +140,4 @@ export function unregister() {
       });
   }
 }
+/* eslint-enable */
