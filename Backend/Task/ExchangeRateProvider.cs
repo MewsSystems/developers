dbbe921de1ffb0ec5cf0e5b51129c556fc4f7109
Atahan <atahanceylan@gmail.com>
2021-03-02 21:23:37 +0100

    First commit

--- Backend/Task/ExchangeRateProvider.cs
+++ Backend/Task/ExchangeRateProvider.cs
@@ -1,5 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace ExchangeRateUpdater
 {
@@ -13,7 +18,79 @@ namespace ExchangeRateUpdater
         /// </summary>
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            List<ExchangeRate> exchangeRateList = new List<ExchangeRate>();
+            Uri url = new Uri("https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/");
+            WebClient client = new WebClient();
+            string html = client.DownloadString(url);
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            var rows = document.DocumentNode.SelectNodes("//table[@class='currency-table']//tr");
+            if (rows != null && rows.Count > 0)
+            {
+                rows.RemoveAt(0); //remove header
+                foreach (var row in rows)
+                {
+                    var cols = row.SelectNodes("td");
+                    if (CheckColumnValidity(cols))
+                    {
+                        var isCurrencyExists = currencies.Any(c => cols[3].InnerText.Contains(c.Code));
+                        if (isCurrencyExists)
+                        {
+                            string sourceCurrency = cols[3].InnerText;
+                            decimal currencyValue;
+
+                            decimal tmpAmount;
+                            decimal? amount = decimal.TryParse((string)cols[2].InnerText, out tmpAmount) ?
+                                              tmpAmount : (decimal?)null;
+
+                            decimal tmpValue;
+                            decimal? value = decimal.TryParse((string)cols[4].InnerText, out tmpValue) ?
+                                              tmpValue : (decimal?)null;
+
+                            if (amount != null)
+                            {
+                                try
+                                {
+                                    currencyValue = (Decimal.Divide(tmpValue, (decimal)amount));
+                                }
+                                catch (Exception)
+                                {
+                                    throw;
+                                }
+                            }
+                            else
+                            {
+                                currencyValue = value == null ? 0 : (decimal)value;
+                            }
+                            exchangeRateList.Add(item: new ExchangeRate(new Currency(sourceCurrency), new Currency("CZK"), currencyValue));
+                        }
+                    }
+                }
+            }
+
+            return exchangeRateList;
+        }
+
+        /// <summary>Checks validity of columns./// </summary>
+        /// <param name="cols">Columns of currency table</param>
+        /// <returns></returns>
+        private bool CheckColumnValidity(HtmlNodeCollection cols)
+        {
+            if (cols != null && cols.Count >= 5)
+            {
+                foreach (var item in cols)
+                {
+                    if (String.IsNullOrWhiteSpace(item.InnerText))
+                        return false;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
