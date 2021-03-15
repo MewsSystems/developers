a649b7c821d71b4cfd73f8d281feab55c37fa3f8
Ivo Silva <ivfernan@redhat.com>
2021-03-15 23:27:02 +0100

    clean up

--- Frontend/src/components/Layout/styled.ts
+++ Frontend/src/components/Layout/styled.ts
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Flex, { FlexItem } from '../common/Flex';
 
@@ -27,52 +26,4 @@ export const PageFooter = styled(Flex).attrs({
   as: 'footer',
 })`
   padding: ${({ theme }) => theme.space[4]};
-`;
-
-export const NavLinks = styled.ul`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-
-  li + li {
-    margin-left: ${({ theme }) => theme.space[4]};
-  }
-
-  li:last-child {
-    margin-right: auto;
-  }
-`;
-
-export const NavLink = styled(Link)`
-  display: flex;
-  align-items: center;
-  position: relative;
-  color: ${({ theme }) => theme.colors.secondaryLight};
-  text-decoration: none;
-  text-transform: capitalize;
-
-  &::after {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 0;
-    bottom: ${({ theme }) => `-${theme.space[1]}`};
-    height: ${({ theme }) => theme.space[1]};
-    width: 0;
-    background: ${({ theme }) => theme.colors.secondaryLight};
-    transition: width 150ms linear;
-  }
-
-  &:hover::after {
-    width: 100%;
-  }
-
-  > svg {
-    height: 1.6rem;
-    padding: ${({ theme }) => theme.space[3]};
-  }
 `;
