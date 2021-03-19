6973d6ec770018fc9172441dc205e852f91d33d2
Ivo Silva <ivfernan@redhat.com>
2021-03-19 19:09:49 +0100

    Simplify LoadingState component

--- Frontend/src/components/EmptyState/LoadingState.tsx
+++ Frontend/src/components/EmptyState/LoadingState.tsx
@@ -1,23 +1,9 @@
-import { useEffect, useState } from 'react';
 import EmptyState, { EmptyStateProps } from './EmptyState';
 
-export interface LoadingStateProps extends Omit<EmptyStateProps, 'children'> {
-  delay?: number;
-}
+export interface LoadingStateProps extends Omit<EmptyStateProps, 'children'> {}
 
-function LoadingState({
-  icon,
-  title = 'Loading...',
-  delay = 300,
-}: LoadingStateProps) {
-  const [showLoading, setShowLoading] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setShowLoading(true), delay);
-    return () => clearTimeout(timer);
-  }, [delay, setShowLoading]);
-
-  return <>{showLoading && <EmptyState title={title} icon={icon} />}</>;
+function LoadingState({ icon, title = 'Loading...' }: LoadingStateProps) {
+  return <EmptyState title={title} icon={icon} />;
 }
 
 export default LoadingState;
