49bc50e12824123922497e9e37ea5c0f197bcf75
Viktoriia Petrenko <vikuacs@gmail.com>
2021-05-02 21:02:18 +0200

    Added theme & global styles

---
+++ movie-finder/src/styles/globalStyles.ts
@@ -0,0 +1,25 @@
+import {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html{
+    font-size: 16px;
+    body {
+      font-family: "Roboto", "Arial", sans-serif;
+      margin: 0;
+      padding: 0;
+      border: 0;
+      vertical-align: baseline;
+      line-height: 1;
+    }
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+  button{
+    font-size: 100%;
+    font-family: inherit;
+    border: 0;
+    padding: 0;
+  }
+`
---
+++ movie-finder/src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+    color: {
+        black: "#000000",
+        white: "#FFFFFF",
+        blue: "#1976d2",
+        grey: "#f5f5f5"
+    },
+    fontSize: {
+        xs: "",
+        small:"0.875em",
+        medium: "1em",
+        large: "1.2em",
+        xl:"",
+    },
+}
