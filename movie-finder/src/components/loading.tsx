cb9c60c51d1302fc6d8a3d2df5186ed5ceb66aeb
Viktoriia Petrenko <vikuacs@gmail.com>
2021-05-04 16:56:49 +0200

    Added loading component

---
+++ movie-finder/src/components/loading.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+
+type Props = {
+  loading?: boolean;
+  children?: never;
+};
+
+export const Loading: React.FC<Props> = (props) => {
+  const { loading } = props;
+
+  return loading ? (
+    <Loader>
+      <div data-testid={"loader"} className="lds-ring">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </Loader>
+  ) : null;
+};
+
+const Loader = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1000;
+
+  .lds-ring {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-ring div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: ${({ theme }) => `8px solid ${theme.color.white}`};
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: ${({ theme }) =>
+      `${theme.color.white} transparent transparent transparent`};
+  }
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
