0f63d028f19aa3f02d564d7480544fac862fec77
chelaton <chelaton@seznam.cz>
2021-05-06 18:50:48 +0200

    get CNB rates

--- Backend/Task/ExchangeRateProvider.cs
+++ Backend/Task/ExchangeRateProvider.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace ExchangeRateUpdater
 {
@@ -13,7 +16,63 @@ namespace ExchangeRateUpdater
         /// </summary>
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            List<ExchangeRate> exchangeRates = new List<ExchangeRate>();
+            try
+            {
+                Console.Write("Downloading exchange rates from CNB…");
+                exchangeRates = GetRatesAsync(currencies).Result;
+                Console.WriteLine("OK");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine(ex);
+            }
+            return exchangeRates;
         }
+
+        public async Task<List<ExchangeRate>> GetRatesAsync(IEnumerable<Currency> currencies)
+        {
+            // Download content from CNB
+            using (var wc = new WebClient())
+            {
+                wc.Encoding = System.Text.Encoding.UTF8;
+                var url = "https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt";
+                var dataString = await wc.DownloadStringTaskAsync(url);
+                return ParseStringToList(dataString, currencies);
+            }
+        }
+
+        public List<ExchangeRate> ParseStringToList(string ratesInSting, IEnumerable<Currency> currencies)
+        {
+            List<ExchangeRate> exchangeRates = new List<ExchangeRate>();
+            Currency targetCurrency = new Currency("CZK");
+            string aLine, aParagraph = null;
+            StringReader strReader = new StringReader(ratesInSting);
+            while (true)
+            {
+                aLine = strReader.ReadLine();
+                if (aLine != null)
+                {
+                    foreach (var currency in currencies)
+                    {
+                        if (aLine.Contains(currency.Code))
+                        {
+                            string[] pole = aLine.Split('|');
+                            Currency sourceCurrency = new Currency(pole[3]);
+                            exchangeRates.Add(new ExchangeRate(sourceCurrency, targetCurrency, Convert.ToDecimal(pole[4])));
+                        }
+                    }
+                }
+                else
+                {
+                    aParagraph = aParagraph + "\n";
+                    break;
+                }
+            }
+            return exchangeRates;
+        }
+
     }
+
 }
