70432ec4fb97e5686c47a9a0485a5a3c23fa2c23
TsuKyTsuna <76910869+TsuKyTsuna@users.noreply.github.com>
2021-07-13 15:10:00 +0200

    Update ExchangeRateProvider.cs
    ExchangeRateProvider basic implementation

--- jobs/Backend/Task/ExchangeRateProvider.cs
+++ jobs/Backend/Task/ExchangeRateProvider.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ExchangeRateUpdater
@@ -11,9 +11,59 @@ namespace ExchangeRateUpdater
         /// do not return exchange rate "USD/CZK" with value calculated as 1 / "CZK/USD". If the source does not provide
         /// some of the currencies, ignore them.
         /// </summary>
+        /// 
+
+        const string CNB_URL = "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt";
+        const char COLUMN_SEPARATOR = '|';
+
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            string downloadedDataString;
+
+            using (var webClient = new System.Net.WebClient())
+            {
+                downloadedDataString = webClient.DownloadString(CNB_URL);
+            }
+
+            return ProcessDownloadedString(downloadedDataString, currencies);
+        }
+
+        private IEnumerable<ExchangeRate> ProcessDownloadedString(string downloadedDataString, IEnumerable<Currency> currencies)
+        {
+            string line;
+
+            int readLines = 0;
+
+            var exchangeRates = new List<ExchangeRate>();
+
+            using (System.IO.StringReader reader = new System.IO.StringReader(downloadedDataString))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (readLines > 1)
+                    {
+                        var parsedLine = line.Split(COLUMN_SEPARATOR);
+
+                        var currencyCode = parsedLine[3];
+
+                        if (currencies.Any(x => x.ToString() == currencyCode))
+                        {
+                            var receivedAmount = decimal.Parse(parsedLine[2]);
+                            var requiredAmount = decimal.Parse(parsedLine[4]);
+
+                            var normalizedRate = requiredAmount / receivedAmount;
+
+                            var exchangeRate = new ExchangeRate(new Currency(currencyCode), new Currency("CZK"), normalizedRate);
+
+                            exchangeRates.Add(exchangeRate);
+                        }
+                    }
+
+                    readLines++;
+                }
+
+                return exchangeRates;
+            }
         }
     }
 }
