b21237d091f68b1271cacba9681c0966915b2485
David Müller <david.muller@mews.com>
2021-10-15 16:50:10 +0200

    migration to .NET 5

---
+++ jobs/Backend/ExchangeRateUpdaterNew/Program.cs
@@ -0,0 +1,44 @@
+﻿using ExchangeRateUpdater;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExchangeRateUpdaterNew
+{
+	class Program
+	{
+        private static IEnumerable<Currency> currencies = new[]
+        {
+            new Currency("USD"),
+            new Currency("EUR"),
+            new Currency("CZK"),
+            new Currency("JPY"),
+            new Currency("KES"),
+            new Currency("RUB"),
+            new Currency("THB"),
+            new Currency("TRY"),
+            new Currency("XYZ")
+        };
+
+        static void Main(string[] args)
+		{
+            try
+            {
+                var provider = new ExchangeRateProvider();
+                var rates = provider.GetExchangeRates(currencies);
+
+                Console.WriteLine($"Successfully retrieved {rates.Count()} exchange rates:");
+                foreach (var rate in rates)
+                {
+                    Console.WriteLine(rate.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Could not retrieve exchange rates: '{e.Message}'.");
+            }
+
+            Console.ReadLine();
+        }
+	}
+}
