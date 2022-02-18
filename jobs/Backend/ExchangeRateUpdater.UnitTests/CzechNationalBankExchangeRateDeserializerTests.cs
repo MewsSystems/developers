d7d12c7483d888d98b0d7694669f1a560e313d95
Ondrej Netocny <ondra.netocny@hotmail.com>
2022-02-18 21:25:36 +0100

    added CzechNationalBankExchangeRateDeserializer + tests

---
+++ jobs/Backend/ExchangeRateUpdater.UnitTests/CzechNationalBankExchangeRateDeserializerTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+
+namespace ExchangeRateUpdater.UnitTests
+{
+    public class CzechNationalBankExchangeRateDeserializerTests
+    {
+        private IExchangeRateDeserializer _deserializer;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _deserializer = new CzechNationalBankExchangeRateDeserializer();
+        }
+
+        [Test]
+        public void ShouldParseCorrectInput()
+        {
+            var rate = _deserializer.Deserialize("Maďarsko|forint|100|HUF|6,825");
+
+            Assert.IsNotNull(rate);
+            Assert.That(rate.SourceCurrency.Code, Is.EqualTo("HUF"));
+            Assert.That(rate.TargetCurrency.Code, Is.EqualTo("CZK"));
+            Assert.That(rate.Value, Is.EqualTo(0.06825m));
+        }
+
+        [Test]
+        public void ShouldHandleWrongSourceAmount()
+        {
+            var rate = _deserializer.Deserialize("Maďarsko|forint|XXX|HUF|6,825");
+
+            Assert.IsNull(rate);
+        }
+
+        [Test]
+        public void ShouldHandleWrongTargetAmount()
+        {
+            var rate = _deserializer.Deserialize("Maďarsko|forint|100|HUF|XXX");
+
+            Assert.IsNull(rate);
+        }
+
+        [Test]
+        public void ShouldHandleNullInput()
+        {
+            var rate = _deserializer.Deserialize(null);
+
+            Assert.IsNull(rate);
+        }
+
+        [TestCase("Maďarsko|forint|100|HUF")]
+        [TestCase("Maďarsko|forint|100|HUF|6,825|XXX")]
+        public void ShouldHandleWrongAmountOfParts(string serializedExchangeRate)
+        {
+            var rate = _deserializer.Deserialize(serializedExchangeRate);
+
+            Assert.IsNull(rate);
+        }
+    }
+}
---
+++ jobs/Backend/Task/CzechNationalBankExchangeRateDeserializer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using log4net;
+
+namespace ExchangeRateUpdater;
+
+public class CzechNationalBankExchangeRateDeserializer : IExchangeRateDeserializer
+{
+    private const string Delimiter = "|";
+    private static readonly Currency CZK = new Currency("CZK");
+
+    private readonly ILog _log;
+
+    public CzechNationalBankExchangeRateDeserializer()
+    {
+        _log = LogManager.GetLogger(GetType());
+    }
+
+    public ExchangeRate Deserialize(string serializedExchangeRate)
+    {
+        //Austrálie|dolar|1|AUD|15,453
+        if (string.IsNullOrEmpty(serializedExchangeRate))
+        {
+            _log.Warn("Serialized exchange rate is null or empty. Cannot process deserialization.");
+            return null;
+        }
+        
+        var split = serializedExchangeRate.Split(Delimiter, StringSplitOptions.RemoveEmptyEntries);
+
+        if (split.Length != 5)
+        {
+            _log.Warn($"Serialized exchange rate {serializedExchangeRate} does not contain expected number of parts.");
+            return null;
+        }
+
+
+        var sourceAmount = ParseDouble(split[2]);
+        if (!sourceAmount.HasValue)
+        {
+            _log.Warn($"Source amount {split[2]} is not valid double.");
+            return null;
+        }
+        
+        var targetAmount = ParseDouble(split[4]);
+        if (!targetAmount.HasValue)
+        {
+            _log.Warn($"Target amount {split[4]} is not valid double.");
+            return null;
+        }
+
+        if (string.IsNullOrEmpty(split[3]))
+        {
+            _log.Warn("Current code is null or empty.");
+            return null;
+        }
+
+        var sourceCurrency = new Currency(split[3]);
+        var rate = Convert.ToDecimal(targetAmount / sourceAmount);
+
+        return new ExchangeRate(sourceCurrency, CZK, rate);
+    }
+
+    private double? ParseDouble(string input)
+    {
+        if (!double.TryParse(input.Replace(',', '.'), NumberStyles.Any, CultureInfo.InvariantCulture, out var result))
+        {
+            return null;
+        }
+        return result;
+    }
+}
