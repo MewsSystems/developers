37ccc70810772ff9ab66620cc7054d65c285e237
Dan <brezoi_dan@yahoo.com>
2022-05-19 08:19:57 +0100

    Adding a WebAPI to retrieve the data

---
+++ jobs/Backend/ExchangeRateUpdater.API/Controllers/ExchangeRate.cs
@@ -0,0 +1,33 @@
+﻿using ExchangeRateUpdater.RatesReader;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRateUpdater.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeRate : ControllerBase
+    {
+        private readonly IAllCurrentRatesReaderService ratesReaderService;
+
+        public ExchangeRate(IAllCurrentRatesReaderService ratesReaderService)
+        {
+            this.ratesReaderService = ratesReaderService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetExchangeRate()
+        {
+            var ratesResult = await ratesReaderService.GetAllExchangeRates();
+            if (ratesResult.Succsess)
+            {
+                if (!ratesResult.Value.Any())
+                {
+                    return new NotFoundResult();
+                }
+                return Ok(ratesResult.Value.Select(r => r.ToString()));
+            }
+            var failureText = ratesResult.FailureResons.Aggregate((partialErrorText, failure) => $"{partialErrorText} {failure} {Environment.NewLine}");
+            return Problem(failureText);
+        }
+    }
+}
---
+++ jobs/Backend/ExchangeRateUpdater.API/Program.cs
@@ -0,0 +1,49 @@
+using ExchangeRateUpdater.RatesReader;
+using Polly;
+using Polly.Extensions.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient<IAllCurrentRatesReaderService, AllCurrentRatesReaderService>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["CNBUri"]);
+})
+    .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+    .AddPolicyHandler(GetRetryPolicy())
+    .AddPolicyHandler(GetCircuitBreakerPolicy());
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+        .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2,
+                                                                    retryAttempt)));
+}
+
+static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
