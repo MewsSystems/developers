a263a32dd7183c4e632df347327855151aabaa35
Dora Lillis <dora.lillis@outlook.com>
2022-06-11 12:07:18 +0200

    pass returns exchange rates for defined specified currencies when some not defined:

--- Mews.BackendDeveloperTask/Mews.BackendDeveloperTask.ExchangeRates.UnitTests/ExchangeRateProviderTests.cs
+++ Mews.BackendDeveloperTask/Mews.BackendDeveloperTask.ExchangeRates.UnitTests/ExchangeRateProviderTests.cs
@@ -51,10 +51,25 @@ public class ExchangeRateProviderTests
     }
 
     [Test]
-    public void ReturnsExchangeRatesForDefinedSpecifiedCurrenciesWhenSomeNotDefinedBySource()
+    public async Task ReturnsExchangeRatesForDefinedSpecifiedCurrenciesWhenSomeNotDefinedBySource()
     {
         // Should return exchange rates among the specified currencies that are defined by the source. But only those defined by the source
-        throw new NotImplementedException();
+        // Arrange
+        var mockDataSource = new Mock<IExchangeRateDataSource>();
+        mockDataSource.Setup(s => s.GetExchangeRatesAsync()).ReturnsAsync(new[] {
+            new ExchangeRate(Currency.USD, Currency.CZK, 23.49f)
+        });
+        var exchangeRateProvider = new ExchangeRateProvider(mockDataSource.Object);
+
+        // Act
+        var specifiedCurrencies = new[] { Currency.USD, Currency.EUR };
+        var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(specifiedCurrencies);
+
+        // Assert
+        var expectedExchangeRates = new[] {
+            new ExchangeRate(Currency.USD, Currency.CZK, 23.49f)
+        };
+        Assert.AreEqual(expectedExchangeRates.OrderBy(o => o.Source), actualExchangeRates.OrderBy(o => o.Source));
     }
 
     [Test]
