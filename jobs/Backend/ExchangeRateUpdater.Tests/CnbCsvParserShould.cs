83687305db1466c7c9ad3d182560ff62b5db85ec
Nikolay Zdravkov <unsafeptr@gmail.com>
2022-06-18 20:25:54 +0300

    - add more tests

--- jobs/Backend/ExchangeRateUpdater.Tests/CnbCsvParserShould.cs
+++ jobs/Backend/ExchangeRateUpdater.Tests/CnbCsvParserShould.cs
@@ -10,16 +10,41 @@ namespace ExchangeRateUpdater.Tests
     public class CnbCsvParserShould
     {
         [TestMethod]
-        public void ParseSample()
+        public void ParseSample_With_Valid_data_ReturnsOk()
         {
             var cnbCsvParser = new CnbCsvParser();
 
-            var sample = File.Open("./Samples/17June.txt", FileMode.Open);
+            var sample = File.Open("./Samples/17June.txt", FileMode.Open, FileAccess.Read);
 
             var result = cnbCsvParser.TryParseExchangeRates(sample);
 
             result.IsSuccess.Should().BeTrue();
             result.Value.Count().Should().Be(32);
+        }
+
+        [TestMethod]
+        public void ParseSample_WithClosedStream_Returns_Failure()
+        {
+            var cnbCsvParser = new CnbCsvParser();
+
+            var streamWriter = new MemoryStream();
+            streamWriter.Close(); // intentionally close stream
+
+            var result = cnbCsvParser.TryParseExchangeRates(streamWriter);
+
+            result.IsFailed.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ParseSample_With_InvalidData_Returns_Failure()
+        {
+            var cnbCsvParser = new CnbCsvParser();
+
+            var sample = File.Open("./Samples/InvalidData.txt", FileMode.Open, FileAccess.Read);
+
+            var result = cnbCsvParser.TryParseExchangeRates(sample);
+
+            result.IsFailed.Should().BeTrue();
         }
     }
 }
