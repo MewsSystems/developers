d040f48735598ae7842863314453776d4ef8bfe4
Senem Unal <senem@omniaretail.com>
2022-06-24 09:44:44 +0200

    Add summary to settings parameters

--- jobs/Backend/Task/src/ExchangeRateUpdater.Contracts/IExchangeRateServiceSettings.cs
+++ jobs/Backend/Task/src/ExchangeRateUpdater.Contracts/IExchangeRateServiceSettings.cs
@@ -2,12 +2,29 @@ namespace ExchangeRateUpdater.Contracts;
 
 public interface IExchangeRateServiceSettings
 {
+    /// <summary> Url that exchange rates will be pulled from </summary>
     string BaseUrl { get; }
+
+    /// <summary> Timezone id where exchange rate data belongs </summary>
     string TimezoneId { get; }
+
+    /// <summary> Target currency </summary>
     string DefaultCurrency { get; }
+
+    /// <summary> Delimiter to separate the lines of exchange rate response </summary>
     string MappingDelimiter { get; }
+
     string MappingDecimalSeparator { get; }
+
+    /// <summary> If false, mapping exceptions will be ignored and not added to response of exchange rates </summary>
     bool ThrowExceptionOnMappingErrors { get; }
+
+    /// <summary> Determines if in memory cache will be used </summary>
     bool UseInMemoryCache { get; }
+
+    /// <summary>
+    /// Time to be added to current day for cache expiration
+    /// If current time is already bigger than the expiry time of day, then expiration will be calculated for next day
+    /// </summary>
     TimeSpan CacheExpiryTime { get; }
 }
