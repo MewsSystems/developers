d0fe9fac088b79b108eb4c55a74a75b06898ffaa
reiandre <andre@omniaretail.com>
2022-08-07 21:02:18 +0200

    Add the ExchangeRateUpdater.Service project with the ExchangeRatesSearcherService class

---
+++ jobs/Backend/Task/src/ExchangeRateUpdater.Service/ExchangeRatesSearcherService.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Domain.Ports;
+
+namespace ExchangeRatesSearcherService;
+
+public class ExchangeRatesSearcherService : IExchangeRatesSearcher
+{
+    public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
+    {
+        throw new NotImplementedException();
+    }
+}
