4cef07038e8e0f2446233d1e1a01ce368ae6929f
reiandre <andre@omniaretail.com>
2022-08-08 16:15:39 +0200

    Add the IExchangeRateInMemoryCache port to domain

---
+++ jobs/Backend/Task/src/ExchangeRateUpdater.Domain/Ports/IExchangeRateInMemoryCache.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Ports;
+
+public interface IExchangeRateInMemoryCache
+{
+    IEnumerable<ExchangeRate>? GetCache(string key);
+    void SetCache(string key, object exchangeRates);
+}
