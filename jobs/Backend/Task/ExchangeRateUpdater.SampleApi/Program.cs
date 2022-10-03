4e7c4cfa50793f186e6168ec0bca911876b74786
João Gomes <joaonuno.gomes@farfetch.com>
2022-10-03 17:58:37 +0100

    feat: update dependency injection middlewares

--- jobs/Backend/Task/ExchangeRateUpdater.SampleApi/Program.cs
+++ jobs/Backend/Task/ExchangeRateUpdater.SampleApi/Program.cs
@@ -1,4 +1,4 @@
-using ExchangeRateUpdater.SampleApi.Configuration;
+using ExchangeRateUpdater.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDependencies();
+builder.Services.AddExchangeRateProviders();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ jobs/Backend/Task/src/ExchangeRateProviders/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ExchangeRateUpdater.Providers.Providers;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace ExchangeRateUpdater.Providers
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddExchangeRateProviders(this IServiceCollection services)
+        {
+            services
+                .AddHttpClient("httpClient");
+
+            services
+                .AddScoped<IExchangeRateProvider>(c =>
+                new CzechNationalBankExchangeRateProvider(
+                    c.GetRequiredService<IHttpClientFactory>().CreateClient("httpClient")));
+
+            return services;
+        }
+    }
+}
