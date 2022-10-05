11ef6e13cb7a5b0758b2878134901fa5717fb9a6
Graham Griffiths <graham.griffiths@web.com>
2022-10-05 21:32:25 +0100

    MBDT-3 ::  Task D/E - add scaffolding for tests

---
+++ jobs/Backend/Common.UnitTests/Csv/CsvWrapperTests.cs
@@ -0,0 +1,20 @@
+﻿using Common.Csv;
+using Moq;
+
+namespace Common.UnitTests.Csv
+{
+    public class CsvWrapperTests
+    {
+        private readonly Mock<ICsvWrapper> _mockCsvWrapper;
+
+        public CsvWrapperTests()
+        {
+            _mockCsvWrapper = new Mock<ICsvWrapper>();
+        }
+
+        public class Csv : CsvWrapperTests
+        {
+            // TODO: Add CSV tests
+        }
+    }
+}
---
+++ jobs/Backend/Core.UnitTests/Parser/ResponseParserTests.cs
@@ -0,0 +1,20 @@
+﻿using Core.Parser;
+using Moq;
+
+namespace Core.UnitTests.Parser
+{
+    public class ResponseParserTests
+    {
+        private readonly Mock<IResponseParser> _mockResponseParser;
+
+        public ResponseParserTests()
+        {
+            _mockResponseParser = new Mock<IResponseParser>();
+        }
+
+        public class ResponseParser : ResponseParserTests
+        {
+            // TODO: Add ResponseParserTests tests
+        }
+    }
+}
