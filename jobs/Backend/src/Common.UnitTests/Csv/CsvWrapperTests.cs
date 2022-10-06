c1e701628a7de8fd6164598bcb321bc0b3e990f4
Graham Griffiths <graham.griffiths@web.com>
2022-10-06 10:48:55 +0100

    MBDT-3 :: Task E - Test coverage
    Coverage on csv wrapper

--- jobs/Backend/src/Common.UnitTests/Csv/CsvWrapperTests.cs
+++ jobs/Backend/src/Common.UnitTests/Csv/CsvWrapperTests.cs
@@ -1,20 +1,66 @@
 ﻿using Common.Csv;
-using Moq;
+using CsvHelper.Configuration.Attributes;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
 
 namespace Common.UnitTests.Csv
 {
     public class CsvWrapperTests
     {
-        private readonly Mock<ICsvWrapper> _mockCsvWrapper;
+        private readonly ICsvWrapper _objectToTest;
+
+        private string testCsvData = "04 Oct 2022 #192\n\rCountry|Currency|Amount|Code|Rate\n\rAustralia|dollar|1|AUD|16.027\n\rBrazil|real|1|BRL|4.854";
+
+        private class TestCsvModel
+        {
+            [Name("Country")]
+            public string Country { get; set; }
+            [Name("Currency")]
+            public string Currency { get; set; }
+            [Name("Amount")]
+            public int Amount { get; set; }
+            [Name("Code")]
+            public string Code { get; set; }
+            [Name("Rate")]
+            public decimal Rate { get; set; }
+        }
 
         public CsvWrapperTests()
         {
-            _mockCsvWrapper = new Mock<ICsvWrapper>();
+            _objectToTest = new CsvWrapper();
         }
 
         public class Csv : CsvWrapperTests
         {
-            // TODO: Add CSV tests
+            [Theory]
+            [InlineData("|")]
+            public void Returns_Data_If_Correct_Delimitter(string delimitter)
+            {
+                // Arrange
+
+                // Act
+                var actual = _objectToTest.ParseCsv<TestCsvModel>(testCsvData, delimitter, true, true);
+
+                // Assert
+                actual.Should().HaveCountGreaterThan(0);
+            }
+
+            [Theory]
+            [InlineData(",")]
+            public void Throws_Exception_If_Wrong_Delimitter(string delimitter)
+            {
+                // Arrange
+
+                // Act
+                Func<object> action = () => _objectToTest.ParseCsv<TestCsvModel>(testCsvData, delimitter, true, true);
+
+                // Assert
+                using (new AssertionScope())
+                {
+                    action.Should().Throw<Exception>();
+                }
+            }
         }
     }
 }
