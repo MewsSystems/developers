454dc1d8918e70149777698c697db23f99fc98c6
Mickaël Derriey <mderriey@gmail.com>
2022-10-09 07:48:20 +0200

    Use file-scoped namespaces and expression body for methods

--- jobs/Backend/Task/ExchangeRateUpdater/ExchangeRate.cs
+++ jobs/Backend/Task/ExchangeRateUpdater/ExchangeRate.cs
@@ -1,23 +1,19 @@
-﻿namespace ExchangeRateUpdater
+namespace ExchangeRateUpdater;
+
+public class ExchangeRate
 {
-    public class ExchangeRate
+    public ExchangeRate(Currency sourceCurrency, Currency targetCurrency, decimal value)
     {
-        public ExchangeRate(Currency sourceCurrency, Currency targetCurrency, decimal value)
-        {
-            SourceCurrency = sourceCurrency;
-            TargetCurrency = targetCurrency;
-            Value = value;
-        }
-
-        public Currency SourceCurrency { get; }
-
-        public Currency TargetCurrency { get; }
-
-        public decimal Value { get; }
-
-        public override string ToString()
-        {
-            return $"{SourceCurrency}/{TargetCurrency}={Value}";
-        }
+        SourceCurrency = sourceCurrency;
+        TargetCurrency = targetCurrency;
+        Value = value;
     }
+
+    public Currency SourceCurrency { get; }
+
+    public Currency TargetCurrency { get; }
+
+    public decimal Value { get; }
+
+    public override string ToString() => $"{SourceCurrency}/{TargetCurrency}={Value}";
 }
