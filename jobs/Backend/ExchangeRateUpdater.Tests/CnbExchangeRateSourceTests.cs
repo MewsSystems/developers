cf94352239e15a55e6ce6a1acc8d164790088f1d
Jonas Pauliukevicius <pauliukevicius.jonas@gmail.com>
2022-10-22 16:16:48 +0200

    add CnbExchangeRateSource and init test

---
+++ jobs/Backend/ExchangeRateUpdater.Tests/CnbExchangeRateSourceTests.cs
@@ -0,0 +1,25 @@
+namespace ExchangeRateUpdater.Tests
+{
+    public class CnbExchangeRateSourceTests
+    {
+        private CnbExchangeRateSource _exchangeRateSource;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new CnbExchangeRateSource.Options
+            {
+                Url = "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt"
+            };
+
+            _exchangeRateSource = new CnbExchangeRateSource(options);
+        }
+
+        [Test]
+        public async Task GetRates()
+        {
+            var rateInfo = await _exchangeRateSource.GetLatestExchangeRatesAsync();
+            Console.WriteLine(rateInfo);
+        }
+    }
+}
---
+++ jobs/Backend/Task/CnbExchangeRateSource.cs
@@ -0,0 +1,30 @@
+﻿using Flurl.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater
+{
+    public class CnbExchangeRateSource
+    {
+        private readonly string _url;
+
+        public CnbExchangeRateSource(Options options)
+        {
+            _url = options.Url ?? throw new ArgumentNullException(nameof(Options.Url));
+        }
+
+        public async Task<string> GetLatestExchangeRatesAsync()
+        {
+            var content = await _url.GetStringAsync();
+            return content;
+        }
+
+        public record Options
+        {
+            public string Url { get; set; }
+        }
+    }
+}
