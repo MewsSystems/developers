308711878fcd3137f3e51b6739c241afdc9e35cc
Jonas Pauliukevicius <pauliukevicius.jonas@gmail.com>
2022-10-23 17:31:37 +0200

    add rate age metric

--- jobs/Backend/Task/ExchangeRateUpdater/ExchangeRateProvider.cs
+++ jobs/Backend/Task/ExchangeRateUpdater/ExchangeRateProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ExchangeRateUpdater.Cnb;
@@ -26,10 +27,22 @@ namespace ExchangeRateUpdater
         public async Task<IEnumerable<ExchangeRate>> GetExchangeRates(IEnumerable<Currency> currencies)
         {
             var dailyRates = await _cnbClient.GetLatestExchangeRatesAsync();
+
+            CountExchangeRatesAgeMetric(dailyRates.Date);
+
             var mapped = dailyRates.Rates
                 .Select(_cnbMapper.MapExchangeRate)
                 .ToArray();
                 return mapped.Where(r => currencies.Any(c => c.Code == r.SourceCurrency.Code));
+        }
+
+        private void CountExchangeRatesAgeMetric(DateOnly exchangeRateDate)
+        {
+            // Here we can publish the time difference between current date and exchangeRateDate to our metrics store.
+            // Alerting stale exchange rates is not trivial because they are not published on weekends and bank holidays.
+            // If business is not particularly sensitive to it, a simple rule of "rates must be at most 7 days old" would
+            // suffice. Otherwise, tracking bank holidays (https://www.cnb.cz/en/public/media-service/schedules-and-other-info/bank-holidays-in-the-czech-republic)
+            // should be implemented and the staleness counted since the last working day.
         }
     }
 }
