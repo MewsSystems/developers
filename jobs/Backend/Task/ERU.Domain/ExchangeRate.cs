f3fac3cb0f4f2be6ab0aa8cc8a1600033717a182
kule <kiril.elizarov@gmail.com>
2022-11-06 17:08:02 +0100

    domain models - refactoring

--- jobs/Backend/Task/ERU.Domain/ExchangeRate.cs
+++ jobs/Backend/Task/ERU.Domain/ExchangeRate.cs
@@ -1,23 +1,9 @@
-﻿namespace ERU.Domain
+﻿namespace ERU.Domain;
+
+public record ExchangeRate(Currency SourceCurrency, Currency TargetCurrency, decimal Value)
 {
-    public class ExchangeRate
-    {
-        public ExchangeRate(Currency sourceCurrency, Currency targetCurrency, decimal value)
-        {
-            SourceCurrency = sourceCurrency;
-            TargetCurrency = targetCurrency;
-            Value = value;
-        }
-
-        public Currency SourceCurrency { get; }
-
-        public Currency TargetCurrency { get; }
-
-        public decimal Value { get; }
-
-        public override string ToString()
-        {
-            return $"{SourceCurrency}/{TargetCurrency}={Value}";
-        }
-    }
+	public override string ToString()
+	{
+		return $"{SourceCurrency}/{TargetCurrency}={Value}";
+	}
 }
