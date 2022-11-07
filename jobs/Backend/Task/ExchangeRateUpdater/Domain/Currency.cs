5eae005ae711f336aca66be8b68bfeeced14c766
Alexandr Tsvetkov <tsvetkov@uget.cz>
2022-11-07 20:50:19 +0100

    Moved files

--- jobs/Backend/Task/ExchangeRateUpdater/Domain/Currency.cs
+++ jobs/Backend/Task/ExchangeRateUpdater/Domain/Currency.cs
@@ -25,26 +25,23 @@ public class Currency : IEquatable<Currency>
         return Code;
     }
 
-    public bool Equals(Currency other)
+    public bool Equals(Currency? other)
     {
         if (ReferenceEquals(null, other)) return false;
         if (ReferenceEquals(this, other)) return true;
-
         return Code == other.Code;
     }
 
-    public override bool Equals(object obj)
+    public override bool Equals(object? obj)
     {
         if (ReferenceEquals(null, obj)) return false;
         if (ReferenceEquals(this, obj)) return true;
-
         if (obj.GetType() != this.GetType()) return false;
-
         return Equals((Currency) obj);
     }
 
     public override int GetHashCode()
     {
-        return Code != null ? Code.GetHashCode() : 0;
+        return Code.GetHashCode();
     }
 }
