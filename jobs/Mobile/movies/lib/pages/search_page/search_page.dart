1b3786e6263d1988436b1f5f5c08e0450f99da5b
Viktor Kirjanov <viktor.kirjanov@gmail.com>
2022-12-01 17:21:09 +0100

    feature: add FloatingActionButton scroll up

--- jobs/Mobile/movies/lib/pages/search_page/search_page.dart
+++ jobs/Mobile/movies/lib/pages/search_page/search_page.dart
@@ -22,7 +22,8 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   late SearchBloc _searchBloc;
 
-  final TextEditingController _textEditingController = TextEditingController();
+  final _textEditingController = TextEditingController();
+  final _scrollController = ScrollController();
 
   @override
   void initState() {
@@ -66,6 +67,7 @@ class _SearchPageState extends State<SearchPage> {
                       return RefreshIndicator(
                         color: CustomTheme.blue,
                         child: ListView.builder(
+                          controller: _scrollController,
                           padding: const EdgeInsets.only(top: 60.0),
                           itemCount: _movies.length + 1,
                           itemBuilder: (_, index) {
@@ -130,11 +132,14 @@ class _SearchPageState extends State<SearchPage> {
             ],
           ),
         ),
-        // TODO(Viktor): add floatingActionButton
-        // floatingActionButton: FloatingActionButton(
-        //   backgroundColor: CustomTheme.grey1.withAlpha(75),
-        //   child: const Icon(Icons.arrow_upward_rounded),
-        //   onPressed: () {},
-        // ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: CustomTheme.grey.withAlpha(75),
+          child: const Icon(Icons.arrow_upward_rounded),
+          onPressed: () => _scrollController.animateTo(
+            0,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.fastOutSlowIn,
+          ),
+        ),
       );
 }
