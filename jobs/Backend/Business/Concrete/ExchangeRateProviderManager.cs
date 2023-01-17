a03336fab7525630e83c9064f997e169a98f9bc1
AyseErd <aysserdogan@gmail.com>
2023-01-17 14:54:52 +0300

    Validation logic replaced under common lib for modularity

--- jobs/Backend/Business/Concrete/ExchangeRateProviderManager.cs
+++ jobs/Backend/Business/Concrete/ExchangeRateProviderManager.cs
@@ -1,16 +1,15 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Common.CrossCuttingConcerns.Validation;
 using Common.Results;
 using ExchangeRateUpdater;
-using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ValidationException = FluentValidation.ValidationException;
 
 namespace Business.Concrete
 {
@@ -18,13 +17,7 @@ namespace Business.Concrete
     {
         public IDataResult<IEnumerable<ExchangeRate>> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            var validator = new ValidationContext<IEnumerable<Currency>>(currencies);
-            CurrencyListValidator clValidator= new CurrencyListValidator(); 
-           var result= clValidator.Validate(validator);
-            if (!result.IsValid)
-            {
-                throw new ValidationException(result.Errors);
-            }
+            CommonValidationTool.Validate(new CurrencyListValidator(),currencies);
            return new DataResult<IEnumerable<ExchangeRate>>(null,true, Messages.ExchangeRatesObtained);
         }
     }
---
+++ jobs/Backend/Common/CrossCuttingConcerns/Validation/CommonValidationTool.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace Common.CrossCuttingConcerns.Validation
+{
+    public class CommonValidationTool
+    {
+        public static void Validate(IValidator validator,object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+           
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
