7e69a1345cbc329d354467dddf9f16a5ddee40c3
AyseErd <aysserdogan@gmail.com>
2023-01-17 14:42:31 +0300

    -added fluentvalidation for currency&currencylist

---
+++ jobs/Backend/Business/ValidationRules/FluentValidation/CurrencyListValidator.cs
@@ -0,0 +1,19 @@
+﻿using Entities.ValidationRules.FluentValidation;
+using ExchangeRateUpdater;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CurrencyListValidator:AbstractValidator<IEnumerable<Currency>>
+    {
+        public CurrencyListValidator()
+        {
+            RuleForEach(x => x).SetValidator(new CurrencyValidator());
+        }
+    }
+}
---
+++ jobs/Backend/Entities/ValidationRules/FluentValidation/CurrencyValidator.cs
@@ -0,0 +1,20 @@
+﻿using ExchangeRateUpdater;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ValidationRules.FluentValidation
+{
+    public class CurrencyValidator:AbstractValidator<Currency>
+    {
+        public CurrencyValidator()
+        {
+            RuleFor(c => c.Code).NotEmpty();
+            RuleFor(c => c.Code).NotNull();
+            RuleFor(c => c.Code).Length(3).WithMessage("Currency code must include 3 letters"); ;       
+        }
+    }
+}
