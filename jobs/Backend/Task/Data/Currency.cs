1bf1a658a094b66dc33ce97f074ded79de5402d7
Martin Zemek <zemek.m@gmail.com>
2023-02-17 12:26:23 +0100

    git mv original Currency, ExchangeRate

--- jobs/Backend/Task/Data/Currency.cs
+++ jobs/Backend/Task/Data/Currency.cs
@@ -1,19 +1,20 @@
-﻿namespace ExchangeRateUpdater.Data;
-
-public sealed record Currency
+﻿namespace ExchangeRateUpdater
 {
-    public Currency(string code)
+    public class Currency
     {
-        Code = code;
-    }
+        public Currency(string code)
+        {
+            Code = code;
+        }
 
-    /// <summary>
-    /// Three-letter ISO 4217 code of the currency.
-    /// </summary>
-    public string Code { get; }
+        /// <summary>
+        /// Three-letter ISO 4217 code of the currency.
+        /// </summary>
+        public string Code { get; }
 
-    public override string ToString()
-    {
-        return Code;
+        public override string ToString()
+        {
+            return Code;
+        }
     }
 }
