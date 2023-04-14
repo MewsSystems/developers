fa0179745b1784c4f174b103acc2c98357a21ece
Ben Abdallah Oussama <benabdalla.oussama@gmail.com>
2023-04-14 02:33:26 +0200

    Added CachedCzechExchangeRateProviderTests.

---
+++ jobs/Backend/Task/ExchangeRateUpdater.Tests/CachedCzechExchangeRateProviderTests.cs
@@ -0,0 +1,46 @@
+﻿using ExchangeRateUpdater.Interfaces;
+using ExchangeRateUpdater.Models;
+using ExchangeRateUpdater.Services.Cached;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Tests
+{
+    public class CachedCzechExchangeRateProviderTests
+    {
+        [Fact]
+        public async Task GetExchangeRates_ReturnsCorrectRatesAndUsesCache()
+        {
+            // Arrange
+            var currencies = new[] { new Currency("USD"), new Currency("EUR") };
+            var expectedRates = new[]
+            {
+                new ExchangeRate(new Currency("USD"), new Currency("CZK"), 24.000m),
+                new ExchangeRate(new Currency("EUR"), new Currency("CZK"), 26.000m)
+            };
+
+            var providerMock = new Mock<IExchangeRateProvider>();
+            providerMock.Setup(x => x.GetExchangeRates(currencies)).ReturnsAsync(expectedRates);
+
+            var clockMock = new Mock<IClock>();
+            clockMock.SetupGet(x => x.Today).Returns(DateOnly.Parse("2023-04-12"));
+
+            var memoryCache = new MemoryCache(new MemoryCacheOptions());
+            var cachedProvider = new CachedCzechExchangeRateProvider(providerMock.Object, clockMock.Object, memoryCache);
+
+            // Act
+            var rates1 = await cachedProvider.GetExchangeRates(currencies);
+            var rates2 = await cachedProvider.GetExchangeRates(currencies);
+
+            // Assert
+            Assert.True(expectedRates.SequenceEqual(rates1));
+            Assert.True(expectedRates.SequenceEqual(rates2));
+            providerMock.Verify(x => x.GetExchangeRates(currencies), Times.Once); // Ensure caching is used
+        }
+    }
+}
