3b6f8ab426932382629899fed968408132018d86
Sam Douglas <sdouglas.contact@gmail.com>
2023-04-23 08:17:25 +0100

    Create CnbExchangeRateSource
    To be used as exchange rate source

---
+++ jobs/Backend/Task/ExchangeRateSource/CnbExchangeRateSource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRateUpdater.Models;
+
+namespace ExchangeRateUpdater.ExchangeRateSource
+{
+    internal class CnbExchangeRateSource : IExchangeRateSource
+    {
+        private const string CzechCurrencyCode = "CNZ";
+        private const string CzechNationalBankBaseUrl = "https://api.cnb.cz/cnbapi/";
+        private const string CzechNationalBankExchangeRateEndpoint = "exrates/daily";
+        public CnbExchangeRateSource()
+        {
+            CurrencyCode = new Currency(CzechCurrencyCode);
+            BaseRestUrl = CzechNationalBankBaseUrl;
+            EndPoint = CzechNationalBankExchangeRateEndpoint;
+        }
+        public Currency CurrencyCode { get; }
+        public string BaseRestUrl { get; }
+        public string EndPoint { get; set; }
+    }
+}
---
+++ jobs/Backend/Task/ExchangeRateSource/IExchangeRateSource.cs
@@ -0,0 +1,10 @@
+﻿using ExchangeRateUpdater.Models;
+
+namespace ExchangeRateUpdater.ExchangeRateSource;
+
+public interface IExchangeRateSource
+{
+    Currency CurrencyCode { get; }
+    string BaseRestUrl { get; }
+    string EndPoint { get; set; }
+}
