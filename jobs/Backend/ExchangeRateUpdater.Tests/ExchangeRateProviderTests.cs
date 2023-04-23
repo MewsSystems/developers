7af14874c76a2ee860ed95e5e5c9b80e10e1b279
Sam Douglas <sdouglas.contact@gmail.com>
2023-04-23 09:54:04 +0100

    Added test to check if any exchange rates not requested are returned

--- jobs/Backend/ExchangeRateUpdater.Tests/ExchangeRateProviderTests.cs
+++ jobs/Backend/ExchangeRateUpdater.Tests/ExchangeRateProviderTests.cs
@@ -1,15 +1,62 @@
+using System.Linq;
+
 namespace ExchangeRateUpdater.Tests;
 
-public class Tests
+[TestFixture]
+public class ExchangeRateProviderTests
 {
+
+    private Mock<IExchangeRateClient> _mockExchangeRateClient;
+    private Mock<IExchangeRateSource> _mockExchangeRateSource;
+
     [SetUp]
     public void Setup()
     {
+        _mockExchangeRateClient = new Mock<IExchangeRateClient>();
+        _mockExchangeRateSource = new Mock<IExchangeRateSource>();
     }
 
     [Test]
-    public void Test1()
+    public async Task CnbExchangeRateProvider_ReturnsOnlyRequiredExchangeRates()
     {
-        Assert.Pass();
+        // Arrange
+        string mockExchangeRateClientResult = @"{""rates"":[
+                {
+                ""currencyCode"":""USD"",
+                ""rate"":123.0
+                },
+                {
+                ""currencyCode"":""EUR"",
+                ""rate"":321.0
+                },
+                {
+                ""currencyCode"":""FAKE"",
+                ""rate"":999.0
+                }
+            ]}";
+        _mockExchangeRateClient.Setup(e => e.GetExchangeRateAsync()).ReturnsAsync(mockExchangeRateClientResult);
+        _mockExchangeRateSource.SetupGet(e => e.CurrencyCode).Returns(new Currency("TEST"));
+        var exchangeRateProvider =
+            new CnbExchangeRateProvider(_mockExchangeRateClient.Object, _mockExchangeRateSource.Object);
+
+        IEnumerable<Currency> currencies = new[]
+        {
+            new Currency("USD"),
+            new Currency("TEST"),
+            new Currency("EUR")
+        };
+
+        // Act
+        var results = exchangeRateProvider.GetExchangeRates(currencies);
+        var resultsList = new List<ExchangeRate>();
+        
+        await foreach (var exchangeRate in results)
+        {
+            resultsList.Add(exchangeRate);
+        }
+
+        // Assert
+        Assert.That(resultsList.All(result => currencies.Any(c => c.Code == result.SourceCurrencyCode)), Is.True); // Checks if any exchange rates that aren't in the provided list are returned
+
     }
 }
--- jobs/Backend/ExchangeRateUpdater.Tests/Usings.cs
+++ jobs/Backend/ExchangeRateUpdater.Tests/Usings.cs
@@ -1,2 +1,6 @@
 global using NUnit.Framework;
 global using Moq;
+global using ExchangeRateUpdater.ExchangeRateSource;
+global using ExchangeRateUpdater.Client;
+global using ExchangeRateUpdater.Models;
+global using ExchangeRateUpdater.Providers;
