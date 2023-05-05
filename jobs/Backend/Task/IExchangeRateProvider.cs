5a7c3249df09e9e0a288a2d36a6956803e7d24ad
Grant Shaw <grantshaw@hotmail.co.uk>
2023-05-05 15:41:24 +0100

    changed to file scope namespace for consistency.

--- jobs/Backend/Task/IExchangeRateProvider.cs
+++ jobs/Backend/Task/IExchangeRateProvider.cs
@@ -1,11 +1,10 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace ExchangeRateUpdater
-{
-    public interface IExchangeRateProvider
-    {
-    public Task <IEnumerable<ExchangeRate>> GetExchangeRatesAsync(IEnumerable<Currency> currencies);
+namespace ExchangeRateUpdater;
 
-    }
+public interface IExchangeRateProvider
+{
+public Task <IEnumerable<ExchangeRate>> GetExchangeRatesAsync(IEnumerable<Currency> currencies);
+
 }
