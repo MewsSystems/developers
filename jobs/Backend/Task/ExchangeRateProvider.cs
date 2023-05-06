c9897e40c5c377544afa7702f72fb6069ad74aff
jane <janenetting@blueyonder.co.uk>
2023-05-06 20:44:57 +0100

    added EschangeRateProvider

--- jobs/Backend/Task/ExchangeRateProvider.cs
+++ jobs/Backend/Task/ExchangeRateProvider.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
+using System.Net.Http;
+using System;
 
 namespace ExchangeRateUpdater
 {
@@ -13,7 +16,47 @@ namespace ExchangeRateUpdater
         /// </summary>
         public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            var doc = GetApi();
+            var exchangeRates = Enumerable.Empty<ExchangeRate>();
+            Currency sourceCurrency = new Currency("CZK");
+
+            if (doc.Descendants("radek") is not null)
+            {
+                var currencyList = currencies.Select(c => c.Code).ToList();
+                var filtered = doc.Descendants("radek").Where(e => currencyList.Contains(e.Attribute("kod").Value));
+                exchangeRates = filtered.Select(e => new ExchangeRate(
+                                sourceCurrency, 
+                                new Currency(e.Attribute("kod").Value),
+                                CalcExchangeRate(e.Attribute("mnozstvi").Value, e.Attribute("kurz").Value)
+                                )
+                );
+                return exchangeRates;
+            }
+            else
+            {
+                Console.WriteLine("No exchange rate results returned from the API");
+                return Enumerable.Empty<ExchangeRate>();
+            }
+        }
+
+        private decimal CalcExchangeRate(string amount, string value)
+        {
+            return decimal.Parse(value.Replace(",", ".")) / decimal.Parse(amount);
+        }
+
+        private static XDocument GetApi()
+        {
+            try
+            {
+                string url = "https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml";
+                XDocument doc = XDocument.Load(url);
+                return doc;
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine("An error occurred while making the HTTP request: {0}", ex.Message);
+                return new XDocument();
+            }
         }
     }
 }
