889ca1ebe309524cbee8001a734c51fac961816d
Sabina Ene <hello@sabinaene.ro>
2023-05-17 02:13:44 +0300

    style: ran prettier

--- jobs/Salesforce/cnbexrates/force-app/main/components/lwc/cnbExchangeRates/cnbExchangeRates.js
+++ jobs/Salesforce/cnbexrates/force-app/main/components/lwc/cnbExchangeRates/cnbExchangeRates.js
@@ -1,19 +1,17 @@
-import { LightningElement, wire } from 'lwc';
-import getExchangeRates from '@salesforce/apex/ExchangeRateController.getExchangeRates';
+import { LightningElement, wire } from "lwc";
+import getExchangeRates from "@salesforce/apex/ExchangeRateController.getExchangeRates";
 
 const columns = [
-  { label: 'Code', fieldName: 'currencyCode' },
-  { label: 'To CZK', fieldName: 'exactRate', type: 'number' },
-  { label: 'From CZK', fieldName: 'invertedRate', type: 'number' }
+  { label: "Code", fieldName: "currencyCode" },
+  { label: "To CZK", fieldName: "exactRate", type: "number" },
+  { label: "From CZK", fieldName: "invertedRate", type: "number" }
 ];
 
 export default class CnbExchangeRates extends LightningElement {
-  
   @wire(getExchangeRates) exchangeRates;
   tableHeaders = columns;
 
   get validityDate() {
     return this.exchangeRates.data?.[0]?.validFor;
   }
-
 }
