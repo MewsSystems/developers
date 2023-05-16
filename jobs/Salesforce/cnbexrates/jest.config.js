49f32cdcd0fc7b9f2f8ab918c4c4af2e0fc43a35
Sabina Ene <hello@sabinaene.ro>
2023-05-17 02:13:02 +0300

    lwc: jest unit tests

--- jobs/Salesforce/cnbexrates/jest.config.js
+++ jobs/Salesforce/cnbexrates/jest.config.js
@@ -1,6 +1,16 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"],
+  testMatch: ["**/lwc/**/__tests__/*.test.js"],
+  coverageDirectory: "coverage",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
