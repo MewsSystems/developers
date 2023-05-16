3b6fb1ec0ac94142e5df04511fc759dea9ae8a79
Sabina Ene <hello@sabinaene.ro>
2023-05-17 02:47:25 +0300

    doc: additional info on methods and functions

--- jobs/Salesforce/cnbexrates/force-app/main/components/lwc/cnbExchangeRates/cnbExchangeRates.js
+++ jobs/Salesforce/cnbexrates/force-app/main/components/lwc/cnbExchangeRates/cnbExchangeRates.js
@@ -7,10 +7,18 @@ const columns = [
   { label: "From CZK", fieldName: "invertedRate", type: "number" }
 ];
 
+/**
+ * Component to list the current exchange rates from CNB.
+ */
 export default class CnbExchangeRates extends LightningElement {
+
+  /** @property {Object} exchangeRates Calculated CNB exchange rate data */
   @wire(getExchangeRates) exchangeRates;
+
+  /** @property {Array} tableHeaders Exchange rate table headers */
   tableHeaders = columns;
 
+  /** @property {string} validityDate Date when the current data is valid */
   get validityDate() {
     return this.exchangeRates.data?.[0]?.validFor;
   }
