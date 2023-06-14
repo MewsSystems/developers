6140c9bf9e8bbe91a92c7dacbaf4c5013ddddf56
Jan Sklenář <jan.sklenar@alpiq.com>
2023-06-14 15:26:50 +0200

    test: add tests empty results, invalid lines

--- jobs/Backend/Task/tests/ExchangeRateUpdater.Tests/CnbParserTests.cs
+++ jobs/Backend/Task/tests/ExchangeRateUpdater.Tests/CnbParserTests.cs
@@ -6,56 +6,35 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace ExchangeRateUpdater.Tests
 {
 	public class CnbParserTests
 	{
-		// TODO Remove this field
-		private readonly ITestOutputHelper output;
-
-		public CnbParserTests(ITestOutputHelper output)
-		{
-			this.output = output;
-		}
-
 		[Theory]
-		[InlineData("cnb_fx_rates_cs_2023-06-12.txt","cs-CZ")]
-		[InlineData("cnb_fx_rates_en_2023-06-12.txt","en-US")]
+		[InlineData("cnb_fx_rates_cs.txt", "cs-CZ")]
+		[InlineData("cnb_fx_rates_en.txt", "en-US")]
 		public void Parse_OK(string sourceFile, string cultureName)
 		{
-			var data = EmbeddedResource.GetResource(sourceFile);
-			var options = Options.Create(new CnbDailyRatesOptions()
-			{
-				CultureName = cultureName
-			});
-			var loggerMock = new Mock<ILogger<CnbParser>>(MockBehavior.Strict);
-			var parser = new CnbParser(options, loggerMock.Object);
+			var data = GetTestDataFromFile(sourceFile);
+			var parser = CreateCnbParser(cultureName);
 
 			var exchangeRates = parser.Parse(data).ToArray();
-			
-			exchangeRates.Should().HaveCount(31);
-			
-			var usdCzk  = exchangeRates.Where(er => er.SourceCurrency.Code == "USD").ToArray();
+
+			var usdCzk = exchangeRates.Where(er => er.SourceCurrency.Code == "USD").ToArray();
 			usdCzk.Should().HaveCount(1);
 			usdCzk.First().TargetCurrency.Code.Should().Be("CZK");
 			usdCzk.First().Value.Should().Be(22.066m);
-			
-			var hufCzk  = exchangeRates.Where(er => er.SourceCurrency.Code == "USD").ToArray();
+
+			exchangeRates.Should().HaveCount(31);
 		}
 
 		[Theory]
-		[InlineData("cnb_fx_rates_cs_2023-06-12.txt", "cs-CZ")]
-		public void Parse_SourceCurrencyAmountNot1_OK(string sourceFile, string cultureName)
+		[InlineData("cnb_fx_rates_cs.txt", "cs-CZ")]
+		public void Parse_SourceCurrencyAmount100_OK(string sourceFile, string cultureName)
 		{
-			var data = EmbeddedResource.GetResource(sourceFile);
-			var options = Options.Create(new CnbDailyRatesOptions()
-			{
-				CultureName = cultureName
-			});
-			var loggerMock = new Mock<ILogger<CnbParser>>(MockBehavior.Strict);
-			var parser = new CnbParser(options, loggerMock.Object);
+			var data = GetTestDataFromFile(sourceFile);
+			var parser = CreateCnbParser(cultureName);
 
 			var exchangeRates = parser.Parse(data).ToArray();
 
@@ -64,5 +43,64 @@ namespace ExchangeRateUpdater.Tests
 			hufCzk.First().TargetCurrency.Code.Should().Be("CZK");
 			hufCzk.First().Value.Should().Be(0.06456m);
 		}
+
+		[Fact]
+		public void Parse_EmptyResult()
+		{
+			var data = GetTestDataFromFile("cnb_fx_rates_en_empty.txt");
+			var loggerMock = CreateLoggerMock();
+			loggerMock.Setup(x => x.Log(
+				LogLevel.Warning,
+				It.IsAny<EventId>(),
+				It.IsAny<It.IsAnyType>(),
+				It.IsAny<Exception>(),
+				It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+			));
+			var parser = CreateCnbParser(loggerMock: loggerMock);
+
+			var exchangeRates = parser.Parse(data).ToArray();
+
+			exchangeRates.Should().BeEmpty();
+		}
+
+		[Fact]
+		public void Parse_InvalidLines()
+		{
+			var data = GetTestDataFromFile("cnb_fx_rates_en_invalid_lines.txt");
+			var loggerMock = CreateLoggerMock();
+			loggerMock.Setup(x => x.Log(
+				LogLevel.Warning,
+				It.IsAny<EventId>(),
+				It.IsAny<It.IsAnyType>(),
+				It.IsAny<Exception>(),
+				It.IsAny<Func<It.IsAnyType, Exception?, string>>()
+			));
+			var parser = CreateCnbParser(loggerMock: loggerMock);
+
+			var exchangeRates = parser.Parse(data).ToArray();
+
+			exchangeRates.Where(er => er.SourceCurrency.Code == "AUD").Should().BeEmpty("AUD has invalid format");
+			exchangeRates.Where(er => er.SourceCurrency.Code == "BRL").Should().BeEmpty("BRL has invalid format");
+			exchangeRates.Where(er => er.SourceCurrency.Code == "BGN").Should().BeEmpty("BGN has invalid format");
+			exchangeRates.Where(er => er.SourceCurrency.Code == "CAD").Should().BeEmpty("CAD has invalid format");
+			exchangeRates.Should().HaveCount(31 - 4);
+		}
+
+		private static CnbParser CreateCnbParser(string cultureName = CnbDailyRatesOptions.DefaultCultureName, Mock<ILogger<CnbParser>>? loggerMock = null)
+		{
+			var options = CreateCnbDailyRatesOptions(cultureName);
+			loggerMock ??= CreateLoggerMock();
+			return new CnbParser(options, loggerMock.Object);
+		}
+
+		private static Mock<ILogger<CnbParser>> CreateLoggerMock() => new(MockBehavior.Strict);
+
+		private static IOptions<CnbDailyRatesOptions> CreateCnbDailyRatesOptions(string cultureName = CnbDailyRatesOptions.DefaultCultureName)
+			=> Options.Create(new CnbDailyRatesOptions()
+			{
+				CultureName = cultureName
+			});
+
+		private static string GetTestDataFromFile(string sourceFile) => EmbeddedResource.GetResource(sourceFile);
 	}
 }
