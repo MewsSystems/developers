9c770f9493be9dea5ec1507cdc6c67d926d29c69
Pablo <pablo@mylaif.es>
2023-07-05 11:19:55 +0200

    Added ExchangeEntities library.

---
+++ jobs/Backend/Task/ExchangeEntities/Currency.cs
@@ -0,0 +1,21 @@
+﻿namespace ExchangeEntities
+{
+	public class Currency
+	{
+        /// <summary>
+        /// Three-letter ISO 4217 code of the currency.
+        /// </summary>
+        public string Code { get; }
+
+        public Currency(string code)
+        {
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            return Code;
+        }
+    }
+}
+
