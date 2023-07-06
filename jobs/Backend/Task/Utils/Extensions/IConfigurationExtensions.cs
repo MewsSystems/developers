69ef373ee43db5875d9d52b1a78030a448a41768
Pablo <pablo@mylaif.es>
2023-07-06 17:08:32 +0200

    Added Utils project

---
+++ jobs/Backend/Task/Utils/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Utils.Extensions
+{
+	public static class IConfigurationExtensions
+	{
+		public static T GetRequiredValue<T>(this IConfiguration configuration, string key)
+		{
+            var section = configuration.GetSection(key);
+
+            if (!section.Exists())
+            {
+                throw new InvalidOperationException($"The `{key}` parameter is missing in the configuration. Please add it.");
+            }
+
+            return section.Get<T>();
+        }
+	}
+}
+
---
+++ jobs/Backend/Task/UtilsTests/Extensions/IConfigurationExtensionsTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using Utils.Extensions;
+
+namespace UtilsTests.Extensions
+{
+	public class IConfigurationExtensionsTests
+	{
+        private Dictionary<string, string> appSettings;
+        private IConfigurationRoot configuration;
+        private string Setting = "Test";
+
+        [SetUp]
+        public void SetUp()
+        {
+            appSettings = new Dictionary<string, string>
+            {
+                { Setting, Setting }
+            };
+
+            configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(appSettings)
+                .Build();
+        }
+
+        [Test]
+		public void GetRequiredValue_Successfully()
+		{
+            // Arrange
+
+            // Act
+            var result = configuration.GetRequiredValue<string>(Setting);
+
+            // Assert
+            result.Should().Be(Setting);
+        }
+
+        [Test]
+        public void GetRequiredValue_Thorws_Exception()
+        {
+            // Arrange
+            string key = "test1";
+
+            // Act
+            Action act = () => configuration.GetRequiredValue<string>(key);
+
+            // Assert
+            act.Should().Throw<Exception>();
+        }
+    }
+}
+
