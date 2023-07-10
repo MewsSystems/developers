4f0461a2d1c6c47b98ef156f984faa43fb2a32a6
danradic <danradic@gmail.com>
2023-07-10 17:11:43 +0200

    create HostBuilder to be able to set up the application's services;

---
+++ jobs/Backend/Task/App.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExchangeRateUpdater;
+
+internal class App
+{
+    private readonly ILogger<App> _logger;
+
+    public App(ILogger<App> logger)
+    {
+        _logger = logger;
+    }
+
+    private static IEnumerable<Currency> currencies = new[]
+    {
+        new Currency("USD"),
+        new Currency("EUR"),
+        new Currency("CZK"),
+        new Currency("JPY"),
+        new Currency("KES"),
+        new Currency("RUB"),
+        new Currency("THB"),
+        new Currency("TRY"),
+        new Currency("XYZ")
+    };
+
+    internal void Run(string[] args)
+    {
+        try
+        {
+            var provider = new ExchangeRateProvider();
+            var rates = provider.GetExchangeRates(currencies);
+
+            Console.WriteLine($"Successfully retrieved {rates.Count()} exchange rates:");
+            foreach (var rate in rates)
+            {
+                Console.WriteLine(rate.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Could not retrieve exchange rates: '{ex.Message}'.");
+            _logger.LogError(ex, "Unhandled exception occured.");
+        }
+
+        Console.ReadLine();
+    }
+}
+
--- jobs/Backend/Task/Program.cs
+++ jobs/Backend/Task/Program.cs
@@ -1,43 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using ExchangeRateUpdater;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace ExchangeRateUpdater
-{
-    public static class Program
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, builder) => builder.AddJsonFile("appsettings.json").Build())
+    .ConfigureServices((context, services) =>
     {
-        private static IEnumerable<Currency> currencies = new[]
-        {
-            new Currency("USD"),
-            new Currency("EUR"),
-            new Currency("CZK"),
-            new Currency("JPY"),
-            new Currency("KES"),
-            new Currency("RUB"),
-            new Currency("THB"),
-            new Currency("TRY"),
-            new Currency("XYZ")
-        };
+        services.AddScoped<App>();
+    }).Build();
 
-        public static void Main(string[] args)
-        {
-            try
-            {
-                var provider = new ExchangeRateProvider();
-                var rates = provider.GetExchangeRates(currencies);
-
-                Console.WriteLine($"Successfully retrieved {rates.Count()} exchange rates:");
-                foreach (var rate in rates)
-                {
-                    Console.WriteLine(rate.ToString());
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Could not retrieve exchange rates: '{e.Message}'.");
-            }
-
-            Console.ReadLine();
-        }
-    }
-}
+host.Services.GetRequiredService<App>().Run(args);
