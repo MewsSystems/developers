64fccd383bbb6a8d2b3f1313e57c2f75f2d17ca7
josepo <jlpolo@gmail.com>
2023-07-24 19:27:27 +0200

    initial test

---
+++ jobs/Backend/Task/ExchangeRateUpdater.Tests/ExchangeRateUpdaterTest.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using Xunit;
+
+namespace ExchangeRateUpdater.Tests;
+
+public class ExchangeRateUpdaterTest
+{
+   [Fact]
+   public void OneExistingRate()
+   {
+      var provider = new ExchangeRateProvider();
+      var rates = provider.GetExchangeRates(new Currency[] {
+         new Currency("EUR")
+      });
+
+      var rate = rates.Single();
+
+      Assert.Equal("EUR", rate.SourceCurrency.Code);
+      Assert.Equal("CZK", rate.TargetCurrency.Code);
+      Assert.Equal(24, rate.Value);
+   }
+}
