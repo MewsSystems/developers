7fd96a53dc2ecd190be1e4f84fb9fc0dff518e93
Silvia Walla <wallovas@gmail.com>
2023-07-27 16:19:12 +0200

    add models

---
+++ jobs/Backend/Task/ExchangeRatesProvider/ExchangeRatesProvider/Models/ExchangeRate.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExchangeRateProvider
+{
+    public class ExchangeRate
+    {
+
+        public string validFor { get; set; }
+        public int order { get; set; }
+        public string country { get; set; }
+        public string currency { get; set; }
+        public int amount { get; set; }
+        public string currencyCode { get; set; }
+        public decimal rate { get; set; }
+        public ExchangeRate(string ValidFor, int Order, string Country, string Currency, int Amount, string CurrencyCode, decimal Rate)
+        {
+            validFor = ValidFor;
+            order = Order;
+            country = Country;
+            currency = Currency;
+            amount = Amount;
+            currencyCode = CurrencyCode;
+            rate = Rate;
+        }
+
+        public override string ToString()
+        {
+            var sourceCurrency = "CZK";
+            return $"{sourceCurrency}/{currencyCode}={rate}";
+        }
+    }
+}
---
+++ jobs/Backend/Task/ExchangeRatesProvider/ExchangeRatesProvider/Models/RatesViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ExchangeRateProvider;
+
+namespace ExchangeRatesProvider.Models
+{
+    public class RatesViewModel
+    {
+        public List<ExchangeRate> rates { get; set; }
+        public Currency sourceCurrency { get; set; }
+    }
+}
