dc4c488fe800043aee92822447982e24c4b5af99
Silvia Walla <wallovas@gmail.com>
2023-07-27 16:19:37 +0200

    added interface

---
+++ jobs/Backend/Task/ExchangeRatesProvider/ExchangeRatesProvider/Interfaces/IExchangeRateService.cs
@@ -0,0 +1,11 @@
+﻿using ExchangeRatesProvider.Models;
+
+namespace ExchangeRatesProvider.Interfaces
+{
+    public interface IExchangeRateService
+    {
+        Task<(RatesViewModel result, int statusCode)> GetExchangeRates();
+        Task<(RatesViewModel result, int statusCode)> GetSearchResults(string search);
+        Task<(RatesViewModel result, int statusCode)> GetSelectedCurrencies();
+    }
+}
