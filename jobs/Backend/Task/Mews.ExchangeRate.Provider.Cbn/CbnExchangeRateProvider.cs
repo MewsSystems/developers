bbe545e49e1fb33e0309074b3750de813f3258ac
Roberto Vázquez Iglesias <roberto.vazquez@hitachivantara.com>
2023-08-06 00:01:35 +0200

    Added dummy exchange rate provider

---
+++ jobs/Backend/Task/Mews.ExchangeRate.Provider.Cbn/CbnExchangeRateProvider.cs
@@ -0,0 +1,10 @@
+﻿using Mews.ExchangeRate.Domain;
+
+namespace Mews.ExchangeRate.Provider.Cbn;
+public class CbnExchangeRateProvider : IProvideExchangeRates
+{
+    public Task<IEnumerable<Domain.ExchangeRate>> GetExchangeRatesForCurrenciesAsync(IEnumerable<Currency> currencies)
+    {
+        return Task.FromResult(Enumerable.Empty<Domain.ExchangeRate>());
+    }
+}
