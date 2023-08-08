9a6b24c5a53b41bec2e83134cab8cb21bdfcb5e3
Max Aldunate <maxaldunate@gmail.com>
2023-08-08 11:41:49 +0200

    record to map json data

---
+++ jobs/Backend/Task/ExchangeRateRecord.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRateUpdater
+{
+    // Max: Rule: one class one file.  Possible exception
+
+    public record ExchangeRatesRecord(
+        IEnumerable<ExchangeRateRecord> rates
+    );
+
+    public record ExchangeRateRecord(
+        string validFor,
+        int order,
+        string country,
+        string currency,
+        int amount,
+        string currencyCode,
+        decimal rate
+
+    );
+
+}
