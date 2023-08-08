6c563f2b0d8c7a5ed25d11f6d3f48f3821bfba09
Max Aldunate <maxaldunate@gmail.com>
2023-08-08 11:43:53 +0200

    fix usings

--- jobs/Backend/Task/ExchangeRateProvider.cs
+++ jobs/Backend/Task/ExchangeRateProvider.cs
@@ -1,10 +1,6 @@
-﻿using ExchangeRateUpdater.APIs;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace ExchangeRateUpdater
 {
--- jobs/Backend/Task/Program.cs
+++ jobs/Backend/Task/Program.cs
@@ -1,5 +1,4 @@
-﻿using ExchangeRateUpdater.APIs;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
