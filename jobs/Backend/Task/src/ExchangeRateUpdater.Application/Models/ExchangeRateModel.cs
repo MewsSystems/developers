1b7a755f150326b72c4378c7e7719764529f4843
Manel Pérez <manel.parez@redarbor.csccsl.com>
2023-08-09 22:41:50 +0200

    added infrastructure data

---
+++ jobs/Backend/Task/src/ExchangeRateUpdater.Application/Models/ExchangeRateModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ExchangeRateUpdater.Application.Models
+{
+    public class ExchangeRateModel
+    {
+        public ExchangeRateModel()
+        {
+            ExchangeRates = new List<string>();
+        }
+
+        public IList<string> ExchangeRates { get; }
+
+        public void Add(string value)
+        {
+            ExchangeRates.Add(value);
+        }
+    }
+}
--- jobs/Backend/Task/src/ExchangeRateUpdater.Application/Services/ExchangeRateProviderService.cs
+++ jobs/Backend/Task/src/ExchangeRateUpdater.Application/Services/ExchangeRateProviderService.cs
@@ -1,21 +1,10 @@
-﻿using ExchangeRateUpdater.Application.Abstractions;
-using ExchangeRateUpdater.Application.Mappings;
+﻿
 using ExchangeRateUpdater.Application.Models;
-using ExchangeRateUpdater.Infrastucture.Data.API.Abstractions;
-using Microsoft.Extensions.Configuration;
 
 namespace ExchangeRateUpdater.Application.Services
 {
-    public class ExchangeRateProviderService : IExchangeRateProviderService
+    public class ExchangeRateProviderService
     {
-        private readonly IExternalAPIService _externalAPI;
-        private readonly IConfiguration _configuration;
-
-        public ExchangeRateProviderService(IExternalAPIService externalAPI, IConfiguration configuration)
-        {
-            _externalAPI = externalAPI;
-            _configuration = configuration;
-        }
         /// <summary>
         /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
         /// by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
@@ -24,23 +13,7 @@ namespace ExchangeRateUpdater.Application.Services
         /// </summary>
         public async Task<ExchangeRateModel> GetExchangeRates(IEnumerable<CurrencyModel> request)
         {
-            var sourceCurrency = _configuration["ExternalApiSettings:SourceCurrency"];
-
-            var currencies = CurrencyMappings.Map(request);
-
-            var response = await _externalAPI.GetFromExternalApi();
-
-            var exchangeRates = ExchangeRateMappings.Map(response, sourceCurrency ?? "CZK");
-
-            var RatesIntersect = exchangeRates
-                       .Join(currencies,
-                       e => e.TargetCurrency.Code,
-                       c => c.Code,
-                       (e, c) => e).ToList();
-
-            var exchangeRateModel = ExchangeRateMappings.Map(RatesIntersect);
-
-            return exchangeRateModel;
+            return Enumerable.Empty<ExchangeRateModel>();
         }
     }
 }
