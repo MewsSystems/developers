29d884d964c84ffc051e1e9c78ef948d68d14306
Fran <fcorralmarin@gmail.com>
2023-08-19 19:05:07 +0200

    feat: add validations to ExchangeRate ctor

--- jobs/Backend/Task/src/Domain/ExchangeRate.cs
+++ jobs/Backend/Task/src/Domain/ExchangeRate.cs
@@ -1,11 +1,23 @@
-﻿namespace ExchangeRateUpdater.Domain
+﻿using System;
+
+namespace ExchangeRateUpdater.Domain
 {
     public class ExchangeRate
     {
         public ExchangeRate(Currency sourceCurrency, Currency targetCurrency, decimal value)
         {
-            SourceCurrency = sourceCurrency;
-            TargetCurrency = targetCurrency;
+            // This will be a good use case for Try.Aggregate
+            // But, I've decided not to do so as it won't reflect how I'd do it currently
+
+            SourceCurrency ??= sourceCurrency ?? throw new ArgumentNullException(nameof(sourceCurrency));
+            TargetCurrency ??= targetCurrency ?? throw new ArgumentNullException(nameof(targetCurrency));
+
+            if (SourceCurrency.Code == TargetCurrency.Code)
+                throw new ApplicationException($"Source and target currencies cannot refer to the same currency ({sourceCurrency.Code})");
+
+            if (value < 0)
+                throw new ApplicationException("Exchange rate value cannot be negative");
+
             Value = value;
         }
 
