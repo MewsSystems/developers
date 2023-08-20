de09d290f40ce24a4cf41ee7511cc2a078e63f90
Fran <fcorralmarin@gmail.com>
2023-08-20 10:10:08 +0200

    feat: create default currency

--- jobs/Backend/Task/src/Domain/Currency.cs
+++ jobs/Backend/Task/src/Domain/Currency.cs
@@ -2,6 +2,13 @@
 {
     public class Currency
     {
+        /// <summary>
+        /// In a real world scenario default currency should not be defined in code
+        /// so that it can be different currency for different instances of the same service.
+        /// This could be defined at appsettings level
+        /// </summary>
+        public static Currency DEFAULT_CURRENCY => new Currency("CZK");
+
         private Currency(string code)
         {
             Code = code;
