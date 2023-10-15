23d272fc7e46e3dd4e9dc59b082665401032cced
Mikel <mikelsalazarcabeza@gmail.com>
2023-10-15 23:52:45 +0200

    This is my solution the the backend home task

---
+++ jobs/Backend/Task/Dto/GetExchangeRateDto.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRateUpdater
+{
+    public record GetExchangeRatesDto(IEnumerable<ExchangeRateDto> Rates);
+    public record ExchangeRateDto(string Currency, string CurrencyCode, int Amount, decimal Rate);
+}
