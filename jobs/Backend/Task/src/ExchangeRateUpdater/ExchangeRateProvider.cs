bbc2928a5f9dd839a39ad9ab193ba90eaf5d53e4
Zdenek Havlin <wdolek@hotmail.com>
2023-11-02 21:11:34 +0100

    Introduce initial implementation of exchange rate provider

--- jobs/Backend/Task/src/ExchangeRateUpdater/ExchangeRateProvider.cs
+++ jobs/Backend/Task/src/ExchangeRateUpdater/ExchangeRateProvider.cs
@@ -1,19 +1,65 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 
-namespace ExchangeRateUpdater
+namespace ExchangeRateUpdater;
+
+public class ExchangeRateProvider
 {
-    public class ExchangeRateProvider
+    /// <summary>
+    /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
+    /// by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
+    /// do not return exchange rate "USD/CZK" with value calculated as 1 / "CZK/USD". If the source does not provide
+    /// some of the currencies, ignore them.
+    /// </summary>
+    public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
     {
-        /// <summary>
-        /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
-        /// by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
-        /// do not return exchange rate "USD/CZK" with value calculated as 1 / "CZK/USD". If the source does not provide
-        /// some of the currencies, ignore them.
-        /// </summary>
-        public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
-        {
-            return Enumerable.Empty<ExchangeRate>();
-        }
+        var now = DateTime.UtcNow;
+
+        using var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.cnb.cz/cnbapi/exrates/daily?date={now:yyyy-MM-dd}&lang=EN");
+        request.Headers.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        using var httpClient = new HttpClient();
+        var response = httpClient.Send(request);
+
+        var ratesPayload = response.Content.ReadFromJsonAsync<RatesResponsePayload>().GetAwaiter().GetResult();
+
+        return ratesPayload.Rates
+            .Where(r => currencies.Any(c => c.Code == r.CurrencyCode))
+            .Select(
+                r => new ExchangeRate(
+                    sourceCurrency: new Currency(r.CurrencyCode),
+                    targetCurrency: new Currency("CZK"),
+                    value: r.ExchangeRate));
     }
+}
+
+public class RatesResponsePayload
+{
+    [JsonPropertyName("rates")]
+    public IReadOnlyCollection<Rate> Rates { get; init; }
+}
+
+public class Rate
+{
+    [JsonPropertyName("validFor")]
+    public DateTime ValidFor { get; init; }
+    
+    [JsonPropertyName("country")]
+    public string CountryName { get; init; }
+    
+    [JsonPropertyName("currency")]
+    public string CurrencyName { get; init; }
+    
+    [JsonPropertyName("amount")]
+    public int Amount { get; init; }
+    
+    [JsonPropertyName("currencyCode")]
+    public string CurrencyCode { get; init; }
+    
+    [JsonPropertyName("rate")]
+    public decimal ExchangeRate { get; init; }
 }
--- jobs/Backend/Task/tests/ExchangeRateUpdater.Tests/ExchangeRateProviderShould.cs
+++ jobs/Backend/Task/tests/ExchangeRateUpdater.Tests/ExchangeRateProviderShould.cs
@@ -2,4 +2,31 @@ namespace ExchangeRateUpdater.Tests;
 
 public class ExchangeRateProviderShould
 {
+    [Fact]
+    public void ReturnExchangeRates()
+    {
+        // act
+        var provider = new ExchangeRateProvider();
+        var rates = provider.GetExchangeRates(new[] { new Currency("EUR") });
+
+        // assert
+        var rate = Assert.Single(rates);
+        Assert.Equal("EUR", rate.SourceCurrency.Code);
+        Assert.Equal("CZK", rate.TargetCurrency.Code);
+        
+        // let's expect that the exchange rate is positive (⊙_⊙;)
+        Assert.True(rate.Value > 0);
+    }
+    
+    [Fact]
+    public void NotReturnUnknownCurrency()
+    {
+        // act
+        var provider = new ExchangeRateProvider();
+        var rates = provider.GetExchangeRates(new[] { new Currency("SPL") });
+
+        // assert
+        // SPL – Seborga Luigino (Principality of Seborga) is not expected to be supported by the CNB
+        Assert.Empty(rates);
+    }
 }
