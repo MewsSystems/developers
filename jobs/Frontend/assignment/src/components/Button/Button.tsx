066bf3ccdac1b601e2bc697921c8073129e7d4b2
Daniel Novosad <work.daniknewgarden@gmail.com>
2023-11-07 14:37:06 +0100

    feat(button): add new Button component

---
+++ jobs/Frontend/assignment/src/components/Button/Button.tsx
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import styled from "styled-components";
+
+export interface ButtonProps {}
+
+const StyledButton = styled.button`
+  border: none;
+  padding: 10px 24px;
+  border-radius: 100px;
+
+  font-size: ${props => props.theme.fonts.labelLarge.fontSize};
+  color: ${props => props.theme.colors.primary.on};
+
+  background-color: ${props => props.theme.colors.primary.main};
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:focus {
+    outline: 2px solid ${props => props.theme.colors.primary.container};
+    opacity: 0.9;
+  }
+`;
+
+export function Button({
+  children,
+  ...props
+}: PropsWithChildren<ButtonHTMLAttributes<ButtonProps>>) {
+  return <StyledButton {...props}>{children}</StyledButton>;
+}
--- jobs/Frontend/assignment/src/pages/Search.tsx
+++ jobs/Frontend/assignment/src/pages/Search.tsx
@@ -1,20 +1,15 @@
 import styled from "styled-components";
-
-const StyledLink = styled.a`
-  color: ${props => props.theme.colors.primary.on};
-`;
+import { Button } from "../components";
 
 const StyledWrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background-color: ${props => props.theme.colors.primary.main};
 `;
 
 export function Search() {
   return (
     <StyledWrapper>
-      Movie search
-      <StyledLink href="/details/123">Go to the details page</StyledLink>
+      <Button>I'm a button</Button>
     </StyledWrapper>
   );
 }
