ee2680d7a2cedf57e857554a7b94be058118f158
Daniel Novosad <work.daniknewgarden@gmail.com>
2023-11-11 16:43:05 +0100

    style(eslint): update eslint rules

--- jobs/Frontend/assignment/.eslintrc.cjs
+++ jobs/Frontend/assignment/.eslintrc.cjs
@@ -5,7 +5,6 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
-    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
