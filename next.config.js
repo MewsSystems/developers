c0da59c695225d2cebf31a62d29bb3f518773421
im-luka <lux.9833@gmail.com>
2023-11-11 16:29:50 +0100

    feat: pwa support

--- next.config.js
+++ next.config.js
@@ -1,4 +1,55 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path");
+const withPlugins = require("next-compose-plugins");
+const withPWAInit = require("next-pwa");
 
-module.exports = nextConfig
+const isDevelopment = process.env.NODE_ENV === "development";
+
+const withPWA = withPWAInit({
+  disable: isDevelopment,
+  dest: "public",
+  exclude: [
+    ({ asset }) => {
+      if (
+        asset.name.startsWith("server/") ||
+        asset.name.match(
+          /^((app-|^)build-manifest\.json|react-loadable-manifest\.json)$/
+        ) ||
+        (isDevelopment && !asset.name.startsWith("static/runtime/"))
+      ) {
+        return true;
+      }
+      return false;
+    },
+  ],
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    if (!isDevelopment) {
+      const registerJs = path.join(
+        path.dirname(require.resolve("next-pwa")),
+        "register.js"
+      );
+      const entry = config.entry;
+      config.entry = () =>
+        entry().then((entries) => {
+          if (
+            entries["main-app"] &&
+            !entries["main-app"].includes(registerJs)
+          ) {
+            if (Array.isArray(entries["main-app"])) {
+              entries["main-app"].unshift(registerJs);
+            } else if (typeof entries["main-app"] === "string") {
+              entries["main-app"] = [registerJs, entries["main-app"]];
+            }
+          }
+          return entries;
+        });
+    }
+    return config;
+  },
+};
+
+const plugins = [withPWA];
+module.exports = withPlugins(plugins, nextConfig);
