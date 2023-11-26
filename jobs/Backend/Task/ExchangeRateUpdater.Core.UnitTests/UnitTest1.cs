4e7388d401beb1fd6c28e870d2ed24dc3f07a8b8
fjmejias <fjmejiasarcos@gmail.com>
2023-11-26 22:28:54 +0100

    added unittest project

---
+++ jobs/Backend/Task/ExchangeRateUpdater.Core.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ExchangeRateUpdadte.Core.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
