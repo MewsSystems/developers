af87abbb7f7932fcac63e5b76b572e6156743d98
isabelhdias <15924034+isabelhdias@users.noreply.github.com>
2023-11-27 20:47:30 +0100

    Add unit test

--- jobs/Backend/Task/Tests/ExchangeRateUpdaterApi.Tests.Unit/Controllers/ExchangeRatesControllerTests.cs
+++ jobs/Backend/Task/Tests/ExchangeRateUpdaterApi.Tests.Unit/Controllers/ExchangeRatesControllerTests.cs
@@ -125,4 +125,65 @@ public class ExchangeRatesControllerTests : TestBase
             }
         );
     }
+    
+    [Test]
+    public async Task GivenOneExchangeRateToRequest_WhenMultipleExchangeRatesArePresent_ShouldReturnRequestedExchangeRate()
+    {
+        // arrange
+        var date = new DateTime(2023, 11, 25);
+        
+        var exchangeRatesToRequest = new ExchangeRatesRequestDto();
+        var exchangeRateRequest = new ExchangeRateRequestDetailsDtoBuilder()
+            .WithSourceCurrency(new CurrencyDto("USD"))
+            .WithTargetCurrency(new CurrencyDto("CZK"))
+            .Build();
+        
+        exchangeRatesToRequest.AddExchangeRate(exchangeRateRequest);
+        
+        var exchangeRate1 = new ExchangeRate(
+            new Currency("USD"),
+            new Currency("CZK"),
+            new decimal(22.00)
+        );
+
+        var exchangeRate2 = new ExchangeRate(
+            new Currency("CZK"),
+            new Currency("USD"),
+            new decimal(12.00)
+        );
+        
+        ExchangeRatesRepositoryInMemory.AddExchangeRate(exchangeRate1, date);
+        ExchangeRatesRepositoryInMemory.AddExchangeRate(exchangeRate2, date);
+        
+        // act
+        var body = JsonConvert.SerializeObject(exchangeRatesToRequest);
+
+        var requestUrl = "api/exchangerates"
+            .AppendPathSegment("getExchangeRates")
+            .SetQueryParam("date", date);
+
+        HttpContent content = new StringContent(body, Encoding.UTF8, "application/json");
+
+        HttpResponseMessage httpResponseMessage = await HttpClient.PostAsync(requestUrl, content);
+        
+        // assert
+        httpResponseMessage.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var getExchangeRatesResponse = await httpResponseMessage.Content.ReadAsStringAsync();
+        var getExchangeRatesResult = JsonConvert.DeserializeObject<List<ExchangeRateResultDto>>(getExchangeRatesResponse);
+
+        getExchangeRatesResult.Should().NotBeEmpty();
+        getExchangeRatesResult.Count.Should().Be(1);
+        getExchangeRatesResult.Should().BeEquivalentTo(
+            new List<ExchangeRateResultDto>
+            {
+                new ExchangeRateResultDto
+                {
+                    SourceCurrency = new CurrencyDto("USD"),
+                    TargetCurrency = new CurrencyDto("CZK"),
+                    Value = new decimal(22.00)
+                }
+            }
+        );
+    }
 }
