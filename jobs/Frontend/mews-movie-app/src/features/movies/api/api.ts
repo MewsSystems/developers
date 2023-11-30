6cff8470400373beb2417d5574e48942b080f024
Michal Kučera <miso.kucera97@gmail.com>
2023-11-30 13:33:03 +0100

    Refactor API endpoints

--- jobs/Frontend/mews-movie-app/src/features/movies/api/api.ts
+++ jobs/Frontend/mews-movie-app/src/features/movies/api/api.ts
@@ -2,43 +2,32 @@ import { ElementOf } from "@/utilities/types";
 import { operations } from "./generated/types";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const api_key = process.env.NEXT_PUBLIC_MOVIE_DB_API_KEY;
+const baseUrl = "https://api.themoviedb.org/3/";
+
 type SearchMovieResults =
     operations["search-movie"]["responses"]["200"]["content"]["application/json"];
+type SearchMovieArgs = { query: string; page: number };
 
 type MovieDetailResult =
     operations["movie-details"]["responses"]["200"]["content"]["application/json"];
+type MovieDetailArgs = number;
+
+export type SearchMovie = ElementOf<SearchMovieResults["results"]>;
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3/" }),
+    baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-        getMovies: builder.query<
-            SearchMovieResults,
-            { query: string; page: number }
-        >({
-            query: ({ query, page }) => {
-                return {
-                    url: `search/movie`,
-                    params: {
-                        api_key: process.env.NEXT_PUBLIC_MOVIE_DB_API_KEY,
-                        query,
-                        page,
-                    },
-                };
-            },
+        getMovies: builder.query<SearchMovieResults, SearchMovieArgs>({
+            query: ({ query, page }) => ({
+                url: `search/movie`,
+                params: { api_key, query, page },
+            }),
         }),
-        getMovieDetail: builder.query<MovieDetailResult, number>({
-            query: (movieId: number) => {
-                return {
-                    url: `movie/${movieId}`,
-                    params: {
-                        api_key: process.env.NEXT_PUBLIC_MOVIE_DB_API_KEY,
-                    },
-                };
-            },
+        getMovieDetail: builder.query<MovieDetailResult, MovieDetailArgs>({
+            query: (id: number) => `movie/${id}?api_key=${api_key}`,
         }),
     }),
 });
-
-export type SearchMovie = ElementOf<SearchMovieResults["results"]>;
 
 export const { useGetMoviesQuery, useGetMovieDetailQuery } = api;
