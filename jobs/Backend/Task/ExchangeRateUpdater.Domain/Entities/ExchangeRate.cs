b4f1e0ac0a489229c306c249f3dd1640498a105c
victorlavric <lavric.victor17@gmail.com>
2023-12-02 12:51:07 +0100

    Add ExchangeRate class

---
+++ jobs/Backend/Task/ExchangeRateUpdater.Domain/Entities/ExchangeRate.cs
@@ -0,0 +1,17 @@
+﻿using ExchangeRateUpdater.Domain.ValueObjects;
+
+namespace ExchangeRateUpdater.Domain.Entities;
+
+public class ExchangeRate
+{
+    public Currency SourceCurrency { get; }
+    public Currency TargetCurrency { get; }
+    public CurrencyRate CurrencyRate { get; }
+
+    public ExchangeRate(Currency? sourceCurrency, Currency? targetCurrency, CurrencyRate? currencyRate)
+    {
+        SourceCurrency = sourceCurrency ?? throw new ArgumentNullException(nameof(sourceCurrency));
+        TargetCurrency = targetCurrency ?? throw new ArgumentNullException(nameof(targetCurrency));
+        CurrencyRate = currencyRate ?? throw new ArgumentNullException(nameof(currencyRate));
+    }
+}
