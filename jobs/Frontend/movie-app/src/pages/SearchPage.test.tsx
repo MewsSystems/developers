5bfaaca7c21dc49cc58e4739d6ea843d7782de1a
Alexandra Espichan <aespichanlinares@gmail.com>
2023-12-04 13:00:55 +0100

    Remove placeholder code

--- jobs/Frontend/movie-app/src/pages/SearchPage.test.tsx
+++ jobs/Frontend/movie-app/src/pages/SearchPage.test.tsx
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
-import App from "./App"
+import { store } from "../app/store"
+import App from "./SearchPage"
 
 test("renders learn react link", () => {
   const { getByText } = render(
---
+++ jobs/Frontend/movie-app/src/pages/SearchPage.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Search Page</div>
+}
+
+export default App
