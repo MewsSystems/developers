ae2f21ffe3eda59eb5e25ddc27fe90d743565547
Alexandra Espichan <aespichanlinares@gmail.com>
2023-12-05 12:33:49 +0100

    Add "@" alias

--- jobs/Frontend/movie-app/vite.config.ts
+++ jobs/Frontend/movie-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,5 +17,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "src/setupTests",
     mockReset: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 })
