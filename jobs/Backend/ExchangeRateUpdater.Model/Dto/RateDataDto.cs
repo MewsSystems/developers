43489ca8c34c94f9fa4519c3be0e9763b2d6977a
Stephanie Finnigan <snoellm@gmail.com>
2023-12-13 13:32:59 +0000

    Add new project Model
    Added folders Common & Dto; copied models from existing project; created new dto RateData

---
+++ jobs/Backend/ExchangeRateUpdater.Model/Dto/RateDataDto.cs
@@ -0,0 +1,18 @@
+﻿namespace ExchangeRateUpdater.Model.Dto
+{
+    public class RateDataDto
+    {
+        public List<CnbRate> Rates { get; set; }
+    }
+
+    public class CnbRate
+    {
+        public string ValidFor { get; set; }
+        public int Order { get; set; }
+        public string Country { get; set; }
+        public string Currency { get; set; }
+        public int Amount { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
