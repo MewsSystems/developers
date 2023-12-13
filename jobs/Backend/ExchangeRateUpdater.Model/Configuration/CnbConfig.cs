91c39753725f24e263b771da571f527e14f8a6db
Stephanie Finnigan <snoellm@gmail.com>
2023-12-13 14:09:32 +0000

    Add new Model classes
    Dtos for request & responses; config for cnb api call

---
+++ jobs/Backend/ExchangeRateUpdater.Model/Configuration/CnbConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Model.Configuration
+{
+    public class CnbConfig
+    {
+        public string BaseAddress { get; set; }
+        public string ApiEndpoint { get; set; }
+        public string TargetCurrency { get; set; }
+    }
+}
