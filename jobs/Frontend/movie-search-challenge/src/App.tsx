f65a62b4e8d80afaaf41da5ed160ceb7373e3123
Hernán Domingo <hernan.domingo@hotmail.com>
2024-01-23 15:49:35 +0100

    feat: simple Not Found page

--- jobs/Frontend/movie-search-challenge/src/App.tsx
+++ jobs/Frontend/movie-search-challenge/src/App.tsx
@@ -4,6 +4,7 @@ import { GlobalStyles } from "./styles/Global"
 import MovieSearch from "./pages/MovieSearch"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import MovieInfo from "./pages/MovieInfo"
+import NotFound from "./pages/NotFound"
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<MovieSearch />} />
           <Route path="/:id" element={<MovieInfo />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
