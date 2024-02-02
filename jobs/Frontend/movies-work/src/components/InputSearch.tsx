cf1f73a6fe5e5dcb3823fbcae6583afacd75af15
Jakub Kares (private) <jakub.kares@gmail.com>
2024-02-02 22:57:56 +0100

    Fetching movies from input

--- jobs/Frontend/movies-work/src/components/InputSearch.tsx
+++ jobs/Frontend/movies-work/src/components/InputSearch.tsx
@@ -1,8 +1,44 @@
-// import { ReactComponent as SearchSVG } from "../assets/search.svg";
+import { useEffect, useContext, useRef } from "react";
+import { AppContext } from "../contexts/AppContext";
 import SearchSVG from "../assets/search.svg";
+
 export default function InputSearch() {
+  const { searchMovieKeyword, changeSearchKeyword, searchMovies } =
+    useContext(AppContext);
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    // it does not make sense to search for an empty string
+    if (searchMovieKeyword) {
+      searchMovies();
+    }
+  }, [searchMovieKeyword, searchMovies]);
+
+  // for debouncing the input change, if user stops typing, then the search will be triggered
+  let timerIdChange: NodeJS.Timeout;
+
+  const onInputChangeHandler = () => {
+    if (timerIdChange) {
+      clearTimeout(timerIdChange);
+    }
+    // do not update the state if the input is not available
+    // updating status to an empty value would cause re-rendering and it is not necessary
+    if (!inputRef.current) return;
+
+    // changing the state will start searching for movies in useEffect
+    timerIdChange = setTimeout(() => {
+      const searchInputKeyword = inputRef.current.value;
+      changeSearchKeyword(searchInputKeyword);
+    }, 500);
+  };
+
+  const onSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <label
         htmlFor="default-search"
         className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
@@ -14,11 +50,12 @@ export default function InputSearch() {
           <img src={SearchSVG} className="w-4" alt="Search icon" />
         </div>
         <input
+          onChange={onInputChangeHandler}
+          ref={inputRef}
           type="search"
           id="default-search"
           className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search movies..."
-          required
         />
       </div>
     </form>
--- jobs/Frontend/movies-work/src/contexts/AppContext.tsx
+++ jobs/Frontend/movies-work/src/contexts/AppContext.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export const AppContext = createContext({
   searchMovieKeyword: "",
+  changeSearchKeyword: () => {},
   fetchedMovies: [],
   page: 1,
   maximumPage: null,
@@ -13,25 +14,25 @@ export default function AppContextProvider({ children }) {
   const [searchMovieKeyword, setSearchMovieKeyword] = useState("");
   const [page, setPage] = useState(1);
 
-  useEffect(() => {
-    async function fetchMovies(searchKeyword: string, page: number) {
-      const options = {
-        method: "GET",
-        headers: {
-          accept: "application/json",
-        },
-      };
-      const searchKeywordURLEncoded = encodeURIComponent(searchKeyword);
-      const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?query=${searchKeywordURLEncoded}&include_adult=false&language=en-US&page=${page}&api_key=03b8572954325680265531140190fd2a`,
-        options
-      );
-      const data = await response.json();
-      console.log(data);
-    }
+  async function fetchMovies(searchKeyword: string, page: number) {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+      },
+    };
+    const searchKeywordURLEncoded = encodeURIComponent(searchKeyword);
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${searchKeywordURLEncoded}&include_adult=false&language=en-US&page=${page}&api_key=03b8572954325680265531140190fd2a`,
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+  }
 
+  function searchMovies() {
     fetchMovies(searchMovieKeyword, page);
-  }, [searchMovieKeyword, page]);
+  }
 
   const changeSearchKeyword = (newKeyword: string) => {
     setSearchMovieKeyword(newKeyword);
@@ -45,6 +46,7 @@ export default function AppContextProvider({ children }) {
     changeSearchKeyword: changeSearchKeyword,
     page: page,
     changePage: changePage,
+    searchMovies: searchMovies,
   };
 
   return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
