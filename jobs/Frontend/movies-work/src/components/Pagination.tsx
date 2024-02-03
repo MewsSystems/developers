85d0538f4d712d0d8c0fc801e35040cf5a14767e
Jakub Kares (private) <jakub.kares@gmail.com>
2024-02-03 23:13:25 +0100

    Pagination init

---
+++ jobs/Frontend/movies-work/src/components/Pagination.tsx
@@ -0,0 +1,134 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../contexts/AppContext";
+
+export default function Pagination() {
+  const { page, maximumPage, changePage } = useContext(AppContext);
+  // TODO check css classes and optimize
+  // TODO improve behavior and user experience
+  // TODO implement changePage on onCLick
+  // TODO generate pages with map fn
+
+  const generatePages = (page: number, maximumPage: number) => {
+    if (page === 1 || page === 2) {
+      return [1, 2, 3, "..", "..", maximumPage - 1, maximumPage];
+    }
+    if (page === maximumPage || page === maximumPage - 1) {
+      return [1, 2, "..", "..", maximumPage - 2, maximumPage - 1, maximumPage];
+    }
+    if (page === 3) {
+      return [1, 2, 3, 4, 5, "..", maximumPage];
+    }
+    if (page === maximumPage - 2) {
+      return [
+        1,
+        2,
+        "..",
+        maximumPage - 3,
+        maximumPage - 2,
+        maximumPage - 1,
+        maximumPage,
+      ];
+    }
+    if (page >= 4 || page <= maximumPage - 3) {
+      return [1, 2, "..", 4, "..", maximumPage - 1, maximumPage];
+    }
+
+    const pages = generatePages(page, maximumPage);
+  };
+  return (
+    <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+      <div className="flex flex-1 justify-between sm:hidden">
+        <a
+          href="#"
+          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Previous
+        </a>
+        <a
+          href="#"
+          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+        >
+          Next
+        </a>
+      </div>
+      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-center">
+        {/* <div>
+          <p className="text-sm text-gray-700">
+            Showing <span className="font-medium">1</span> to{" "}
+            <span className="font-medium">10</span> of{" "}
+            <span className="font-medium">97</span> results
+          </p>
+        </div> */}
+        <div>
+          <nav
+            className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            aria-label="Pagination"
+          >
+            <a
+              href="#"
+              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Previous</span>
+              <p className="h-5 w-5" aria-hidden="true">
+                &#60;
+              </p>
+            </a>
+
+            <a
+              href="#"
+              aria-current="page"
+              className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              1
+            </a>
+            <a
+              href="#"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              2
+            </a>
+            <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+              ...
+            </span>
+            <a
+              href="#"
+              className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+            >
+              3
+            </a>
+            <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">
+              ...
+            </span>
+            <a
+              href="#"
+              className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+            >
+              8
+            </a>
+            <a
+              href="#"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              {maximumPage - 1}
+            </a>
+            <a
+              href="#"
+              className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              {maximumPage}
+            </a>
+            <a
+              href="#"
+              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            >
+              <span className="sr-only">Next</span>
+              <p className="h-5 w-5" aria-hidden="true">
+                &#62;
+              </p>
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
--- jobs/Frontend/movies-work/src/pages/SearchPage.tsx
+++ jobs/Frontend/movies-work/src/pages/SearchPage.tsx
@@ -1,11 +1,13 @@
 import InputSearch from "../components/InputSearch";
 import MoviesList from "../components/MoviesList";
+import Pagination from "../components/Pagination";
 
 export default function SearchPage() {
   return (
     <>
       <InputSearch />
       <MoviesList />
+      <Pagination />
     </>
   );
 }
