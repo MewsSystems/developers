b43bc044353bddf9a21d218b48efb531fb7c644a
Jakub Kares (private) <jakub.kares@gmail.com>
2024-02-04 10:44:10 +0100

    Improving search layout

--- jobs/Frontend/movies-work/src/pages/SearchPage.tsx
+++ jobs/Frontend/movies-work/src/pages/SearchPage.tsx
@@ -1,13 +1,20 @@
 import InputSearch from "../components/InputSearch";
 import MoviesList from "../components/MoviesList";
 import Pagination from "../components/Pagination";
+import styles from "./SearchPage.module.css";
 
 export default function SearchPage() {
   return (
-    <>
-      <InputSearch />
-      <MoviesList />
-      <Pagination />
-    </>
+    <div className="flex flex-col max-h-screen ">
+      <div className="">
+        <InputSearch />
+      </div>
+      <div className={styles.moviesList + " flex-grow-1 px-4 mt-2"}>
+        <MoviesList />
+      </div>
+      <div className="">
+        <Pagination />
+      </div>
+    </div>
   );
 }
