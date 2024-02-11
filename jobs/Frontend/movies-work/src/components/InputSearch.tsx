2c4d72acaee6e3b131920fcf922535196778b3bc
Jakub Kares (private) <jakub.kares@gmail.com>
2024-02-11 21:53:10 +0100

    Input text larger to prevent zoom in when focus

--- jobs/Frontend/movies-work/src/components/InputSearch.tsx
+++ jobs/Frontend/movies-work/src/components/InputSearch.tsx
@@ -49,7 +49,7 @@ export default function InputSearch() {
           value={inputValue}
           type="search"
           id="default-search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-4 ps-10 text-lg text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search movies..."
         />
       </div>
