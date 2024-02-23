c5279d7bb9eafd375d70e86053c3076b3380c08e
Reece Scott <reececsscott1996@gmail.com>
2024-02-23 15:53:48 +0000

    feat: pushing basic working poc

--- jobs/Backend/Task/ExchangeRateProvider.cs
+++ jobs/Backend/Task/ExchangeRateProvider.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
 namespace ExchangeRateUpdater
 {
@@ -11,9 +15,27 @@ namespace ExchangeRateUpdater
         /// do not return exchange rate "USD/CZK" with value calculated as 1 / "CZK/USD". If the source does not provide
         /// some of the currencies, ignore them.
         /// </summary>
-        public IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies)
+        public async Task<IEnumerable<ExchangeRate>> GetExchangeRates(IEnumerable<Currency> currencies, string languageCode)
         {
-            return Enumerable.Empty<ExchangeRate>();
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Clear();
+
+            string currentUtcDateString = DateTime.UtcNow.Date.ToString("yyyy-MM-dd");
+
+            ExchangeRateApiResponse exchangeRateApiResponse = await httpClient.GetFromJsonAsync<ExchangeRateApiResponse>(
+        $"https://api.cnb.cz/cnbapi/exrates/daily?date={currentUtcDateString}&lang={languageCode}");
+
+            List<ExchangeRateApiData> filteredExchangeRateData =
+                exchangeRateApiResponse.Rates.Where(x => currencies.Any(y => y.Code == x.CurrencyCode)).ToList();
+
+            var exchangeRateData = new List<ExchangeRate>();
+
+            foreach (ExchangeRateApiData data in filteredExchangeRateData)
+            {
+                exchangeRateData.Add(new ExchangeRate(currencies.FirstOrDefault(x => x.Code == data.CurrencyCode), currencies.FirstOrDefault(x => x.Code == "CZK"), data.Rate));
+            }
+
+            return exchangeRateData;
         }
     }
 }
