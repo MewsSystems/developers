075017410a85b0b2e1c18c584db49f99be3c8644
Tess Sinamal <tess.sinamal@gmail.com>
2024-03-18 21:31:50 +0100

    feat: add input to do the search

--- jobs/Frontend/src/components/MoviesList.tsx
+++ jobs/Frontend/src/components/MoviesList.tsx
@@ -1,19 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import React, {ReactEventHandler, useEffect, useState} from 'react'
 import {searchMovies} from "@/services";
 import {Movie} from "@/types";
+import {Input} from "@mui/material";
 
 export const MoviesList = () => {
     const [movies, setMovies] = useState<Movie[]>([])
+    const [query, setQuery] = useState<string>('')
 
     useEffect(() => {
-        const query = 'Alice'
         searchMovies(query).then((response) => {
             setMovies(response.results)
             }
         )
-    }, []);
+    }, [query]);
+
+    const handleChange = (event) => {
+        const {value} = event.target
+        setQuery(value)
+    }
 
     return <div>
+        <Input onChange={handleChange} />
         {movies.map(
             movie =>
                 <div key={movie.id}>
