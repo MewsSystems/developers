1a9e61b6f8b7d28215cc69f5d8a7bd53bce4757c
Tess Sinamal <tess.sinamal@gmail.com>
2024-03-18 19:46:29 +0100

    build: init project

---
+++ jobs/Frontend/src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { HelloWorld } from './components'
+
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container)
+
+root.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>
+)
