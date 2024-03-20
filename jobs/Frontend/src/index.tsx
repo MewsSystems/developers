eb93536de6beefb44abcecdf4924ba0584df9433
Tess Sinamal <tess.sinamal@gmail.com>
2024-03-20 18:17:23 +0100

    feat: add generic font

--- jobs/Frontend/src/index.tsx
+++ jobs/Frontend/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import {RouterProvider} from "react-router-dom";
 import {router} from "@/routes/router";
+import "./index.css"
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
