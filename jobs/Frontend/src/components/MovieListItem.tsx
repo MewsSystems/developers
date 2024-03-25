c16d33fd5568d9125d4de88d2dc9a1632eaa6de5
Tess Sinamal <tess.sinamal@gmail.com>
2024-03-25 23:24:51 +0100

    feat: add margin to box inside MovieListItem

--- jobs/Frontend/src/components/MovieListItem.tsx
+++ jobs/Frontend/src/components/MovieListItem.tsx
@@ -1,11 +1,16 @@
-import {Box, ListItemButton, styled, Typography} from "@mui/material";
-import {getMoviePosterPath} from "@/utils";
-import React from "react";
-import {useNavigate} from "react-router-dom";
-import {Movie} from "@/types";
+import {
+  Box,
+  ListItemButton,
+  styled,
+  Typography,
+} from '@mui/material'
+import { getMoviePosterPath } from '@/utils'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Movie } from '@/types'
 
 interface MovieListItemProps {
-    movie: Movie
+  movie: Movie
 }
 
 const posterOriginalWidth = 200
@@ -14,36 +19,41 @@ const posterHeight = posterOriginalWidth / 2.66
 const paddingListItem = 8
 
 const StyledListItemButton = styled(ListItemButton)`
-    padding: ${paddingListItem}px;
+  padding: ${paddingListItem}px;
 `
 
-const StyledBox = styled(Box)`
-    width: ${posterWidth}px;
-    height: ${posterHeight}px;
-    
-    &.default-img {
-        background-color: gray;
-    }
-`
+const StyledBox = styled(Box)(({ theme }) => ({
+  width: posterWidth + 'px',
+  height: posterHeight + 'px',
+  marginRight: theme.spacing(2),
 
-export const MovieListItem = ({movie}: MovieListItemProps) => {
-    const navigate = useNavigate()
-    const { id, poster_path, title } = movie
+  '&.default-img': {
+    backgroundColor: 'grey',
+  },
+}))
 
-    const handleClick = (id: number) => {
-        navigate(`/movie/${id}`)
-    }
+export const MovieListItem = ({ movie }: MovieListItemProps) => {
+  const navigate = useNavigate()
+  const { id, poster_path, title } = movie
 
-    return <StyledListItemButton
-        onClick={() => handleClick(id)}
-    >
-        {poster_path ?
-            <StyledBox component="img" src={getMoviePosterPath(posterOriginalWidth, poster_path)} alt={title} />
-         :
-            <StyledBox className={'default-img'} />
-        }
-        <Typography color="textSecondary" variant="body1">
-            {title}
-        </Typography>
+  const handleClick = (id: number) => {
+    navigate(`/movie/${id}`)
+  }
+
+  return (
+    <StyledListItemButton onClick={() => handleClick(id)}>
+      {poster_path ? (
+        <StyledBox
+          component="img"
+          src={getMoviePosterPath(posterOriginalWidth, poster_path)}
+          alt={title}
+        />
+      ) : (
+        <StyledBox className={'default-img'} />
+      )}
+      <Typography color="textSecondary" variant="body1">
+        {title}
+      </Typography>
     </StyledListItemButton>
+  )
 }
