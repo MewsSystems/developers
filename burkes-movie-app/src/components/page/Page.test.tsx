c0b90240113616b18a9bf270f49f883a489adb74
Burkes321 <sam.burke99@gmail.com>
2024-03-25 18:18:03 +0100

    [MEWS-007] fix failing test

--- burkes-movie-app/src/components/page/Page.test.tsx
+++ burkes-movie-app/src/components/page/Page.test.tsx
@@ -1,13 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import { Page } from './Page';
+import { PageContainer } from './Page';
 
 describe('Page', () => {
   it('renders children correctly', () => {
     const testMessage = 'test message';
 
-    render(<Page>{testMessage}</Page>);
+    render(<PageContainer>{testMessage}</PageContainer>);
 
     expect(screen.getByText('test message')).toBeInTheDocument();
   });
