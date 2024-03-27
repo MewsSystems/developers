f43c7ac0fd3bea9e67d7218e7843154fed7f200d
smurawczik <sebastian.murawczik@gmail.com>
2024-03-27 16:40:24 +0100

    add MUI and sass libs

--- jobs/Frontend/src/main.tsx
+++ jobs/Frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { CssBaseline } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./index.scss";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
