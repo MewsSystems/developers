b620f334321241714ef8889dcf28aa11bb5124b0
smurawczik <sebastian.murawczik@gmail.com>
2024-03-29 14:30:24 +0100

    add search input test

--- jobs/Frontend/.eslintrc.cjs
+++ jobs/Frontend/.eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
   plugins: ['react-refresh'],
   overrides: [
     {
-      files: ['./src/api/instance.ts'],
+      files: ['./src/api/instance.ts', './src/**/*.test.*'],
       rules: { 'no-restricted-imports': 'off' },
     }
   ],
---
+++ jobs/Frontend/src/components/MovieSearch/components/SearchInput.test.tsx
@@ -0,0 +1,13 @@
+// make a test that verifies the input changes value and that an api call is being made
+
+import { fireEvent, render, screen } from "../../../test/utils";
+import { SearchInput } from "./SearchInput";
+
+describe("SearchInput", () => {
+  it("input changes value and an api call is made", async () => {
+    render(<SearchInput />);
+    const input = screen.getByPlaceholderText(/Search for a movie/i);
+    fireEvent.change(input, { target: { value: "batman" } });
+    expect(input).toHaveValue("batman");
+  });
+});
