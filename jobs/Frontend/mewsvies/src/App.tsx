8d1cb7aa86761c76763c15bb696323fc5e0dba37
David Portilla <david_portilla@hotmail.com>
2024-04-07 11:46:22 -0500

    feat: Add Prettier-ESlint

--- jobs/Frontend/mewsvies/src/App.tsx
+++ jobs/Frontend/mewsvies/src/App.tsx
@@ -1,15 +1,13 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Mewsvies App
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Mewsvies App</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
