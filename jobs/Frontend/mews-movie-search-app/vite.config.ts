9181f787d260556d1a56e819d08ad8e5466b0900
Alexis Troncoso <alexis@foundernest.com>
2024-04-18 10:19:35 +0200

    🔧 chore: configure testing

--- jobs/Frontend/mews-movie-search-app/vite.config.ts
+++ jobs/Frontend/mews-movie-search-app/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    // include: ["./{src}/**/*.test.{ts,tsx}"],
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
+  },
+});
+
+// /// <reference types="vitest" />
+// /// <reference types="vite/client" />
+// import { defineConfig } from "vitest/config";
+// import react from "@vitejs/plugin-react-swc";
+// import "@testing-library/jest-dom/vitest";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
