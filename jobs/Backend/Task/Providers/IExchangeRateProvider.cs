511b9cb634c6d714c7d33691b9a27d8c02394817
Dmitry Alekhin <alekhindmitry2212@gmail.com>
2024-04-25 15:33:24 +0200

    exchRateUpd

---
+++ jobs/Backend/Task/Providers/IExchangeRateProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRateUpdater.Models;
+
+namespace ExchangeRateUpdater.Providers
+{
+    public interface IExchangeRateProvider
+    {
+        Task<IEnumerable<ExchangeRate?>> GetExchangeRates(IEnumerable<Currency> currencies);
+    }
+}
