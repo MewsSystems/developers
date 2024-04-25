072af20eb3ec0c7367ea8b83501e865a176cb666
fcarmona <fcarmona.olmedo@gmail.com>
2024-04-25 13:02:51 +0200

    chore: configure Vitest for testing

--- jobs/Frontend/fe-challenge/vite.config.ts
+++ jobs/Frontend/fe-challenge/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/setupTests.ts'],
+  },
+});
