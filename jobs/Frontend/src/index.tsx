ee3f59820c6f7c51778b060e68d35a0ee132d2c8
Fatima Santos <fatima.santos@wefox.com>
2024-04-27 15:14:00 +0200

    chore: added eslint and prettier config files

--- jobs/Frontend/src/index.tsx
+++ jobs/Frontend/src/index.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById("app");
+const container = document.getElementById('app');
 const root = createRoot(container);
 root.render(<p>Hello World</p>);
