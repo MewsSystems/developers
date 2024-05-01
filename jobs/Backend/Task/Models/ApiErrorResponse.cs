a4cc4e27c71e89b28d1bae90f153ae02befe001b
darsruiz <darsruiz@gmail.com>
2024-05-01 21:36:46 +0200

    Moved Models to folder

---
+++ jobs/Backend/Task/Models/ApiErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExchangeRateUpdater.Models
+{
+    public class ApiErrorResponse
+    {
+        public string Description { get; set; }
+        public string EndPoint { get; set; }
+        public string ErrorCode { get; set; }
+        public DateTime HappenedAt { get; set; }
+        public string MessageId { get; set; }
+    }
+}
---
+++ jobs/Backend/Task/Models/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ExchangeRateUpdater.Models
+{
+    public class ApiResponse
+    {
+        public List<ApiExchangeRate> Rates { get; set; }
+    }
+}
