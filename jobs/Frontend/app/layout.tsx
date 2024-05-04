0f5948714aed8eb0c7fb4a15349b7d17e86bfae1
Jake Towers <JakeTowers@users.noreply.github.com>
2024-05-04 13:37:06 +0100

    Add Styled Components

--- jobs/Frontend/app/layout.tsx
+++ jobs/Frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import StyledComponentsRegistry from "@/lib/registry";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
---
+++ jobs/Frontend/components/TestComponent.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 1px solid black;
+`;
+
+const TestComponent = () => {
+  return <Container>Test</Container>;
+};
+
+export default TestComponent;
