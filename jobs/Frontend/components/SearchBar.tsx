023f9195af275efc22b019d0e9862b7c426b4f7b
Jake Towers <JakeTowers@users.noreply.github.com>
2024-05-05 16:20:16 +0100

    Remove TODO

--- jobs/Frontend/components/SearchBar.tsx
+++ jobs/Frontend/components/SearchBar.tsx
@@ -62,7 +62,7 @@ export default SearchBar;
  * Additionally fetching data in useEffect is not recommended - https://react.dev/reference/react/useEffect#what-are-good-alternatives-to-data-fetching-in-effects
  * Recommendation with Next is to use queryParams, and this allows us to make the request server-side
  *
- * TODO - Debouncing:
+ * Debouncing:
  * To debounce the request so it's not called until the user finishes typing - https://github.com/xnimorz/use-debounce
  * Why? Prevents unnecessary trips to the API while the user is typing
  * This could be written as a custom hook that is shared and to save on import size, but for the purposes of this exercise I'd prefer to save the time
