5cefeafe189a7aa1290809332e0896b03ae4be9b
hhoffmann-dev <hhoffmann@tuta.io>
2024-05-08 20:37:22 +0200

    Improve conditional rendering

--- jobs/Frontend/movie-search/src/features/movies/components/MovieSearch/MovieSearch.tsx
+++ jobs/Frontend/movie-search/src/features/movies/components/MovieSearch/MovieSearch.tsx
@@ -27,6 +27,7 @@ import { Movie } from '../../models/Movie';
 import EnhancedTable from './EnhancedTable/EnhancedTable';
 
 export default function MovieSearch() {
+  const initialPage = '1';
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -36,7 +37,7 @@ export default function MovieSearch() {
   const [searchParams, setSearchParams] = useSearchParams();
   const [query, setQuery] = useState(searchParams.get('query') || '');
   const [debouncedQuery, setDebouncedQuery] = useState('');
-  const [page, setPage] = useState(searchParams.get('page') || '1');
+  const [page, setPage] = useState(searchParams.get('page') || initialPage);
   const [pageRange, setPageRange] = useState<number[]>([]);
   const [movies, setMovies] = useState<Movie[]>([]);
 
@@ -64,26 +65,93 @@ export default function MovieSearch() {
     setPageRange(Array.from({ length: data?.total_pages ?? 0 }, (_, i) => i + 1));
   }, [data]);
 
-  const shouldRenderError = useDelayedRender(error !== null);
-  const shouldRenderLoading = useDelayedRender(isLoading);
+  const shouldRenderError = error !== null;
+  const shouldRenderLoading = isLoading || isFetching;
+  const shouldRenderMovies = !(isLoading || isFetching) && movies.length > 0;
+  const shouldRenderNoMovies = useDelayedRender(!error && movies.length === 0 && query !== '', 500);
+
+  const setUrlParams = (page: string, replaceOption = false) => {
+    setPage(page);
+    setSearchParams({ query: query, page: page }, { replace: replaceOption });
+  };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => setQuery(event.target.value);
-  const handleSearch = () => setSearchParams({ query: query, page: page }, { replace: true });
-
-  const setNewPage = (page: string) => {
-    setPage(page);
-    setSearchParams({ query: query, page: page });
-  };
-
-  const handleNextPage = () => {
-    if (data && data?.page < data?.total_pages) setNewPage(`${++data.page}`);
-  };
+  const handleSearch = () => setUrlParams(initialPage, true);
 
   const handlePreviousPage = () => {
-    if (data && data?.page > 1) setNewPage(`${--data.page}`);
+    if (data && data?.page > 1) setUrlParams(`${--data.page}`);
+  };
+  const handleNextPage = () => {
+    if (data && data?.page < data?.total_pages) setUrlParams(`${++data.page}`);
   };
 
-  const handleSelectChange = (event: SelectChangeEvent) => setNewPage(event.target.value);
+  const handleSelectChange = (event: SelectChangeEvent) => setUrlParams(event.target.value);
+
+  const ContentToRender = () => {
+    if (shouldRenderLoading) {
+      return <CircularProgress sx={{ m: 4 }} />;
+    } else if (shouldRenderError) {
+      return (
+        <Paper sx={{ m: 2, p: 3, borderRadius: 2 }} elevation={3}>
+          <Typography data-testid="movie-search-error" variant="h6" color="error">
+            Error: {error?.message}
+          </Typography>
+        </Paper>
+      );
+    } else if (shouldRenderNoMovies) {
+      return (
+        <Paper sx={{ m: 2, p: 3, borderRadius: 2 }} elevation={3}>
+          <Typography data-testid="movie-search-no-movies" variant="h6" color="info.main">
+            {t('error.noMoviesMatch')}
+          </Typography>
+        </Paper>
+      );
+    } else if (shouldRenderMovies) {
+      return (
+        <Stack direction="column" spacing={2} alignItems="center" divider={<Divider orientation="vertical" />}>
+          <EnhancedTable rows={movies} />
+
+          <Stack direction="row" spacing={6} alignItems="center">
+            <Button
+              sx={{ width: 175, height: 45 }}
+              size="large"
+              variant="outlined"
+              startIcon={<KeyboardArrowLeftIcon />}
+              onClick={handlePreviousPage}
+              disabled={data?.page === 1}>
+              {t('common.previousPage')}
+            </Button>
+
+            <FormControl variant="filled" sx={{ m: 1, minWidth: 80 }} size="small">
+              <InputLabel id="movie-search-page-select-label">{`/ ${data?.total_pages}`}</InputLabel>
+              <Select
+                id="movie-search-page-select"
+                labelId="movie-search-page-select-label"
+                value={page}
+                label="Page"
+                onChange={handleSelectChange}>
+                {pageRange.map(page => (
+                  <MenuItem key={`movie-search-select-item-${page}`} value={page}>
+                    {page}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <Button
+              sx={{ width: 175, height: 45 }}
+              size="large"
+              variant="outlined"
+              endIcon={<KeyboardArrowRightIcon />}
+              onClick={handleNextPage}
+              disabled={data?.page === data?.total_pages}>
+              {t('common.nextPage')}
+            </Button>
+          </Stack>
+        </Stack>
+      );
+    }
+  };
 
   return (
     <>
@@ -105,64 +173,7 @@ export default function MovieSearch() {
         />
       </Paper>
 
-      {shouldRenderLoading && <CircularProgress sx={{ m: 4 }} />}
-
-      {shouldRenderError && (
-        <>
-          <CircularProgress sx={{ m: 4 }} />
-          <Paper sx={{ m: 2, p: 3, borderRadius: 2 }} elevation={3}>
-            <Typography data-testid="movie-search-error" variant="h6" color="error">
-              Error: {error?.message}
-            </Typography>
-          </Paper>
-        </>
-      )}
-
-      {movies.length > 0 && (
-        <Stack direction="column" spacing={2} alignItems="center" divider={<Divider orientation="vertical" />}>
-          <EnhancedTable rows={movies} />
-
-          {isFetching && <CircularProgress sx={{ m: 4 }} />}
-
-          {!isFetching && (
-            <Stack direction="row" spacing={6} alignItems="center">
-              <Button
-                sx={{ width: 175, height: 45 }}
-                size="large"
-                variant="outlined"
-                startIcon={<KeyboardArrowLeftIcon />}
-                onClick={handlePreviousPage}
-                disabled={data?.page === 1}>
-                {t('common.previousPage')}
-              </Button>
-
-              <FormControl variant="filled" sx={{ m: 1, minWidth: 80 }} size="small">
-                <InputLabel id="movie-search-page-select-label">{`/ ${data?.total_pages}`}</InputLabel>
-                <Select
-                  id="movie-search-page-select"
-                  labelId="movie-search-page-select-label"
-                  value={page}
-                  label="Page"
-                  onChange={handleSelectChange}>
-                  {pageRange.map(page => (
-                    <MenuItem value={page}>{page}</MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-
-              <Button
-                sx={{ width: 175, height: 45 }}
-                size="large"
-                variant="outlined"
-                endIcon={<KeyboardArrowRightIcon />}
-                onClick={handleNextPage}
-                disabled={data?.page === data?.total_pages}>
-                {t('common.nextPage')}
-              </Button>
-            </Stack>
-          )}
-        </Stack>
-      )}
+      <ContentToRender />
 
       <Footer />
     </>
