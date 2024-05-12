2ede2bbd88288c15e4f6f54d662327b72064d573
James Shore <james@jamesshore.co.uk>
2024-05-12 19:17:01 +0100

    created a no movies component, this relays information to the user about the state of the search

--- jobs/Frontend/movie-search/src/app/page.tsx
+++ jobs/Frontend/movie-search/src/app/page.tsx
@@ -4,6 +4,7 @@ import { fetchMovies } from "@/data/FetchMovies";
 import MovieCard from "@/components/MovieCard/MovieCard";
 import MovieCardContainer from "@/components/MovieCard/MovieCardContainer";
 import MovieHeader from "@/components/MovieHeader/MovieHeader";
+import NoMoviesComponent from "@/components/NoMovies/NoMovies";
 
 export const metadata: Metadata = {
   title: "MEWS Movie Search",
@@ -24,7 +25,19 @@ export default async function Home({ searchParams }: HomePageProps) {
   return (
     <main>
       <MovieHeader />
-      {movies.length > 0 && (
+      {!searchParams.searchterm && (
+        <NoMoviesComponent
+          message="Search For Movies"
+          additionalText="Use the search bar to find movies"
+        />
+      )}
+      {movies.length === 0 && searchParams.searchterm && (
+        <NoMoviesComponent
+          message="No movies found"
+          additionalText="Try searching for another movie"
+        />
+      )}
+      {searchParams.searchterm && movies.length > 0 && (
         <MovieCardContainer>
           {movies.map((movie, index) => (
             <MovieCard key={movie.id} index={index} movie={movie} />
