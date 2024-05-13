475264291f2dce0327ebbde152206a0f3fac918f
James Shore <james@jamesshore.co.uk>
2024-05-13 20:27:42 +0100

    more code tidy up

--- jobs/Frontend/movie-search/src/components/MovieDetails/MovieDetailsStyledComponents.ts
+++ jobs/Frontend/movie-search/src/components/MovieDetails/MovieDetailsStyledComponents.ts
@@ -2,25 +2,25 @@ import styled from "styled-components";
 
 export const MainContainer = styled.main`
   padding: 20px;
-  background-color: #ecf0f1; // Light grey, consistent with the rest of your app
-  color: #2c3e50; // Dark grey text for readability
+  background-color: #ecf0f1;
+  color: #2c3e50;
 `;
 
 export const Title = styled.h1`
-  color: #34495e; // Dark blue for titles
+  color: #34495e;
   text-align: center;
-  margin-bottom: 20px; // Adds space below the title
+  margin-bottom: 20px;
 `;
 
 export const Text = styled.p`
   font-size: 16px;
   line-height: 1.5;
-  margin-bottom: 10px; // Adds space below each paragraph
+  margin-bottom: 10px;
 `;
 
 export const MoviePoster = styled.img`
   display: block;
-  margin: 20px auto; // Centers the image horizontally
-  max-width: 200px; // Fixed width for consistency
-  border-radius: 8px; // Rounded corners for the image
+  margin: 20px auto;
+  max-width: 200px;
+  border-radius: 8px;
 `;
