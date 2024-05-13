d06c95acdd1240a2aa7959b6f54b35fb8181e18e
Robert Fiteni <robertfiteni@hotmail.com>
2024-05-14 01:03:28 +0200

    Created constants.

---
+++ jobs/Frontend/movie-search/src/constants/index.ts
@@ -0,0 +1,19 @@
+// used by components: App, MovieListItem and MovieDetail 
+export const SEARCH_VIEW_ROUTE_PATH = '/';
+export const MOVIE_DETAIL_VIEW_ROUTE_PATH = '/movies/:id';
+export const MOVIE_DETAIL_PATH = '/movies/';
+
+// used by hook: useMovies
+export const API_KEY = '03b8572954325680265531140190fd2a';
+export const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+// used by utility: DateFormatter
+export const DATE_INPUT_FORMAT = 'YYYY-MM-DD';
+export const DATE_OUTPUT_FORMAT = 'dddd, Do MMMM YYYY';
+
+// used by components: MovieList and MovieDetail
+export const IMAGE_POSTER_THUMBNAIL_URL = 'https://image.tmdb.org/t/p/w92'
+export const IMAGE_ORIGINAL_URL = 'https://image.tmdb.org/t/p/original'
+export const IMAGE_THUMBNAIL_UNAVAILABLE_URL = 'https://placehold.co/92x138?text=Image+Unavailable'
+export const IMAGE_BACKGROUND_UNAVAILABLE_URL = 'https://placehold.co/600?text=Image+Unavailable'
+export const OVERVIEW_UNAVAILABLE_TEXT = 'Plot Unknown'
