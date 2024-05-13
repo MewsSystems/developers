f5d926f7ffe6e6f39aa511f0fd10e999f7013c59
lucijajaksic <lucia.jaksic@gmail.com>
2024-05-13 18:27:10 +0200

    Backend developer task

---
+++ jobs/Backend/Task/ExchangeRateAPI/ExchangeRateMemoryCache.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace ExchangeRateUpdater.ExchangeRateAPI
+{
+    public class ExchangeRateMemoryCache
+    {
+        public MemoryCache Cache { get; } = new MemoryCache(
+            new MemoryCacheOptions
+            {
+                SizeLimit = 1024
+            });
+    }
+}
