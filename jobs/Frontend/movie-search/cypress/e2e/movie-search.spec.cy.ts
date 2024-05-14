452db81a5dada0d267fa470b110814be055133dc
Robert Fiteni <robertfiteni@hotmail.com>
2024-05-14 10:35:50 +0200

    Revised e2e tests.

--- jobs/Frontend/movie-search/cypress/e2e/movie-search.spec.cy.ts
+++ jobs/Frontend/movie-search/cypress/e2e/movie-search.spec.cy.ts
@@ -1,7 +1,25 @@
-describe('Movie Search application', () => {
-  it('Visits Movie Search', () => {
-    cy.visit('http://localhost:3000/');
-    cy.get('h2[data-test=heading]').contains('Movie Search')
-  })
+const gotoApp = () => {
+  cy.visit('http://localhost:3000/');
+}
+
+beforeEach(() => {
+  gotoApp();
 });
+
+describe('Movie Search application', () => {
+  it('displays the logo link', () => {
+    cy.get('.logo-link').should('exist');
+  });
+
+  it('navigates to SearchView route by default', () => {
+    cy.get('.search-view').should('exist');
+  });
+
+  it('displays search box', () => {
+    cy.get('input[type="text"]').should('exist');
+  });
+});
+
+
+
 
