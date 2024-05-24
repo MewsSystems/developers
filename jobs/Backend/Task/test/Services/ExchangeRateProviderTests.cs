e7c04a5e97e12e94abe382bcc907b64ef78ae6ad
Gerard Vilaprinyo <gvilaprinyo@outlook.com>
2024-05-24 19:41:58 +0200

    test

--- jobs/Backend/Task/test/Services/ExchangeRateProviderTests.cs
+++ jobs/Backend/Task/test/Services/ExchangeRateProviderTests.cs
@@ -1,6 +1,25 @@
-﻿namespace ExchangeRatesTests.Services
+﻿using Xunit;
+using ExchangeRateUpdater.Models;
+
+namespace ExchangeRatesTests.Services
 {
     public class ExchangeRateProviderTests
     {
+    
+    [Fact]
+    public async Task GetExchangeRates_ShouldReturnExchangeRates_WhenCurrenciesAreProvided()
+    {
+        // Arrange
+        var cancellationToken = CancellationToken.None;
+
+        var cnbRates = new CnbRates
+        {
+            Rates = new List<CnbRate>
+            {
+                new CnbRate { CurrencyCode = "USD", Rate = 22.5m, Amount = 1 },
+                new CnbRate { CurrencyCode = "EUR", Rate = 25.5m, Amount = 1 }
+            }
+        };
     }
+    }    
 }
