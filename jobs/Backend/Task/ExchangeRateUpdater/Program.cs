8ca9b3c313c43b01b060416622857e08cdcdc6c8
roro <roro@roros-MBP.attlocal.net>
2024-07-08 19:06:47 -0400

    add Serilog

--- jobs/Backend/Task/ExchangeRateUpdater/Program.cs
+++ jobs/Backend/Task/ExchangeRateUpdater/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace ExchangeRateUpdater
 {
@@ -8,6 +11,20 @@ namespace ExchangeRateUpdater
     {
         static async Task Main(string[] args)
         {
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            // Create a service collection and configure logging
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            // Get the logger
+            var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
             IExchangeRateService exchangeRateService = new CnbExchangeRateService();
             IExchangeRateProvider exchangeRateProvider = new ExchangeRateProvider(exchangeRateService);
 
@@ -24,12 +41,25 @@ namespace ExchangeRateUpdater
                 new Currency("XYZ")
             };
 
-            var exchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(currencies);
-
-            foreach (var rate in exchangeRates)
+            try
             {
-                Console.WriteLine(rate);
+                var exchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(currencies);
+
+                foreach (var rate in exchangeRates)
+                {
+                    logger.LogInformation(rate.ToString());
+                }
             }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while fetching exchange rates.");
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            // Add logging
+            services.AddLogging(configure => configure.AddSerilog());
         }
     }
 }
