82b8c96fbdabe7c435efbe088c92ab009607b79f
roro <roro@roros-MBP.attlocal.net>
2024-07-08 20:08:23 -0400

    add tests for Currency and ExchangeRate

--- jobs/Backend/Task/ExchangeRateUpdater.Tests/ExchangeRateUpdater.Tests.cs
+++ jobs/Backend/Task/ExchangeRateUpdater.Tests/ExchangeRateUpdater.Tests.cs
@@ -2,10 +2,208 @@ using Moq;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Xunit;
+using System;
 
 namespace ExchangeRateUpdater.Tests
 {
+
+    public class ExchangeRateTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeExchangeRate_WhenParametersAreValid()
+        {
+            // Arrange
+            var sourceCurrency = new Currency("USD");
+            var targetCurrency = new Currency("EUR");
+            var value = 0.85m;
+
+            // Act
+            var exchangeRate = new ExchangeRate(sourceCurrency, targetCurrency, value);
+
+            // Assert
+            exchangeRate.SourceCurrency.Should().Be(sourceCurrency);
+            exchangeRate.TargetCurrency.Should().Be(targetCurrency);
+            exchangeRate.Value.Should().Be(value);
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentNullException_WhenSourceCurrencyIsNull()
+        {
+            // Arrange
+            Currency sourceCurrency = null;
+            var targetCurrency = new Currency("EUR");
+            var value = 0.85m;
+
+            // Act
+            Action act = () => new ExchangeRate(sourceCurrency, targetCurrency, value);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>()
+                .WithMessage("Source currency cannot be null. (Parameter 'sourceCurrency')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentNullException_WhenTargetCurrencyIsNull()
+        {
+            // Arrange
+            var sourceCurrency = new Currency("USD");
+            Currency targetCurrency = null;
+            var value = 0.85m;
+
+            // Act
+            Action act = () => new ExchangeRate(sourceCurrency, targetCurrency, value);
+
+            // Assert
+            act.Should().Throw<ArgumentNullException>()
+                .WithMessage("Target currency cannot be null. (Parameter 'targetCurrency')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentOutOfRangeException_WhenValueIsZero()
+        {
+            // Arrange
+            var sourceCurrency = new Currency("USD");
+            var targetCurrency = new Currency("EUR");
+            var value = 0m;
+
+            // Act
+            Action act = () => new ExchangeRate(sourceCurrency, targetCurrency, value);
+
+            // Assert
+            act.Should().Throw<ArgumentOutOfRangeException>()
+                .WithMessage("Exchange rate value must be greater than zero. (Parameter 'value')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentOutOfRangeException_WhenValueIsNegative()
+        {
+            // Arrange
+            var sourceCurrency = new Currency("USD");
+            var targetCurrency = new Currency("EUR");
+            var value = -1m;
+
+            // Act
+            Action act = () => new ExchangeRate(sourceCurrency, targetCurrency, value);
+
+            // Assert
+            act.Should().Throw<ArgumentOutOfRangeException>()
+                .WithMessage("Exchange rate value must be greater than zero. (Parameter 'value')");
+        }
+
+        [Fact]
+        public void ToString_ShouldReturnCorrectFormat()
+        {
+            // Arrange
+            var sourceCurrency = new Currency("USD");
+            var targetCurrency = new Currency("EUR");
+            var value = 0.85m;
+            var exchangeRate = new ExchangeRate(sourceCurrency, targetCurrency, value);
+
+            // Act
+            var result = exchangeRate.ToString();
+
+            // Assert
+            result.Should().Be("USD/EUR=0.85");
+        }
+    }
+
+    public class CurrencyTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeCurrency_WhenCodeIsValid()
+        {
+            // Arrange
+            var validCode = "USD";
+
+            // Act
+            var currency = new Currency(validCode);
+
+            // Assert
+            currency.Code.Should().Be(validCode);
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentException_WhenCodeIsNull()
+        {
+            // Act
+            Action act = () => new Currency(null);
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Currency code cannot be null or empty. (Parameter 'code')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentException_WhenCodeIsEmpty()
+        {
+            // Act
+            Action act = () => new Currency(string.Empty);
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Currency code cannot be null or empty. (Parameter 'code')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentException_WhenCodeIsWhitespace()
+        {
+            // Act
+            Action act = () => new Currency("   ");
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Currency code cannot be null or empty. (Parameter 'code')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentException_WhenCodeIsTooShort()
+        {
+            // Act
+            Action act = () => new Currency("US");
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Currency code must be a three-letter ISO 4217 code consisting of uppercase letters. (Parameter 'code')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentException_WhenCodeIsTooLong()
+        {
+            // Act
+            Action act = () => new Currency("USDE");
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Currency code must be a three-letter ISO 4217 code consisting of uppercase letters. (Parameter 'code')");
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowArgumentException_WhenCodeIsLowercase()
+        {
+            // Act
+            Action act = () => new Currency("usd");
+
+            // Assert
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("Currency code must be a three-letter ISO 4217 code consisting of uppercase letters. (Parameter 'code')");
+        }
+
+        [Fact]
+        public void ToString_ShouldReturnCode()
+        {
+            // Arrange
+            var validCode = "USD";
+            var currency = new Currency(validCode);
+
+            // Act
+            var result = currency.ToString();
+
+            // Assert
+            result.Should().Be(validCode);
+        }
+    }
     public class ExchangeRateProviderTests
     {
         [Fact]
