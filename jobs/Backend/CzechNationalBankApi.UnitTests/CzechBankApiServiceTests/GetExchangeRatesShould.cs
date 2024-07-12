54cc266ecde3749e6cbc3ea11822211e0243960d
Daniel Wood <thenabster@hotmail.co.uk>
2024-07-12 16:23:01 +0100

    Implemented CzechBankApiService to satisfy pre-written unit tests. All passing.

--- jobs/Backend/CzechNationalBankApi.UnitTests/CzechBankApiServiceTests/GetExchangeRatesShould.cs
+++ jobs/Backend/CzechNationalBankApi.UnitTests/CzechBankApiServiceTests/GetExchangeRatesShould.cs
@@ -1,5 +1,7 @@
+using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 
 namespace CzechNationalBankApi.UnitTests.CzechBankApiServiceTests
 {
@@ -12,21 +14,75 @@ namespace CzechNationalBankApi.UnitTests.CzechBankApiServiceTests
 
         public GetExchangeRatesShould()
         {
-            _httpClient = new HttpClient(_mockHttpMessageHandler);
+            _httpClient = new HttpClient(_mockHttpMessageHandler) { BaseAddress = new Uri("https://localhost/") };
 
             _czechBankApiService = new CzechBankApiService(_logger, _httpClient);
         }
 
         [Fact]
-        public async Task ReturnExpectedData()
+        public async Task ReturnsEmpty_WhenApiReturnsNoData()
         {
+            _mockHttpMessageHandler.MockSend(Arg.Is<HttpRequestMessage>(x => x.RequestUri!.OriginalString.Contains("daily.txt")), Arg.Any<CancellationToken>())
+               .Returns(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(string.Empty) });
 
+            _mockHttpMessageHandler.MockSend(Arg.Is<HttpRequestMessage>(x => x.RequestUri!.OriginalString.Contains("fx_rates.txt")), Arg.Any<CancellationToken>())
+                .Returns(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(string.Empty) });
+
+            var actual = await _czechBankApiService.GetExchangeRatesAsync();
+
+            actual.Should().BeEmpty();
         }
 
         [Fact]
-        public async Task ThrowExceptionAndNotSwallow()
+        public async Task ReturnExpectedData_MapsCorrectlyFromApiTextFormat()
         {
+            var dummyApiDailyResponseData = @$"11 Jul 2024 #133{Environment.NewLine}Country|Currency|Amount|Code|Rate{Environment.NewLine}Australia|dollar|1|AUD|15.777";
 
+            var dummyApiFxRatesResponseData = @$"28 Jun 2024 #6{Environment.NewLine}Country|Currency|Amount|Code|Rate{Environment.NewLine}Afghanistan|afghani|100|AFN|32.957{Environment.NewLine}Albania|lek|100|ALL|25.052";
+
+            _mockHttpMessageHandler.MockSend(Arg.Is<HttpRequestMessage>(x => x.RequestUri!.OriginalString.Contains("daily.txt")), Arg.Any<CancellationToken>())
+                .Returns(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(dummyApiDailyResponseData) });
+
+            _mockHttpMessageHandler.MockSend(Arg.Is<HttpRequestMessage>(x => x.RequestUri!.OriginalString.Contains("fx_rates.txt")), Arg.Any<CancellationToken>())
+                .Returns(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(dummyApiFxRatesResponseData) });
+
+            var expected = new List<CzechExchangeItemDto>() { 
+                new CzechExchangeItemDto { Country = "Australia", Currency = "dollar", Amount = 1, Code = "AUD", Rate = 15.777m },
+                new CzechExchangeItemDto { Country = "Afghanistan", Currency = "afghani", Amount = 100, Code = "AFN", Rate = 32.957m },
+                new CzechExchangeItemDto { Country = "Albania", Currency = "lek", Amount = 100, Code = "ALL", Rate = 25.052m },
+            };
+
+            var actual = await _czechBankApiService.GetExchangeRatesAsync();
+
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public async Task ThrowExceptionAndNotSwallow_WhenApiDataCannotBeParsedIntoType()
+        {
+            _mockHttpMessageHandler.MockSend(Arg.Any<HttpRequestMessage>(), Arg.Any<CancellationToken>()).Throws(new Exception("dummy-exception"));
+
+            var actual = async () => await _czechBankApiService.GetExchangeRatesAsync();
+
+            await actual.Should().ThrowAsync<Exception>();
+        }
+
+        [Fact]
+        public async Task ThrowsExceptionWhenDuplicateCurrencyInfoFound()
+        {
+            var dummyApiDailyResponseData = @$"11 Jul 2024 #133{Environment.NewLine}Country|Currency|Amount|Code|Rate{Environment.NewLine}Australia|dollar|1|AUD|15.777";
+
+            var dummyApiFxRatesResponseData = @$"28 Jun 2024 #6{Environment.NewLine}Country|Currency|Amount|Code|Rate{Environment.NewLine}Afghanistan|afghani|100|AFN|32.957{Environment.NewLine}Albania|lek|100|ALL|25.052{Environment.NewLine}Australia|dollar|1|AUD|15.777";
+
+            _mockHttpMessageHandler.MockSend(Arg.Is<HttpRequestMessage>(x => x.RequestUri!.OriginalString.Contains("daily.txt")), Arg.Any<CancellationToken>())
+                .Returns(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(dummyApiDailyResponseData) });
+
+            _mockHttpMessageHandler.MockSend(Arg.Is<HttpRequestMessage>(x => x.RequestUri!.OriginalString.Contains("fx_rates.txt")), Arg.Any<CancellationToken>())
+                .Returns(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(dummyApiFxRatesResponseData) });
+
+            var actual = _czechBankApiService.GetExchangeRatesAsync;
+
+            await actual.Should().ThrowAsync<Exception>().WithMessage("Duplicate currency code information found");
         }
     }
 }
--- jobs/Backend/CzechNationalBankApi/CzechBankApiService.cs
+++ jobs/Backend/CzechNationalBankApi/CzechBankApiService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace CzechNationalBankApi
 {
@@ -19,13 +21,57 @@ namespace CzechNationalBankApi
 
         public async Task<IEnumerable<CzechExchangeItemDto>> GetExchangeRatesAsync()
         {
+            var czechItems = new List<CzechExchangeItemDto>();
+
             //Two stage check - encapsulate within one method
             //  Some currencies are main currencies, then there are "other" ones. Just check both inside the method and do out best to return something :-)
             //      https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt?date=11.07.2024
             //  Second check, and final: https://www.cnb.cz/en/financial-markets/foreign-exchange-market/fx-rates-of-other-currencies/fx-rates-of-other-currencies/fx_rates.txt?year=2024&month=6
             //All values from the two api endpoints come back like: Country|Currency|Amount|Code|Rate
 
-            return Enumerable.Empty<CzechExchangeItemDto>();
+            var date = DateTime.UtcNow;
+
+            var dailyResponse = await _httpClient.GetAsync($"en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt?date={date:dd.MM.yyyyy}");
+
+            dailyResponse.EnsureSuccessStatusCode();
+
+            var dailyResponseStream = await dailyResponse.Content.ReadAsStreamAsync();
+
+            czechItems.AddRange(ParseStreamAsCSV(dailyResponseStream));
+
+
+            var fxResponse = await _httpClient.GetAsync($"en/financial-markets/foreign-exchange-market/fx-rates-of-other-currencies/fx-rates-of-other-currencies/fx_rates.txt?year={date:yyyy}&month={date:M}");
+
+            fxResponse.EnsureSuccessStatusCode();
+
+            var fxResponseStream = await fxResponse.Content.ReadAsStreamAsync();
+
+            czechItems.AddRange(ParseStreamAsCSV(fxResponseStream));
+
+            //Could have done this using hashsets instead
+            var duplicates = czechItems.GroupBy(x => x.Code.ToLower()).Where(g => g.Count() > 1);
+
+            if (duplicates.Any())
+            {
+                throw new Exception("Duplicate currency code information found");
+            }
+
+            return czechItems;
+        }
+
+        private IEnumerable<CzechExchangeItemDto> ParseStreamAsCSV(Stream data)
+        {
+            var config = new CsvConfiguration(System.Globalization.CultureInfo.CurrentCulture)
+            {
+                Delimiter = "|",
+                ShouldSkipRecord = r => r.Row[0].Contains("#") //This removes the weird extra row in the response from the API
+            };
+
+            using var reader = new StreamReader(data);
+
+            using var csv = new CsvReader(reader, config);
+
+            return csv.GetRecords<CzechExchangeItemDto>().ToList();
         }
     }
 }
