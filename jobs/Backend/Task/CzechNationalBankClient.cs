082544bd8607dc68c64a1759d9e8e1e1afa3d321
Tyler <tylerericmanning@gmail.com>
2024-07-18 10:57:12 -0400

    Add Unit Test project and DI dependencies

---
+++ jobs/Backend/Task/CzechNationalBankClient.cs
@@ -0,0 +1,9 @@
+﻿namespace ExchangeRateUpdater;
+
+public class CzechNationalBankClient : IExchangeClient
+{
+    public ExchangeRate RetrieveExchangeRate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
