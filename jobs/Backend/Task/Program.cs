a756c282a6837e8d8d03dc877cddc89d2bd408d3
Tyler <tylerericmanning@gmail.com>
2024-07-21 18:23:39 -0400

    Stop using ActivatorUtilities

--- jobs/Backend/Task/Program.cs
+++ jobs/Backend/Task/Program.cs
@@ -6,34 +6,34 @@ using ExchangeRateUpdater.ExternalVendors.CzechNationalBank;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace ExchangeRateUpdater
 {
-    public static class Program
+    public class Program
     {
         public static async Task Main(string[] args)
         {
             var host = CreateHost();
-            var provider = ActivatorUtilities.CreateInstance<CzechNationalBankExchangeRateProvider>(host.Services);
+            var provider = host.Services.GetRequiredService<IExchangeRateProvider>();
+            var config = host.Services.GetRequiredService<IConfiguration>();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
 
             try
             {
-                var config = host.Services.GetRequiredService<IConfiguration>();
                 var codes = config.GetSection("DESIRED_CURRENCIES").Get<List<string>>();
-
                 var currencies = codes.Select(code => new Currency(code)).ToList();
-
                 var rates = await provider.GetExchangeRates(currencies);
-                Console.WriteLine($"Successfully retrieved {rates.Count()} exchange rates:");
+                logger.LogInformation($"Successfully retrieved {rates.Count()} exchange rates:");
                 foreach (var rate in rates)
                 {
-                    Console.WriteLine(rate.ToString());
+                    logger.LogInformation(rate.ToString());
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine($"Could not retrieve exchange rates: '{e.ToString()}'.");
+                logger.LogError($"Could not retrieve exchange rates: '{e}'.");
             }
 
             Console.ReadLine();
