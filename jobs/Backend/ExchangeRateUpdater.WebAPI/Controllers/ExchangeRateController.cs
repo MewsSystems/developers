4f0f9fae438ab6e3e2f35dbe5f37eae021003b9d
Michael Reagan <mreagan@assurecare.com>
2024-07-23 12:56:17 -0400

    Initial .NET Core Web API project creation and ExchangeRate Controller

---
+++ jobs/Backend/ExchangeRateUpdater.WebAPI/Controllers/ExchangeRateController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeRateUpdater.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExchangeRateController : ControllerBase
+    {
+        private readonly ILogger<ExchangeRateController> _logger;
+
+        public ExchangeRateController(ILogger<ExchangeRateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetExchangeRates")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Initial Exchange Rate Controller Creation");
+        }
+    }
+}
