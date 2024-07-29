449a35e601ff7504c00d44846bcb6f153142d8ea
s0lena <solena.work@gmail.com>
2024-07-29 23:30:55 +0300

    Migrate index.jsx and App.jsx to TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
