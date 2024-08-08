4ce19fb0aa18a04f881716061780260e2231ed53
Olena Horovenko <elena.gorovenko@gmail.com>
2024-08-08 15:32:34 +0200

    improved the 404 view, added back to start button

--- jobs/Frontend/mews-movies/src/styles/global.ts
+++ jobs/Frontend/mews-movies/src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&display=swap');
--- jobs/Frontend/mews-movies/src/views/Error404.tsx
+++ jobs/Frontend/mews-movies/src/views/Error404.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Error404Container = styled.div`
-  padding: 2rem;
+  padding: 4rem;
+`;
+
+const ErrorTitle = styled.h1`
+  font-size: 3rem;
+`;
+
+const ButtonElement = styled.button`
+  padding: 1rem;
+  margin: 2rem;
+  border-radius: 0.5rem;
+  background-color: #4b83f1;
+  border: none;
+  color: white;
+  font-size: 1rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #5ca0ff;
+  }
 `;
 
 const Error404: React.FC = () => {
+  const navigate = useNavigate();
+  const handleBackToStart = () => {
+    navigate("/");
+  };
+
   return (
     <Error404Container>
-      <h1>404 - Not Found</h1>
-      <p>We couldn't find the movie you were looking for</p>
+      <ErrorTitle>404 - Not Found</ErrorTitle>
+      <p>Something went wrong</p>
+      <ButtonElement onClick={handleBackToStart}>Back to Start</ButtonElement>
     </Error404Container>
   );
 };
