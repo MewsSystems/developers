00412a0423b03df06f6fb70722b2ea6948638067
Emir Pozzi <emir.pozzi@klarna.com>
2024-09-05 11:04:41 +0200

    feat(frontend): scaffold nextjs app and add react query, axios, dotenv

---
+++ jobs/Frontend/movies/src/app/page.tsx
@@ -0,0 +1,37 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <ol>
+          <li>
+            Get started by editing <code>src/app/page.tsx</code>.
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className={styles.ctas}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.secondary}
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn
+        </a>
+      </footer>
+    </div>
+  );
+}
