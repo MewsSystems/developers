fa8869b41dff40482c00784b79a752f53902c649
Rashid <mr.rashidkk@gmail.com>
2024-10-17 14:38:21 +0200

    Add more unit tests

---
+++ jobs/Backend/Task/ExchangeRateUpdater.UnitTests/ExchangeRateApiClientFactoryTests.cs
@@ -0,0 +1,50 @@
+﻿using ExchangeRateUpdater.Domain;
+using ExchangeRateUpdater.Infrastructure;
+using ExchangeRateUpdater.Infrastructure.CzechNationalBank;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ExchangeRateUpdater.UnitTests
+{
+    public class ExchangeRateApiClientFactoryTests
+    {
+        [Fact]
+        public void CreateExchangeRateApiClient_ReturnsCorrectExchangeRateApiClient()
+        {
+            // Arrange
+            var factory = CreateExchangeRateApiClientFactory();
+
+            // Act
+            var exchangeRateApiClient = factory.CreateExchangeRateApiClient(WellKnownCurrencyCodes.CZK);
+
+            // Assert
+            Assert.NotNull(exchangeRateApiClient);
+            Assert.IsType<CzechNationalBankExchangeRateApiClient>(exchangeRateApiClient);
+        }
+
+        [Fact]
+        public void CreateExchangeRateApiClient_IfCurrencyCodeUnknown_ThrowsException()
+        {
+            // Arrange
+            var factory = CreateExchangeRateApiClientFactory();
+
+            // Act
+            var exception = Assert.Throws<NotImplementedException>(
+                () => factory.CreateExchangeRateApiClient("INVALID"));
+
+            // Assert
+            Assert.Equal("Exchange rate API client not implemented for currency INVALID", exception.Message);
+        }
+
+        private static ExchangeRateApiClientFactory CreateExchangeRateApiClientFactory()
+        {
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            httpClientFactoryMock.Setup(x => x.CreateClient(It.IsAny<string>()))
+                .Returns<HttpClient>(null);
+
+            var loggerFactoryMock = new Mock<ILoggerFactory>();            
+
+            return new(httpClientFactoryMock.Object, loggerFactoryMock.Object);
+        }
+    }
+}
--- jobs/Backend/Task/ExchangeRateUpdater.UnitTests/ExchangeRateProviderTests.cs
+++ jobs/Backend/Task/ExchangeRateUpdater.UnitTests/ExchangeRateProviderTests.cs
@@ -1,58 +1,150 @@
 using Moq;
-using ExchangeRateUpdater.Infrastructure.CzechNationalBank;
+using ExchangeRateUpdater.Domain;
 
 namespace ExchangeRateUpdater.UnitTests
 {
     public class ExchangeRateProviderTests
     {
         [Fact]
-        public async Task GetExchangeRatesAsync_IgnoresCurrenciesNotProvidedByBankApi()
+        public async Task GetExchangeRatesAsync_IgnoresCurrenciesNotProvidedByApi()
         {
             // Arrange
+            var targetCurrencyCode = "CZK";
             var requestedCurrencies = new Currency[]
             {
                 new("USD"), new("EUR"), new("RUB")
             };
-
-            var exchangeRatesProvidedByBankApi = new BankApiExchangeRate[]
+            var exchangeRatesProvidedByApi = new ApiExchangeRate[]
             {
                 new("USD", 1.2m), new("EUR", 1.3m)
-            };            
+            };
+            var apiClientFactoryMock = CreateExchangeApiClientFactoryMock(exchangeRatesProvidedByApi, targetCurrencyCode);               
 
-            var bankApiClientMock = new Mock<ICzechNationalBankApiClient>();
-            bankApiClientMock.Setup(x => x.GetDailyExchangeRatesAsync())
-                .ReturnsAsync(exchangeRatesProvidedByBankApi);
-
-            var exchangeRateProvider = new ExchangeRateProvider(bankApiClientMock.Object);
+            var exchangeRateProvider = new ExchangeRateProvider(apiClientFactoryMock.Object);
 
             // Act
-            var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(requestedCurrencies);
+            var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(requestedCurrencies, targetCurrencyCode);
 
             // Assert
             Assert.Equal(2, actualExchangeRates.Count);
 
             var actualSourceCurrencyCodes = actualExchangeRates.Select(x => x.SourceCurrency.Code).ToHashSet();
-            var expectedSourceCurrencyCodes = exchangeRatesProvidedByBankApi.Select(x => x.CurrencyCode).ToHashSet();
+            var expectedSourceCurrencyCodes = new[] { "USD", "EUR" }.ToHashSet();
             Assert.Equal(expectedSourceCurrencyCodes, expectedSourceCurrencyCodes);
         }
 
         [Fact]
-        public async Task GetExchangeRatesAsync_WhenCurrenciesEmpty_DoesNotCallBankApi()
+        public async Task GetExchangeRatesAsync_WhenCurrenciesEmpty_DoesNotCallExchangeRateApi()
         {
             // Arrange
+            var targetCurrencyCode = "CZK";
             var requestedCurrencies = Array.Empty<Currency>();
+            var apiClientFactoryMock = CreateExchangeApiClientFactoryMock([], targetCurrencyCode);           
 
-            var bankApiClientMock = new Mock<ICzechNationalBankApiClient>();
-            bankApiClientMock.Setup(x => x.GetDailyExchangeRatesAsync()).ReturnsAsync([]);
-
-            var exchangeRateProvider = new ExchangeRateProvider(bankApiClientMock.Object);
+            var exchangeRateProvider = new ExchangeRateProvider(apiClientFactoryMock.Object);
 
             // Act
-            var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(requestedCurrencies);
+            var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(requestedCurrencies, targetCurrencyCode);
 
             // Assert
             Assert.Empty(actualExchangeRates);
-            bankApiClientMock.Verify(x => x.GetDailyExchangeRatesAsync(), Times.Never);
+            apiClientFactoryMock.Verify(x => x.CreateExchangeRateApiClient(It.IsAny<string>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task GetExchangeRatesAsync_ReturnsDataOnlyForRequestedCurrencies()
+        {
+            // Arrange
+            var targetCurrencyCode = "CZK";
+            var requestedCurrencies = new Currency[]
+            {
+                new("USD"), new("EUR"), new("RUB")
+            };
+            var exchangeRatesProvidedByApi = new ApiExchangeRate[]
+            {
+                new("USD", 1.2m), new("EUR", 1.3m), new("KES", 1.2m), new("RUB", 1.3m), new("HBC", 1.2m)
+            };
+            var apiClientFactoryMock = CreateExchangeApiClientFactoryMock(exchangeRatesProvidedByApi, targetCurrencyCode);           
+
+            var exchangeRateProvider = new ExchangeRateProvider(apiClientFactoryMock.Object);
+
+            // Act
+            var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(requestedCurrencies, targetCurrencyCode);
+
+            // Assert
+            Assert.Equal(3, actualExchangeRates.Count);
+
+            var actualSourceCurrencyCodes = actualExchangeRates.Select(x => x.SourceCurrency.Code).ToHashSet();
+            var expectedSourceCurrencyCodes = new[] { "USD", "EUR", "RUB" }.ToHashSet();
+            Assert.Equal(expectedSourceCurrencyCodes, expectedSourceCurrencyCodes);
+        }
+
+        [Fact]
+        public async Task GetExchangeRatesAsync_ReturnsCorrectTargetCurrency()
+        {
+            // Arrange
+            var targetCurrencyCode = "CZK";
+            var requestedCurrencies = new Currency[]
+            {
+                new("USD"), new("EUR"), new("RUB")
+            };
+            var exchangeRatesProvidedByApi = new ApiExchangeRate[]
+            {
+                new("USD", 1.2m), new("EUR", 1.3m), new("RUB", 1.3m)
+            };
+            var apiClientFactoryMock = CreateExchangeApiClientFactoryMock(exchangeRatesProvidedByApi, targetCurrencyCode);            
+
+            var exchangeRateProvider = new ExchangeRateProvider(apiClientFactoryMock.Object);
+
+            // Act
+            var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(requestedCurrencies, targetCurrencyCode);
+
+            // Assert
+            Assert.NotEmpty(actualExchangeRates);
+            Assert.All(actualExchangeRates, rate => Assert.Equal(targetCurrencyCode, rate.TargetCurrency.Code));
+        }
+
+        [Fact]
+        public async Task GetExchangeRatesAsync_ReturnsCorrectRateValues()
+        {
+            // Arrange
+            var targetCurrencyCode = "CZK";
+            var requestedCurrencies = new Currency[]
+            {
+                new("USD"), new("EUR"), new("RUB")
+            };
+            var exchangeRatesProvidedByApi = new ApiExchangeRate[]
+            {
+                new("USD", 0.2m), new("EUR", 1.8m), new("RUB", 5.8m)
+            };
+            var apiClientFactoryMock = CreateExchangeApiClientFactoryMock(exchangeRatesProvidedByApi, targetCurrencyCode);           
+
+            var exchangeRateProvider = new ExchangeRateProvider(apiClientFactoryMock.Object);
+
+            // Act
+            var actualExchangeRates = await exchangeRateProvider.GetExchangeRatesAsync(requestedCurrencies, targetCurrencyCode);
+
+            // Assert
+            Assert.Equal(3, actualExchangeRates.Count);
+            Assert.All(exchangeRatesProvidedByApi, apiRate => 
+            {
+                var actualRate = actualExchangeRates.FirstOrDefault(x => x.SourceCurrency.Code == apiRate.CurrencyCode);
+                Assert.Equal(apiRate.Rate, actualRate?.Value);
+            });                     
+        }
+
+        private static Mock<IExchangeRateApiClientFactory> CreateExchangeApiClientFactoryMock(
+            ApiExchangeRate[] exchangeRates, string targetCurrencyCode)
+        {
+            var exchangeRateApiClientMock = new Mock<IExchangeRateApiClient>();
+            exchangeRateApiClientMock.Setup(x => x.GetDailyExchangeRatesAsync())
+                .ReturnsAsync(exchangeRates);
+
+            var apiClientFactoryMock = new Mock<IExchangeRateApiClientFactory>();
+            apiClientFactoryMock.Setup(x => x.CreateExchangeRateApiClient(targetCurrencyCode))
+                .Returns(exchangeRateApiClientMock.Object);
+
+            return apiClientFactoryMock;
         }
     }
 }
