c26909e47a503597f6c3cfe0a9756847396a3907
alexcodito <alexcodito@users.noreply.github.com>
2024-11-07 22:24:35 +0000

    Initial project structure

---
+++ jobs/Backend/Task/ExchangeRateUpdater.Api/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateSlimBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/healthcheck", () => Results.Ok())
+    .WithOpenApi();
+
+app.Run();
