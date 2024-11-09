0169d290c7fe461c7f4845bc5acfae9d0c2d52e4
JOSEP PORCAR NADAL <jjppnn@hotmail.com>
2024-11-09 15:29:16 +0100

    Josep: Mews backend developer task (.NET)

---
+++ jobs/Backend/ExchangeRateUpdaterTests/Mapping/ExRatesDailyProfileTests.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using AutoFixture;
+using Xunit;
+using FluentAssertions;
+using CNB = Cnb.Api.Client;
+
+namespace ExchangeRateUpdater.Mapping.Tests
+{
+
+    public class ExRatesDailyProfileTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Fixture _fixture;
+
+        public ExRatesDailyProfileTests()
+        {
+            _fixture = new Fixture();
+
+            var config = new MapperConfiguration((cfg) =>
+            {
+                cfg.AddProfile<ExRatesDailyProfile>();
+            });
+
+            _mapper = config.CreateMapper();
+        }
+
+
+        [Fact]
+        public void ExRatesDailyProfile_ConfigurationIsValid()
+        {
+            _mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void ExRateDailyRest_To_ExchangeRate_Mapping_Should_Be_Valid()
+        {
+            // Arrange
+            var source = _fixture.Build<CNB.ExRateDailyRest>()
+                .With(x => x.CurrencyCode, "USD")
+                .With(x => x.Rate, 23.048)
+                .With(x => x.Amount, 1)
+                .Create();
+
+            // Act
+            var result = _mapper.Map<ExchangeRate>(source);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.SourceCurrency.Code.Should().Be("USD");
+            result.TargetCurrency.Code.Should().Be("CZK");
+            result.Value.Should().Be((decimal)(source.Rate ?? 0) / (source.Amount ?? 1));
+        }
+    }
+}
---
+++ jobs/Backend/Task/Mapping/ExRatesDailyProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CNB = Cnb.Api.Client;
+
+namespace ExchangeRateUpdater.Mapping
+{
+    public class ExRatesDailyProfile : Profile
+    {
+        public ExRatesDailyProfile()
+        {
+            CreateMap<CNB.ExRateDailyRest, ExchangeRate>(MemberList.Destination)
+            .ForCtorParam("sourceCurrency", opt => opt.MapFrom(src => new Currency(src.CurrencyCode)))
+            .ForCtorParam("targetCurrency", opt => opt.MapFrom(src => new Currency("CZK")))
+            .ForCtorParam("value", opt => opt.MapFrom(src => (decimal)(src.Rate ?? 0) / (src.Amount ?? 1)));
+            ;
+        }
+    }
+}
