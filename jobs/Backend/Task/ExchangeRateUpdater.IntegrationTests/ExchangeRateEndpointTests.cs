4452f5dade6e443a5131560c66ed40966ea5faa3
alexcodito <alexcodito@users.noreply.github.com>
2024-11-09 22:44:30 +0000

    Red integration tests

--- jobs/Backend/Task/ExchangeRateUpdater.IntegrationTests/ExchangeRateEndpointTests.cs
+++ jobs/Backend/Task/ExchangeRateUpdater.IntegrationTests/ExchangeRateEndpointTests.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExchangeRate.IntegrationTests;
 
@@ -10,10 +12,28 @@ public class ExchangeRateEndpointTests : ApiTestBase
     }
 
     [Test]
-    public async Task ExchangeRateEndpoint_GetDailyRate_ReturnsOkResponse()
+    [TestCase("2024-01-01", null)]
+    [TestCase(null, "CZ")]
+    [TestCase("2024-01-01", "CZ")]
+    public async Task ExchangeRateEndpoint_GetExchangeRate_ReturnsOkResponse(
+        string date, string lang)
     {
-        var response = await Client.GetAsync("/exrates/cnb");
+        var response = await Client.GetAsync($"/exrates/cnb?date={date}&lang={lang}");
 
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+    }
+
+    [Test]
+    [TestCase("0000-00-00", null)]
+    [TestCase(null, "XX")]
+    [TestCase("0000-00-00", "XX")]
+    public async Task ExchangeRateEndpoint_GetExchangeRateWithInvalidParameters_ReturnsBadRequestResponse(
+        string date, string lang)
+    {
+        var response = await Client.GetAsync($"/exrates/cnb?date={date}&lang={lang}");
+        var validationDetails = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        Assert.NotNull(validationDetails);
     }
 }
