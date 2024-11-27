3374b1b7bef926d32ae7eca16d896a0fbc5f7ac2
Raffaele Nicosia <raffaele.nicosia91@gmail.com>
2024-11-27 16:59:54 +0100

    build(monitoring): add and configure Sentry

--- jobs/Frontend/src/main.tsx
+++ jobs/Frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import "../sentry.config.ts";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.tsx";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
