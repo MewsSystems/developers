fa93a66192b4ea71ed380069d5dbfc96d8780b3b
D Lopez <danilopezf10@gmail.com>
2024-11-28 12:45:24 +0100

    Initial commit

---
+++ jobs/Frontend/daniel-movie-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex flex-column align-items-center'>
+      <header>
+        <h1 className="text-7xl font-semibold">THE MOVIE APP</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ jobs/Frontend/daniel-movie-app/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// PrimeReact and PrimeFlex imports
+import 'primereact/resources/primereact.min.css';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/themes/md-dark-indigo/theme.css';
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
