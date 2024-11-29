1e330b361a8bbf5e5e92e69fd2436d1a3b298aa1
D Lopez <danilopezf10@gmail.com>
2024-11-29 16:36:20 +0100

    Go back button

--- jobs/Frontend/daniel-movie-app/src/pages/MovieDetail/MovieDetail.tsx
+++ jobs/Frontend/daniel-movie-app/src/pages/MovieDetail/MovieDetail.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import './MovieDetail.css';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { fetchMovie } from '../../services/movieService';
 import { Movie } from '../../models/Movie';
+import { Button } from 'primereact/button';
 
 function MovieDetail() {
+  const navigate = useNavigate();
   const location = useLocation();
   const [movie, setMovie] = useState<Movie>(location.state?.movie || null);
   const { id } = useParams<{ id: string }>();
@@ -25,13 +27,21 @@ function MovieDetail() {
   }, [id, movie]);
 
   return (
-    <div className='flex flex m-4 sm:m-8 gap-5 flex-wrap'>
-      <img src={'https://image.tmdb.org/t/p/w300/'+movie?.poster_path} alt={movie?.title + " poster"} />
-      <div className='flex flex-column' style={{ maxWidth: '60rem'}}>
-        <h1>{movie?.title}</h1>
-        <span className='font-italic'>Release date: {movie?.release_date}</span>
-        <p className='mt-6'>{movie?.overview}</p>
-       </div>
+    <div className='flex flex-column m-2 sm:m-5 gap-4 sm:gap-6'>
+      <Button 
+        icon="pi pi-chevron-left" 
+        rounded 
+        text 
+        onClick={() => navigate('/')}
+      ></Button>
+      <div className='flex flex-wrap gap-5 mx-3 sm:mx-6'>
+        <img src={'https://image.tmdb.org/t/p/w300/'+movie?.poster_path} alt={movie?.title + " poster"} />
+        <div className='flex flex-column' style={{ maxWidth: '60rem'}}>
+          <h1>{movie?.title}</h1>
+          <span className='font-italic'>Release date: {movie?.release_date}</span>
+          <p className='mt-6'>{movie?.overview}</p>
+        </div>
+      </div>
     </div>
   );
 }
