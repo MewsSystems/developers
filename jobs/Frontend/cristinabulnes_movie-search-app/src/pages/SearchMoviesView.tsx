750f6cb812d989c751ee6dc424e0319d3f325f0c
Cristina Bulnes <cristinabulnesmaya@gmail.com>
2024-12-19 14:02:59 +0100

    Refactor: Move movie search redux logic to custom hook
        - Extracted movie search logic from SearchMoviesView into a new custom hook `useMovieSearchRedux`.
        - Simplified the component by delegating state management and data fetching to the custom hook.
        - Improved code readability and reusability by encapsulating logic.

--- jobs/Frontend/cristinabulnes_movie-search-app/src/pages/SearchMoviesView.tsx
+++ jobs/Frontend/cristinabulnes_movie-search-app/src/pages/SearchMoviesView.tsx
@@ -1,44 +1,20 @@
-import React, { useCallback, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../redux/store";
-import { fetchMoviesAsync } from "../redux/thunk/movieSearchThunk";
-import { loadMore, setQuery } from "../redux/slices/movieSearchSlice";
-import { useDebounce } from "../hooks/useDebounce";
+import React from "react";
 import Input from "../components/Input/Input";
 import MoviesGrid from "../components/MoviesGrid";
 import Loading from "../components/Loading";
 import ErrorComponent from "../components/Error";
+import { useMovieSearchRedux } from "../hooks/useMovieSearchRedux";
 
 const SearchMoviesView = () => {
-	const handleInputChange = useCallback(
-		(e: React.ChangeEvent<HTMLInputElement>) => {
-			dispatch(setQuery(e.target.value));
-		},
-		[]
-	);
-
-	const dispatch = useDispatch<AppDispatch>();
-	const { movies, isLoading, error, hasMore, query, page } = useSelector(
-		(state: RootState) => state.movieSearch
-	);
-
-	const debouncedQuery = useDebounce(query, 500);
-
-	const fetchMovies = useCallback(async () => {
-		if (debouncedQuery) {
-			dispatch(fetchMoviesAsync({ query: debouncedQuery, page }));
-		}
-	}, [dispatch, debouncedQuery, page]);
-
-	useEffect(() => {
-		if (debouncedQuery) {
-			fetchMovies();
-		}
-	}, [debouncedQuery, page, fetchMovies]);
-
-	const handleLoadMore = () => {
-		dispatch(loadMore());
-	};
+	const {
+		query,
+		handleInputChange,
+		movies,
+		isLoading,
+		error,
+		handleLoadMore,
+		hasMore,
+	} = useMovieSearchRedux();
 
 	return (
 		<>
