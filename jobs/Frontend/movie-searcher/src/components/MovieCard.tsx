bf5ad7f952d3e4773847f308ace76ef9cd175a02
Juan Sierra <juan.sierra@gitkraken.com>
2024-12-19 19:06:52 +0100

    hover effect

--- jobs/Frontend/movie-searcher/src/components/MovieCard.tsx
+++ jobs/Frontend/movie-searcher/src/components/MovieCard.tsx
@@ -3,7 +3,10 @@ import { Movie } from '../ models/movieModel';
 export const MovieCard = ({ movie }: { movie: Movie }) => {
   return (
     <div data-testId='movieCard' className='h-full'>
-      <a href={`/movies/${movie.id}`} className='flex flex-col items-center h-full'>
+      <a
+        href={`/movies/${movie.id}`}
+        className='flex flex-col items-center h-full ease-in-out duration-500 hover:scale-110'
+      >
         <img
           className='h-full w-64 object-cover'
           src={movie.poster ? `https://image.tmdb.org/t/p/w300/${movie.poster}` : '/src/assets/images/no-image.jpg'}
