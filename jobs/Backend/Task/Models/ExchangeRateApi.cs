32ca0849f84494c92769b22ae49017ff6f826ea8
Antonio Buznego <aebuznego@gmail.com>
2024-12-27 20:33:26 +0100

    Retrieves rates from CNB API and converts it

---
+++ jobs/Backend/Task/Models/ExchangeRateApi.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.Models
+{
+    [Serializable]
+    public class ExchangeRateApi
+    {
+        [JsonProperty("currencyCode")]
+        public string CurrencyCode { get; set; }
+
+        [JsonProperty("rate")]
+        public decimal Rate { get; set; }
+
+        [JsonProperty("amount")]
+        public decimal Amount { get; set; }
+    }
+}
---
+++ jobs/Backend/Task/RateConverter.cs
@@ -0,0 +1,15 @@
+﻿using ExchangeRateUpdater.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExchangeRateUpdater
+{
+    internal static class RateConverter
+    {
+        // Convert from ExchangeRateApi to ExchangeRate
+        public static IEnumerable<ExchangeRate> ConvertApiToExchangeRate(IEnumerable<ExchangeRateApi> ratesApi, Currency baseCurrency)
+        {
+            return ratesApi.Select(c => new ExchangeRate(new Currency(c.CurrencyCode), baseCurrency, c.Rate / c.Amount));
+        }
+    }
+}
