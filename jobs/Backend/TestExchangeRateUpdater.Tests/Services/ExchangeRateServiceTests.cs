f8b3c83e96a90a9e508d3b62659648389d4e3b64
Graeme Hedges <graeme.hedges@gmail.com>
2025-01-11 13:56:29 +0000

    Minor: Added failing test for ExchangeRateService

---
+++ jobs/Backend/TestExchangeRateUpdater.Tests/Services/ExchangeRateServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExchangeRateUpdater.Tests.Services;
+
+[TestFixture]
+public class ExchangeRateServiceTests
+{
+    [Test]
+    public void ExchangeRateService_ShouldBe_InstanceOfExchangeRateService()
+    {
+        // Arrange
+        var exchangeRateService = new ExchangeRateService();
+
+        // Assert
+        exchangeRateService.Should().BeOfType<ExchangeRateService>();
+    }
+}
