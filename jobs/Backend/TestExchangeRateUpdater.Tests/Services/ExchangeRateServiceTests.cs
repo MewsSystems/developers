2d58aa50d2136a493378ff98ca729ee3a93267a7
Graeme Hedges <graeme.hedges@gmail.com>
2025-01-11 14:34:19 +0000

    Minor: Added setup to ExchangeRateServiceTests, added expected return

--- jobs/Backend/TestExchangeRateUpdater.Tests/Services/ExchangeRateServiceTests.cs
+++ jobs/Backend/TestExchangeRateUpdater.Tests/Services/ExchangeRateServiceTests.cs
@@ -6,16 +6,39 @@ namespace TestExchangeRateUpdater.Tests.Services;
 [TestFixture]
 public class ExchangeRateServiceTests
 {
+    private ExchangeRateService _exchangeRateService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _exchangeRateService = new ExchangeRateService();
+    }
+
     [Test]
-    public void CallingGetExchangeRates_ShouldReturn_ExchangeRatesDTO()
+    public void GetExchangeRates_ShouldReturn_CorrectExchangeRatesDTO()
     {
         // Arrange
-        var exchangeRateService = new ExchangeRateService();
+        var expected = new ExchangeRatesDTO
+        {
+            Rates = new List<ExchangeRateDTO>
+            {
+                new ()
+                {
+                    ValidFor = "2025-01-11",
+                    Order = 1,
+                    Currency = "Currency",
+                    Country = "Country",
+                    Amount = 1,
+                    CurrencyCode = "CUR",
+                    Rate = 10.00M
+                }
+            }
+        };
 
         // Act
-        var actual = exchangeRateService.GetExchangeRates();
+        var actual = _exchangeRateService.GetExchangeRates();
 
         // Assert
-        Assert.That(actual, Is.InstanceOf<ExchangeRatesDTO>());
+        Assert.That(actual.Rates, Is.EqualTo(expected.Rates));
     }
 }
