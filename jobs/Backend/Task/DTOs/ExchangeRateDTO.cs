d0427953b1891913372330ca54c909a5a29216ef
Graeme Hedges <graeme.hedges@gmail.com>
2025-01-11 14:33:26 +0000

    Minor: Added global usings to ExchangeRateUpdater, added ExchangeRateDTO, used in ExchangeRatesDTO

---
+++ jobs/Backend/Task/DTOs/ExchangeRateDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExchangeRateUpdater.DTOs;
+
+public record ExchangeRateDTO
+{
+    [JsonPropertyName("validFor")]
+    public string ValidFor { get; init; }
+
+    [JsonPropertyName("order")]
+    public int Order { get; init; }
+
+    [JsonPropertyName("currency")]
+    public string Currency { get; init; }
+
+    [JsonPropertyName("country")]
+    public string Country { get; init; }
+
+    [JsonPropertyName("amount")]
+    public int Amount { get; init; }
+
+    [JsonPropertyName("currencyCode")]
+    public string CurrencyCode { get; init; }
+
+    [JsonPropertyName("rate")]
+    public decimal Rate { get; init; }
+}
--- jobs/Backend/Task/DTOs/ExchangeRatesDTO.cs
+++ jobs/Backend/Task/DTOs/ExchangeRatesDTO.cs
@@ -1,5 +1,9 @@
-﻿namespace ExchangeRateUpdater.DTOs;
+﻿using System.Text.Json.Serialization;
+
+namespace ExchangeRateUpdater.DTOs;
 
 public record ExchangeRatesDTO
 {
+    [JsonPropertyName("rates")]
+    public IEnumerable<ExchangeRateDTO> Rates { get; init; }
 }
