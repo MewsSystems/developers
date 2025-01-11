0d84379e92042bb64fa70a26efc131a3a82a8360
Graeme Hedges <graeme.hedges@gmail.com>
2025-01-11 11:35:58 +0000

    Minor: Added test project to ExchangeRateUpdater

---
+++ jobs/Backend/TestExchangeRateUpdater.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestExchangeRateUpdater.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
