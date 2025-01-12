99b2e5ee544c65d940341d2124e6042318b5f6a9
Graeme Hedges <graeme.hedges@gmail.com>
2025-01-12 15:12:57 +0000

    Handle non response status code in ExchangeRateService

--- jobs/Backend/Task/Services/ExchangeRateService.cs
+++ jobs/Backend/Task/Services/ExchangeRateService.cs
@@ -2,6 +2,8 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Text.Json;
+using System;
+using System.Net.Http.Json;
 
 namespace ExchangeRateUpdater.Services;
 
@@ -9,15 +11,22 @@ public class ExchangeRateService: IExchangeRateService
 {
     private IHttpClientFactory _httpClientFactory;
 
-    public ExchangeRateService(IHttpClientFactory httpClientFactory) 
+    public ExchangeRateService(IHttpClientFactory httpClientFactory)
         => _httpClientFactory = httpClientFactory;
 
     public async Task<ExchangeRatesDTO> GetExchangeRates()
     {
         var httpClient = _httpClientFactory.CreateClient();
 
-        var result = await httpClient.GetAsync("https://api.cnb.cz/cnbapi/exrates/daily?date=2019-05-17&lang=EN");
+        var response = await httpClient.GetAsync("https://api.cnb.cz/cnbapi/exrates/daily?date=2025-11-01&lang=EN");
 
-        return JsonSerializer.Deserialize<ExchangeRatesDTO>(result.Content.ReadAsStringAsync().Result);
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception($"Failed to get exchange rates. Status code: {response.StatusCode}");
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<ExchangeRatesDTO>();
+
+        return result;
     }
 }
