86844ffa11dbe352dc1d050d28b00fd9f6b031e4
Patrick Alvarez Eades <p.alvarezeades@gmail.com>
2025-01-25 18:02:41 +0100

    Removing unneeded items from Movie

--- jobs/Frontend/front-patrick-alvarez/types/Movie.ts
+++ jobs/Frontend/front-patrick-alvarez/types/Movie.ts
@@ -1,16 +1,12 @@
 export interface Movie {
-    adult: boolean
     backdrop_path: string | null
-    genre_ids: number[]
     id: number
     original_language: string
-    original_title: string
+    tagline: string
     overview: string
     popularity: number
     poster_path: string | null
     release_date: string
     title: string
-    video: boolean
     vote_average: number
-    vote_count: number
 }
