03ff1f6be9dedd4a4d018beaea69203ea12c6a1d
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-01-26 22:58:31 +0100

    Replace currency with a record

--- jobs/Backend/Task/ExchangeRateUpdater/Currency.cs
+++ jobs/Backend/Task/ExchangeRateUpdater/Currency.cs
@@ -1,20 +1,14 @@
 ﻿namespace ExchangeRateUpdater
 {
-    public class Currency
+    public record Currency(string Code)
     {
-        public Currency(string code)
-        {
-            Code = code;
-        }
-
         /// <summary>
         /// Three-letter ISO 4217 code of the currency.
         /// </summary>
-        public string Code { get; }
-
+        public string Code { get; init; } = Code;
         public override string ToString()
         {
             return Code;
         }
-    }
+    };
 }
