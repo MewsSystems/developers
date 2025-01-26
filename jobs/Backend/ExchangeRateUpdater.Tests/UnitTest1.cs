b3e252a1fdacaac1d37660a9f9c0304b750adb58
RhyHorn2006 <ryan99horn@gmail.com>
2025-01-26 20:41:13 +0000

    Minor: Updated gitignore, updated project to .net8, added test project

---
+++ jobs/Backend/ExchangeRateUpdater.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ExchangeRateUpdater.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
