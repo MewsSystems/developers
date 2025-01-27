1412091e9038952d2a228604ad1a461f252e9fcf
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-01-27 23:23:58 +0100

    Add stub for czechnationalbank specific cache

---
+++ jobs/Backend/Task/ExchangeRateUpdater/RateSources/CzechNationalBank/CzechNationalBankCache.cs
@@ -0,0 +1,26 @@
+using System;
+using ExchangeRateUpdater.Cache;
+using PublicHoliday;
+
+namespace ExchangeRateUpdater.RateSources.CzechNationalBank;
+
+public class CzechNationalBankRatesCache
+{
+    static readonly TimeZoneInfo CETTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central European Time");
+    static readonly TimeOnly MainRatesRefreshTime = new TimeOnly(14, 30);
+    
+    private readonly CzechRepublicPublicHoliday _holidays;
+    private readonly TimeProvider _timeProvider;
+
+    public CzechNationalBankRatesCache(CzechRepublicPublicHoliday holidays, TimeProvider timeProvider)
+    {
+        _holidays = holidays;
+        _timeProvider = timeProvider;
+    }
+
+    public DateTime? GetMainRateExpirationDate(DateOnly targetDate)
+    {
+        var now = _timeProvider.GetUtcNow();
+        var targetDateTime = targetDate.ToDateTime(new(0, 0));
+    }
+}
