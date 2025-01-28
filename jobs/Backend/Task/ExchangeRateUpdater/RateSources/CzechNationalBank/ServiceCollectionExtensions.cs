432506c8e5a8ca970a22e8c9ae53f7415bdcf341
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-01-28 17:10:49 +0100

    Move holiday to the DI

--- jobs/Backend/Task/ExchangeRateUpdater/RateSources/CzechNationalBank/ServiceCollectionExtensions.cs
+++ jobs/Backend/Task/ExchangeRateUpdater/RateSources/CzechNationalBank/ServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using ExchangeRateUpdater.Cache;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using PublicHoliday;
 
 namespace ExchangeRateUpdater.RateSources.CzechNationalBank;
 
@@ -12,15 +13,17 @@ public static class ServiceCollectionExtensions
         services.Configure(configureSourceOptions);
         services.TryAddSingleton<ICzechNationalBankRateParser, CzechNationalBankRateParser>();
         services.TryAddSingleton<ICzechNationalBankRateUriBuilder, CzechNationalBankRateUriBuilder>();
-        services.AddTransient<IRateSource, CzechNationalBankRateSource>();
         services.TryAddSingleton<IExchangeRateCache, NullCache>();
+        services.TryAddSingleton<CzechRepublicPublicHoliday>((_) => new CzechRepublicPublicHoliday() { UseCachingHolidays = true });
+        services.TryAddSingleton<ICzechNationalBankRatesCacheExpirationCalculator, CzechNationalBankRatesCacheExpirationCalculator>();
+        services.AddTransient<IRateSource, CzechNationalBankRateSource>();
 
         services.AddHttpClient<CzechNationalBankRateSource>(opts =>
         {
             opts.Timeout = TimeSpan.FromSeconds(30);
         });
         services.AddLogging();
-        
+
         return services;
     }
 }
