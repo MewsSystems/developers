f38a280147b793323e938a03c140a604ad0cebc8
RhyHorn2006 <ryan99horn@gmail.com>
2025-01-30 19:40:52 +0000

    Minor: Removed redundant code

--- jobs/Backend/Task/Program.cs
+++ jobs/Backend/Task/Program.cs
@@ -11,19 +11,6 @@ namespace ExchangeRateUpdater;
 
 public static class Program
 {
-    private static IEnumerable<Currency> currencies = new[]
-    {
-        new Currency("USD"),
-        new Currency("EUR"),
-        new Currency("CZK"),
-        new Currency("JPY"),
-        new Currency("KES"),
-        new Currency("RUB"),
-        new Currency("THB"),
-        new Currency("TRY"),
-        new Currency("XYZ")
-    };
-
     public static async Task Main(string[] args)
     {
         var services = new ServiceCollection();
@@ -36,34 +23,15 @@ public static class Program
         try
         {
             var exchangeRateResponse = await exchangeRateService.GetExchangeRatesAsync();
-
             Console.WriteLine($"Successfully retrieved {exchangeRateResponse.ExchangeRates.Count} exchange rates:");
             foreach (var rate in exchangeRateResponse.ExchangeRates)
             {
                 Console.WriteLine($"Country: {rate.Country}, Currency: {rate.Currency}, Rate: {rate.Rate}");
             }
         }
-        catch (Exception e) 
-        {
-            Console.WriteLine($"Could not retrieve exchange rates: '{e.Message}'");
-        }
-
-        try
-        {
-            var provider = new ExchangeRateProvider();
-            var rates = provider.GetExchangeRates(currencies);
-
-            Console.WriteLine($"Successfully retrieved {rates.Count()} exchange rates:");
-            foreach (var rate in rates)
-            {
-                Console.WriteLine(rate.ToString());
-            }
-        }
         catch (Exception e)
         {
-            Console.WriteLine($"Could not retrieve exchange rates: '{e.Message}'.");
+            Console.WriteLine($"An error occurred while retrieving exchange rates: '{e.Message}'.");
         }
-
-        Console.ReadLine();
     }
 }
