7b017c619b4ef4d7d1dbd98017ac72086fc54419
Dan <dan@galeta.org>
2025-02-02 21:10:09 +0100

    logic implemented

---
+++ jobs/Backend/Task/IExchangeRateService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater
+{
+    public interface IExchangeRateService
+    {
+        Task<IEnumerable<ExchangeRate>> GetExchangeRates(IEnumerable<Currency> currencies);
+    }
+}
