9f8c72aca30380527ac0023028581129567cb548
Eva Čechová <cechovaev@gmail.com>
2025-03-19 23:10:19 +0100

    added basic styling to some components

--- jobs/Frontend/my-app/src/components/PageSectionContainer.tsx
+++ jobs/Frontend/my-app/src/components/PageSectionContainer.tsx
@@ -1,9 +1,16 @@
+import styled from 'styled-components';
+
 type PageSectionContainerProps = {
   children?: React.ReactNode;
 };
 
+const StyledPageSectionContainer = styled.div`
+  max-width: 1250px;
+  padding: 2rem;
+`;
+
 export const PageSectionContainer = ({
   children,
 }: PageSectionContainerProps) => {
-  return <>{children}</>;
+  return <StyledPageSectionContainer>{children}</StyledPageSectionContainer>;
 };
