bb54b71394fcdf92d740513f090d6fbda298c35f
Barbora Lendacka <borka34@gmail.com>
2025-03-23 16:29:47 +0100

    implement movie fetching and rendering in CardContainer

--- project-movie-app/src/Content/CardContainer.jsx
+++ project-movie-app/src/Content/CardContainer.jsx
@@ -1,16 +1,34 @@
 import "./cardContainer.style.css"
 import { Card } from "./Card"
+import { BASE_API_URL, API_KEY } from "../constants/API.constants"
+import { useState } from "react"
+import { useEffect } from "react"
 
 export const CardContainer = () => {
+    const [items, setItems] = useState([])
+
+    useEffect(()=> {
+        const fetchPaginatedMovies = async () => {
+            try {
+                const reqMovies = await fetch(`${BASE_API_URL}/movie/popular?api_key=${API_KEY}&page=1`)
+                const movies = await reqMovies.json()
+
+                setItems(movies.results)
+            }
+            catch (error) {
+                console.error("Error fetching popular movies:", error)
+            }
+        }
+        fetchPaginatedMovies()
+    },[])
 
     return (
         <div className="card-container">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            {items
+                .map((item) => (
+                <Card posterPath={item.poster_path} title={item.original_title}/>
+                ))
+            }
         </div>
     )
 }
---
+++ project-movie-app/src/constants/API.constants.js
@@ -0,0 +1,2 @@
+export const BASE_API_URL = "https://api.themoviedb.org/3";
+export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
