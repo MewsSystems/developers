fa3523035473db7177b4ec7e92ad1af7375b4fe9
Barbora Lendacka <borka34@gmail.com>
2025-03-25 02:46:17 +0100

    add homepage navigation to MovieApp logo

--- project-movie-app/src/Header/Header.jsx
+++ project-movie-app/src/Header/Header.jsx
@@ -1,12 +1,15 @@
 import "./header.style.css"
 import logo from "./assets/logo-movie-app.svg"
+import { Link } from "react-router"
 
 export const Header = () => {
 
     return (
         <header className="header">
             <div className="header__logo">
-                <img className="header__image" src={logo} alt="movie app logo" />
+                <Link to = "/">
+                    <img className="header__image" src={logo} alt="movie app logo" />
+                </Link>
             </div>
         </header>
     )
