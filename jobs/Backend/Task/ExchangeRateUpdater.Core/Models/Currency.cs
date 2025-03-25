9d3a13c1d81419a258d462a12faa7767912e15d9
Vojtěch Kožuch <kozuch.vojtech@gmail.com>
2025-03-25 14:29:08 +0100

    Fixed .gitignore

--- jobs/Backend/Task/ExchangeRateUpdater.Core/Models/Currency.cs
+++ jobs/Backend/Task/ExchangeRateUpdater.Core/Models/Currency.cs
@@ -5,7 +5,7 @@ public class Currency(string code)
     /// <summary>
     /// Three-letter ISO 4217 code of the currency.
     /// </summary>
-    public string Code { get; } = code;
+    public string Code => code;
 
     public override string ToString()
     {
