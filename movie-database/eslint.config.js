4aac3a123ee01385513579745fa3c040ca2eba87
EvaMach <evca.machova@gmail.com>
2025-03-27 02:25:38 +0100

    Fix formatting

--- movie-database/eslint.config.js
+++ movie-database/eslint.config.js
@@ -7,9 +7,7 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked
-
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
