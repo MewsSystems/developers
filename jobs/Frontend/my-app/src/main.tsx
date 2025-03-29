27b1ad2428370845177cd0740cb4137e7dceae26
Eva Čechová <cechovaev@gmail.com>
2025-03-29 14:02:47 +0100

    moved routing directly to main.tsx, deleted app.tsx

--- jobs/Frontend/my-app/src/main.tsx
+++ jobs/Frontend/my-app/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import './main.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { MovieSearch } from './pages/MovieSearch';
+import { MovieDetail } from './pages/MovieDetail';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MovieSearch />,
+  },
+  {
+    path: '/movie-detail/:movieId',
+    element: <MovieDetail />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
