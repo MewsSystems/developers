9905b7ec11ea9bc24fff59824bc4d98076ede410
svancarova <81180766+AlzbetaSv@users.noreply.github.com>
2025-04-09 22:05:55 +0200

    Restructure project directories and add initial configuration

--- jobs/Backend/Task/Infrastructure/ExchangeRateProvider.cs
+++ jobs/Backend/Task/Infrastructure/ExchangeRateProvider.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using ExchangeRateUpdater.Domain;
 
-namespace ExchangeRateUpdater
+namespace ExchangeRateUpdater.Infrastructure
 {
-    public class ExchangeRateProvider
+    public interface IExchangeRateProvider
     {
+        IEnumerable<ExchangeRate> GetExchangeRates(IEnumerable<Currency> currencies);
+    }
+    public class CzechNationalBankExchangeRateProvider : IExchangeRateProvider
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl;
+
+        public CzechNationalBankExchangeRateProvider(HttpClient httpClient, string baseUrl)
+        {
+            _httpClient = httpClient;
+            _baseUrl = baseUrl;
+        }
+
         /// <summary>
         /// Should return exchange rates among the specified currencies that are defined by the source. But only those defined
         /// by the source, do not return calculated exchange rates. E.g. if the source contains "CZK/USD" but not "USD/CZK",
--- jobs/Backend/Task/Program.cs
+++ jobs/Backend/Task/Program.cs
@@ -1,29 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using ExchangeRateUpdater.Domain;
+using ExchangeRateUpdater.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ExchangeRateUpdater
 {
     public static class Program
     {
-        private static IEnumerable<Currency> currencies = new[]
-        {
-            new Currency("USD"),
-            new Currency("EUR"),
-            new Currency("CZK"),
-            new Currency("JPY"),
-            new Currency("KES"),
-            new Currency("RUB"),
-            new Currency("THB"),
-            new Currency("TRY"),
-            new Currency("XYZ")
-        };
-
         public static void Main(string[] args)
         {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(AppContext.BaseDirectory)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                }) .ConfigureServices((context, services) =>
+                {
+                    services.AddHttpClient<CzechNationalBankExchangeRateProvider>();
+                })
+                .Build();
+            
+            var configuration = host.Services.GetRequiredService<IConfiguration>();
+            
+            string baseUrl = configuration["ExchangeRateProvider:BaseUrl"];
+            string[] currencyCodes = configuration.GetSection("Currencies").Get<string[]>();
+            var currencies = currencyCodes.Select(code => new Currency(code)).ToArray();
+            
+            
+            var httpClientFactory = host.Services.GetRequiredService<IHttpClientFactory>();
+            var httpClient = httpClientFactory.CreateClient();
+            
             try
             {
-                var provider = new ExchangeRateProvider();
+                var provider = new CzechNationalBankExchangeRateProvider(httpClient, baseUrl);
                 var rates = provider.GetExchangeRates(currencies);
 
                 Console.WriteLine($"Successfully retrieved {rates.Count()} exchange rates:");
