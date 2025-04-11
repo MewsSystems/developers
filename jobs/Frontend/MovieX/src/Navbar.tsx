a4d6c8e468b680e6b37d1b0b04817858f60f0d34
Ole Hendrix <ole.hendrixsk8@gmail.com>
2025-04-11 17:36:31 +0200

    .env gone

--- jobs/Frontend/MovieX/src/Navbar.tsx
+++ jobs/Frontend/MovieX/src/Navbar.tsx
@@ -5,7 +5,7 @@ import Mews from './assets/Mews.png';
 function Navbar()
 {
 	return (
-		<nav className="h-[8vh] min-h-18 p-5 px-20 flex justify-between items-center">
+		<nav className="h-[8vh] p-5 px-20 flex justify-between items-center">
 			<img src={Logo} className="h-18 w-auto"/>
 			<img src={Codam} className="hidden md:flex h-18 w-auto"/>
 			<img src={Mews} className='hidden md:flex h-18 w-auto'/>
