2e0acb119c532aea49a16875e3241fb32febd563
Xieli <lexnardgonzalez@gmail.com>
2025-04-11 20:51:56 +0200

    Create a directory to store constants

---
+++ jobs/Backend/Task/Common/Constants/ErpPlatform.cs
@@ -0,0 +1,8 @@
+﻿namespace ExchangeRateUpdater.Common.Constants
+{
+    public static class ErpPlatform
+    {
+        public const string BaseUrl = "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt";
+        public const string DefaultSourceCurrency = "CZK";
+    }
+}
