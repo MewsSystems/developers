28aabd56303b474dba45372e51b0da30e26994ff
Dylan Meivis <meivisdylan@gmail.com>
2025-05-14 01:49:56 +0200

    Move Currency and ExchangeRate to Entities

---
+++ jobs/Backend/Task/src/ExchangeRateUpdater/Entities/ExchangeRate.cs
@@ -0,0 +1,15 @@
+namespace ExchangeRateUpdater.Entities;
+
+public class ExchangeRate(Currency sourceCurrency, Currency targetCurrency, decimal value)
+{
+    public Currency SourceCurrency { get; } = sourceCurrency;
+
+    public Currency TargetCurrency { get; } = targetCurrency;
+
+    public decimal Value { get; } = value;
+
+    public override string ToString()
+    {
+        return $"{SourceCurrency}/{TargetCurrency}={Value}";
+    }
+}
