f3133245110e843cabb5b493a06ee7c867102368
Diego Pandiello <diegofernandezpandiello@gmail.com>
2025-05-17 10:58:30 +0200

    feat: implement CNBExchangeRateProvider and CNBOptions with dependency injection setup

---
+++ jobs/Backend/Task/Infrastructure/Registry/InfrastructureServiceCollection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExchangeRateUpdater.Infrastructure.Registry
+{
+    // The infrastrucutre folder could be (in a bigger project) a separate project.
+    internal static class InfrastructureServiceCollection
+    {
+        public static IServiceCollection AddExchangeRateUpdater(this IServiceCollection services, IConfiguration config)
+            => services
+                .AddCNBOptions(config)
+                .AddSingleton<IExchangeRateProvider, CNBExchangeRateProvider>();
+
+        private static IServiceCollection AddCNBOptions(this IServiceCollection services, IConfiguration config) =>
+            services.Configure<CNBOptions>(config.GetSection(key: nameof(CNBOptions)));
+    }
+}
