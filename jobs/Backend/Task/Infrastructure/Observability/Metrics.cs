4b6f5fbf49c266f3695949f677339b6cc0255f11
Diego Pandiello <diegofernandezpandiello@gmail.com>
2025-05-18 10:29:34 +0200

    feat(observability): add Metrics class and service registration
    Introduce a new `Metrics` class to manage observability metrics for exchange rate updates, including counters for HTTP client retries and circuit breaker events.
    
    Add an extension method `AddObservabilityInfrastructure` to register the `Metrics` class as a singleton and configure OpenTelemetry for tracing and metrics, ensuring proper instrumentation and console exporting.

---
+++ jobs/Backend/Task/Infrastructure/Observability/Metrics.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+namespace ExchangeRateUpdater.Infrastructure.Observability
+{
+    internal sealed class Metrics : IDisposable
+    {
+        public const string ExchangeRateUpdateMeterName = "ExchangeRateUpdate.Metrics";
+
+        private readonly Meter meter;
+
+        public Metrics(IMeterFactory meterFactory)
+        {
+            meter = meterFactory.Create(ExchangeRateUpdateMeterName, "1.0.0");
+            RetryCounter = meter.CreateCounter<long>("httpclient_retry_count", description: "Number of retries");
+            CircuitBreakerOpened = meter.CreateCounter<long>("circuit_breaker_opened_count", description: "Number of times the circuit breaker opened");
+            CircuitBreakerReset = meter.CreateCounter<long>("circuit_breaker_reset_count", description: "Number of times the circuit breaker reset");
+        }
+
+        public Counter<long> RetryCounter { get; }
+
+        public Counter<long> CircuitBreakerOpened { get; }
+
+        public Counter<long> CircuitBreakerReset { get; }
+
+        public void Dispose()
+        {
+            meter.Dispose();
+        }
+    }
+}
---
+++ jobs/Backend/Task/Infrastructure/Observability/Registry/ObservabilityServiceCollection.cs
@@ -0,0 +1,29 @@
+﻿using ExchangeRateUpdater.Infrastructure.Observability;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace ExchangeRateUpdater.Infrastructure.CNB.Registry
+{
+    internal static class ObservabilityServiceCollection
+    {
+        public static IServiceCollection AddObservabilityInfrastructure(this IServiceCollection services)
+            => services
+                .AddSingleton<Metrics>()
+                .AddOpenTelemetry()
+                .ConfigureResource(builder => builder.AddService("ExchangeRateUpdater"))
+                .WithTracing(builder =>
+                {
+                    builder.AddHttpClientInstrumentation();
+                    builder.AddConsoleExporter();
+                })
+                .WithMetrics(builder =>
+                {
+                    builder.AddHttpClientInstrumentation();
+                    builder.AddMeter([Metrics.ExchangeRateUpdateMeterName]);
+                    builder.AddConsoleExporter();
+                }).Services;
+    }
+}
