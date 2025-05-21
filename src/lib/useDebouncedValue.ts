340426f480f37ff4a94c2a2f426f7502cb775364
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-05-21 15:12:54 +0200

    feat: add debounce

---
+++ src/lib/useDebouncedValue.ts
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export const useDebouncedValue = (value: string, delay: number = 600) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(handler);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
