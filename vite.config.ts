62e40f000b79c5b7f29175c5b2005022086e2bb6
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-05-22 18:24:49 +0200

    feat: update TypeScript configuration and Vite aliases Refactor tsconfig.json and tsconfig.node.json to include compiler options and path mappings for improved module resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@core': path.resolve(__dirname, './src/core'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@movie': path.resolve(__dirname, './src/core/movie'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@app': path.resolve(__dirname, './src')
+    }
+  }
 })
