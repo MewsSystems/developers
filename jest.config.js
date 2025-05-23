2558937a94a100c9f6b8686df26c6e4186c15c64
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-05-23 19:33:58 +0200

    chore: add Jest configuration and update testing dependencies in package.json and package-lock.json

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+  },
+}; 
