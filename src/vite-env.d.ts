ccedf304befe88257f3c8fda653f18616c132f5d
titov-alexx <titov.aleksej@gmail.com>
2025-05-31 12:13:58 +0100

    Move SVG module declaration to src/types to keep vite-env.d.ts uncluttered

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module '*.svg' {
-  import React from 'react';
-  const SVGComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  export default SVGComponent;
-}
