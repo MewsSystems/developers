335b42e8bec6cc04a6b55f94f53c252f30f201bf
Barbora Lendacka <borka34@gmail.com>
2025-06-09 16:04:17 +0200

    refactor(card-container): migrate styles to styled-components

--- project-movie-app/src/Content/CardContainer/CardContainer.tsx
+++ project-movie-app/src/Content/CardContainer/CardContainer.tsx
@@ -1,16 +1,16 @@
-import "./cardContainer.style.css"
-import { Card } from "./Card/Card"
-import { MovieCardContainerProps } from "../types/movie"
+import { Card } from "../Card/Card"
+import { CardGrid } from "./style"
+import { MovieCardContainerProps } from "./types"
 
 export const CardContainer: React.FC<MovieCardContainerProps> = ({page, items}) => {
 
     return (
-        <div className="card-container">
+        <CardGrid>
             {items
                 .map((item, index) => (
                     <Card key={`${item.id}-${page}-${index}`} posterPath={item.posterPath} title={item.title} id={item.id}/>
                 ))
             }
-        </div>
+        </CardGrid>
     )
 }
--- project-movie-app/src/Content/CardContainer/style.ts
+++ project-movie-app/src/Content/CardContainer/style.ts
@@ -1,26 +1,22 @@
-/* card container */
+import styled from "styled-components";
 
-.card-container {
+export const CardGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     gap: 16px;
     margin-bottom: 40px;
     min-height: calc(100vh - (79.45px + 40px + 37.5px + 102.5px + 40px + 42.5px + 60px + 58.5px));
-    /* min-heigt: calc(100vh - (header + main margin-top + h2 + search-row + container margin-bottom + button + main margin-bottom + footer)) */
-}
+    //min-heigt: calc(100vh - (header + main margin-top + h2 + search-row + container margin-bottom + button + main margin-bottom + footer));
 
-@media screen and (min-width: 600px) {
-    .card-container {
+    @media screen and (min-width: 600px) {
         display: grid;
         grid-template-columns: repeat(4, 1fr);
         gap: 16px;
     }
-}
 
-@media screen and (min-width: 1100px) {
-    .card-container {
+    @media screen and (min-width: 1100px) {
         display: grid;
         grid-template-columns: repeat(5, 1fr);
         gap: 16px;
     }
-}
+`
