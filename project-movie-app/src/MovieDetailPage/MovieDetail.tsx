88c3d6beace98d4fad4fa302d4bba0a2e2348758
Barbora Lendacka <borka34@gmail.com>
2025-06-10 16:06:43 +0200

    reorganize MovieDetailPage folder structure and create MovieDetailContent component

--- project-movie-app/src/MovieDetailPage/MovieDetail.tsx
+++ project-movie-app/src/MovieDetailPage/MovieDetail.tsx
@@ -1,11 +1,9 @@
-import { Button } from "../Button"
-import "./movieDetail.style.css"
-import { Link } from "react-router"
 import { useParams } from "react-router"
 import { useEffect } from "react"
 import { useState } from "react"
 import { getFormattedMovieID } from "../API/getFormattedMovieID"
 import { MovieDetailInfo } from "../types/movieDetail"
+import { MovieDetailContent } from "./MovieDetailContent.tsx"
 
 export const MovieDetail = () => {
 
@@ -24,56 +22,6 @@ export const MovieDetail = () => {
     if (!movie) return <p>Loading...</p>;
 
     return (
-        <section className="movie-detail">
-            <h1 className="movie-title">{movie.title}</h1>
-            <p className="movie-tagline">
-                <em>
-                    {movie.tagline && `"${movie.tagline}"`}
-                </em>
-            </p>
-            <div className="movie-content">
-                <div className="movie-poster">
-                    {movie.backdropPath && 
-                        <img
-                            src={movie.backdropPath}
-                            alt={movie.title}
-                        />
-                    }
-                </div>
-                <div className="movie-info">
-                    {movie.genres &&
-                        <p><strong>Genres: </strong>{movie.genres}</p>
-                    }
-                    {movie.userScore &&
-                        <p><strong>User Score: </strong>{movie.userScore}</p>
-                    }
-                    {movie.releaseDate &&
-                        <p><strong>Release Date: </strong>{movie.releaseDate}</p>
-                    }
-                    {movie.originalLanguage &&
-                        <p><strong>Original Language: </strong>{movie.originalLanguage}</p>
-                    }
-                    {movie.runtime  &&
-                        <p><strong>Runtime: </strong>{movie.runtime}</p>
-                    }
-                    {movie.homepage &&
-                        <p><strong>Homepage: </strong>
-                            <a href={movie.homepage} target="_blank" rel="noopener noreferrer">
-                                {movie.title}
-                            </a>
-                        </p>
-                    }
-                </div>
-            </div>
-            {movie.overview && 
-                <div className="movie-overview">
-                    <h2>Movie overview</h2>
-                    <p>{movie.overview}</p>
-                </div>
-            }
-            <Link to = "/">
-                <Button label="Back to movies"/>
-            </Link>
-        </section>
+        <MovieDetailContent movie={movie}/>
     )
 }
---
+++ project-movie-app/src/MovieDetailPage/MovieDetailContent.tsx/index.ts
@@ -0,0 +1,1 @@
+export { MovieDetailContent } from "./MovieDetailContent";
