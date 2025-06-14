f8460c73d5d95c5efd307aaade159a678f31b49d
Barbora Lendacka <borka34@gmail.com>
2025-06-14 02:04:39 +0200

    remove unused index.css file after migrating to styled-components

--- project-movie-app/src/main.tsx
+++ project-movie-app/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './index.css'
 import { App } from './App.js'
 import { MovieDetailPage } from './MovieDetailPage/MovieDetailPage';
 
