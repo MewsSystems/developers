06036621e6a53bab77454caf661fa622815f4172
Ivan <donivanes@gmail.com>
2025-06-20 12:07:09 +0200

    chore: first commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
