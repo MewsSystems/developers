7bc538195fa35787e4e8979028d52e9c31c0e652
Ivan <donivanes@gmail.com>
2025-06-21 08:36:07 +0200

    feat: mocks service working init

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
-import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router"
+import App from "./App.tsx"
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+const isMockingEnabled = import.meta.env.VITE_ENABLE_MSW === "true"
+
+async function enableMocking() {
+  if (isMockingEnabled) {
+    const { worker } = await import("./test/mocks/browser")
+
+    await worker.start({
+      onUnhandledRequest: "bypass",
+    })
+  }
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+})
