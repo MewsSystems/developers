00ff4bfe134daba90809db005fcd952220bd32fa
zayprom <zayakov@promoplace.cz>
2025-06-28 22:27:07 +0200

    HomePage added to App

--- jobs/Frontend/movie-search/src/App.tsx
+++ jobs/Frontend/movie-search/src/App.tsx
@@ -1,52 +1,13 @@
 import "./App.css";
 import { LayoutWrapper } from "./components/Layout/LayoutWrapper";
-import { Input } from "./components/Form/Input";
-import { MainContent } from "./components/Layout/MainContent";
-import { Header } from "./components/Header/Header";
+
 import { Footer } from "./components/Footer/Footer";
-import { Grid } from "./components/Grid/Grid";
-import { GridCard } from "./components/Grid/GridCard";
-import { useEffect, useState } from "react";
-import { getPopularMovies } from "./api/requests";
-import { type Movie } from "./api/types";
-
-interface MovieCardProps {
-  movieData: Movie;
-}
-
-const MovieCard = (props: MovieCardProps) => {
-  return (
-    <GridCard item={props.movieData} onClick={() => console.log("clicked")}>
-      {props.movieData.original_title}
-    </GridCard>
-  );
-};
+import { HomePage } from "./pages/HomePage";
 
 function App() {
-  const [movieData, setMovieData] = useState<Movie[]>([]);
-  useEffect(() => {
-    (async () => {
-      try {
-        const data = await getPopularMovies();
-        setMovieData(data.results);
-      } catch (e) {
-        console.error(e);
-      }
-    })();
-  }, []);
-
   return (
     <LayoutWrapper>
-      <Header>
-        <Input name="searchField" />
-      </Header>
-      <MainContent>
-        <Grid
-          items={movieData}
-          keyExtractor={(movie) => movie.id}
-          renderItem={(movie) => <MovieCard movieData={movie} />}
-        />
-      </MainContent>
+      <HomePage />
       <Footer>Created by Petar Zayakov</Footer>
     </LayoutWrapper>
   );
