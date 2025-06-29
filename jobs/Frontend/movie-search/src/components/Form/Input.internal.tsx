e13f686cd4f46de88988c38c5b2bd1871fa51185
zayprom <zayakov@promoplace.cz>
2025-06-29 10:54:49 +0200

    input styling change

--- jobs/Frontend/movie-search/src/components/Form/Input.internal.tsx
+++ jobs/Frontend/movie-search/src/components/Form/Input.internal.tsx
@@ -4,14 +4,16 @@ export const InputWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  width: 100%;
+  max-width: 100%;
 `;
 
 export const StyledInput = styled.input`
-  border: 0.5px solid #666;
+  border: 1px solid #666;
   border-radius: 15rem;
   font-family: inherit;
   height: 2.4rem;
-  width: 20rem;
+  width: 30%;
   padding: 0.8rem;
 `;
