21340d3b8fb68038f1978211c382448e477dc960
zayprom <zayakov@promoplace.cz>
2025-06-30 11:32:20 +0200

    App test added

---
+++ jobs/Frontend/movie-search/src/__tests__/App.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { vi, describe, it, expect } from "vitest";
+import App from "../App";
+
+describe("App", () => {
+  it("renders the HomePage for the root route", () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText("List of movies")).toBeInTheDocument();
+  });
+
+  it("renders the DetailPage for the /movie/:id route", () => {
+    render(
+      <MemoryRouter initialEntries={["/movie/123"]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    // we have only "Back button" present as no other data are loaded
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+});
