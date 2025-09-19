893de45678c0e6570dfe8deff9097442f02fc731
Thiago Dieguez <thiagoo.dieguez@gmail.com>
2025-09-19 18:12:59 +0200

    Add unit tests for `Currency` value object

---
+++ jobs/Backend/Task/Exchange.Domain.UnitTests/ValueObjects/CurrencyTests.cs
@@ -0,0 +1,33 @@
+using Exchange.Domain.ValueObjects;
+using FluentAssertions;
+
+namespace Exchange.Domain.UnitTests.ValueObjects;
+
+public class CurrencyTests
+{
+    [Fact]
+    public void FromCode_WhenCodeIsValid_ReturnsCurrency()
+    {
+        // Arrange
+        const string code = "EUR";
+
+        // Act
+        var currency = Currency.FromCode(code);
+
+        // Assert
+        currency.Should().Be(Currency.EUR);
+    }
+
+    [Fact]
+    public void FromCode_WhenCodeIsInvalid_ThrowsException()
+    {
+        // Arrange
+        const string code = "INVALID";
+
+        // Act
+        var act = () => Currency.FromCode(code);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+}
