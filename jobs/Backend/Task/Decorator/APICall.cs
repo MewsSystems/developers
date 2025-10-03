ccc6d73cbeb73a0cf96066ed1a1838e898852432
Isaac Rossell i Ripoll <isaac.rossell.ripoll@outlook.com>
2025-10-03 19:00:22 +0200

    Call to CNB

---
+++ jobs/Backend/Task/Decorator/APICall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRateUpdater.CNB
+{
+    internal class APICall
+    {
+        readonly string path;
+
+        readonly HttpClient client;
+        readonly StringBuilder result;
+        
+        HttpResponseMessage response;
+
+        public APICall()
+        {
+            path = "https://www.cnb.cz/en/financial-markets/foreign-exchange-market/central-bank-exchange-rate-fixing/central-bank-exchange-rate-fixing/daily.txt";
+            
+            client = new();
+            result = new();
+        }
+
+        public async Task<string> DayliExchange()
+        {
+
+            response = await client.GetAsync(path);
+            
+            if(response.IsSuccessStatusCode)
+            {
+                result.Append(await response.Content.ReadAsStringAsync());
+            }
+
+            return result.ToString();
+        }
+    }
+}
