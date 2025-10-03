8f9bf2d1476bd1b697d3aadfc78144918853a914
Isaac Rossell i Ripoll <isaac.rossell.ripoll@outlook.com>
2025-10-03 19:01:36 +0200

    Read line by line

---
+++ jobs/Backend/Task/Decorator/ReadResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRateUpdater.CNB;
+using ExchangeRateUpdater.Models;
+
+namespace ExchangeRateUpdater.Decorator
+{
+    internal class ReadResult
+    {
+        readonly List<Rate> rates;
+
+        public ReadResult()
+        {
+            rates = new();
+        }
+
+        public async Task ReadString() //string
+        {
+            APICall aPICall = new();
+
+            string result = await aPICall.DayliExchange();
+
+            var lines = new StringReader(result)
+                .ReadToEnd()
+                .Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Skip(2);
+
+            foreach(string line in lines)
+            {
+                string[] list = line.Split('|');
+                rates.Add(new Rate(list[0], list[1], Convert.ToInt16(list[2]), list[3], Convert.ToDouble(list[4])));
+            }
+
+            rates.ForEach(rate => Console.WriteLine(rate));
+        }
+    }
+}
