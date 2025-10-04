ebc656e6cf9da1114318be850ca6515fc317ac0b
Isaac Rossell i Ripoll <isaac.rossell.ripoll@outlook.com>
2025-10-04 10:28:53 +0200

    Singleton db

---
+++ jobs/Backend/Task/Singleton/DB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRateUpdater.Models;
+
+namespace ExchangeRateUpdater.Singleton
+{
+    internal sealed class DB
+    {
+        private static DB? _instance;
+
+        private Dictionary<string, Rate> _rates;
+
+        private DB() => _rates = new();
+
+        public static DB GetInstance() => _instance ??= new DB();
+
+        public void Add(string key, Rate value) => _rates.Add(key, value);
+
+        public bool TryGetValue(string key, out Rate rate) => _rates.TryGetValue(key, out rate);
+    }
+}
