a234b0cba2413c2bdef8a0c9420c09cbb9b848f9
Funminiyi Anwo <funminiyianwo@gmail.com>
2025-11-05 15:58:34 +0000

    Add caching

--- jobs/Backend/Task/ExchangeRateUpdater.Core/Providers/CzechNationalBankExchangeRateProvider.cs
+++ jobs/Backend/Task/ExchangeRateUpdater.Core/Providers/CzechNationalBankExchangeRateProvider.cs
@@ -1,22 +1,30 @@
 using ExchangeRateUpdater.Core.ApiVendors;
 using ExchangeRateUpdater.Core.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace ExchangeRateUpdater.Core.Providers;
 
 public class CzechNationalBankExchangeRateProvider(
-        IExchangeRateVendor exchangeRateVendor
+        IExchangeRateVendor exchangeRateVendor,
+        IMemoryCache? cache = null
     ) : IExchangeRateProvider
 {
     private const string BaseCurrencyCode = "CZK";
+    private const string CacheKey = $"ExchangeRates:{BaseCurrencyCode}";
+    private readonly IMemoryCache _cache = cache ?? new MemoryCache(new MemoryCacheOptions());
 
     public async Task<List<ExchangeRate>> GetExchangeRates(IEnumerable<Currency> currencies)
     {
         currencies = currencies.ToArray();
         if (!currencies.Any()) return [];
 
-        var rates = await exchangeRateVendor.GetExchangeRates(BaseCurrencyCode);
+        var allRates = await _cache.GetOrCreateAsync(CacheKey, async entry =>
+        {
+            entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1);
+            return await exchangeRateVendor.GetExchangeRates(BaseCurrencyCode);
+        }) ?? [];
 
-        return rates
+        return allRates
                 .Where(rate => currencies.Any(currency => currency.ToString() == rate.TargetCurrency.ToString()))
                 .ToList();
     }
