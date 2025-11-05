f507af9c682da990a50c76d8605df9c5f999d82b
Mihail Mogildea <mihail.mogildea@zen-solutions.com>
2025-11-05 18:32:17 +0100

    ConfigurationLayer V1.

---
+++ jobs/Backend/Task/ConfigurationLayer/Interface/IConfigurationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationLayer.Interface
+{
+    public interface IConfigurationService
+    {
+        Task<string> GetValueAsync(string key, string defaultValue = "");
+        Task<T?> GetValueAsync<T>(string key, T? defaultValue = default);
+    }
+}
---
+++ jobs/Backend/Task/ConfigurationLayer/Option/ExchangeRateProviderOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationLayer.Option
+{
+    public class ExchangeRateProviderOptions
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public string BaseCurrency { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public bool RequiresAuthentication { get; set; } = false;
+        public Dictionary<string, string> Configuration { get; set; } = new();
+    }
+
+    public class ExchangeRateProvidersOptions
+    {
+        public ExchangeRateProviderOptions CNB { get; set; } = new();
+        public ExchangeRateProviderOptions ECB { get; set; } = new();
+        public ExchangeRateProviderOptions BNR { get; set; } = new();
+    }
+}
